ee5e4b667d312ae453d34773a44b959642c79603
joelkim-negocio <joel.kim@negocio.co.kr>
2023-01-26 13:14:43 +0900

    add layout support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-export default function Home() {
-  return (
-    <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-      Hello world!
-    </h1>
-  );
-}
+const Home = () => <p>Home</p>;
+
+export default Home;
