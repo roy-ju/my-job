ef4ccd7ec03ee87b2248b8019bc42cc41c5d87ea
Yenny-Kim <yenny.kim@negocio.co.kr>
2023-02-27 16:17:18 +0900

    feat : update MapTabBar component

--- src/components/molecules/MapTabBar/index.tsx
+++ src/components/molecules/MapTabBar/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/atoms/Button';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
+import tw from 'twin.macro';
 
 function MapTabBar({ children }: { children: ReactNode }) {
   return (
@@ -9,19 +10,67 @@ function MapTabBar({ children }: { children: ReactNode }) {
   );
 }
 
-function BuildingTab() {
+function BuildingTab({
+  defaultTabIndex,
+  onChange,
+}: {
+  defaultTabIndex?: number;
+  onChange?: () => void;
+}) {
+  const TabIndex = {
+    Apart: 0,
+    villa: 1,
+    oneRoom: 2,
+  };
+
+  const [selectedTab, setSelectedTab] = useState<number>(
+    defaultTabIndex || TabIndex.Apart,
+  );
+
+  useEffect(() => {
+    if (!onChange) return;
+    onChange();
+  }, [onChange, selectedTab]);
+
   return (
     <div tw="flex items-center px-2 border-b-[1px] border-gray-300">
-      <Button theme="ghost" size="big">
-        <span tw="font-bold">아파트 · 오피스텔</span>
+      <Button
+        theme="ghost"
+        custom={tw`px-2`}
+        onClick={() => setSelectedTab(TabIndex.Apart)}
+      >
+        <span
+          tw="font-bold text-gray-600"
+          css={[selectedTab === TabIndex.Apart && tw`text-gray-1000`]}
+        >
+          아파트 · 오피스텔
+        </span>
       </Button>
       <div tw="w-[1px] h-2 bg-gray-300 mx-2" />
-      <Button theme="ghost" size="big">
-        <span tw="font-bold">빌라 · 주택</span>
+      <Button
+        theme="ghost"
+        custom={tw`px-2`}
+        onClick={() => setSelectedTab(TabIndex.villa)}
+      >
+        <span
+          tw="font-bold text-gray-600"
+          css={[selectedTab === TabIndex.villa && tw`text-gray-1000`]}
+        >
+          빌라 · 주택
+        </span>
       </Button>
       <div tw="w-[1px] h-2 bg-gray-300 mx-2" />
-      <Button theme="ghost" size="big">
-        <span tw="font-bold">원룸 · 투룸</span>
+      <Button
+        theme="ghost"
+        custom={tw`px-2`}
+        onClick={() => setSelectedTab(TabIndex.oneRoom)}
+      >
+        <span
+          tw="font-bold text-gray-600"
+          css={[selectedTab === TabIndex.oneRoom && tw`text-gray-1000`]}
+        >
+          원룸 · 투룸
+        </span>
       </Button>
     </div>
   );
