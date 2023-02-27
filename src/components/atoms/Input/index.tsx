7bf73511adececcae80e0a479afcd58cd3c2d9c9
Yenny-Kim <yenny.kim@negocio.co.kr>
2023-02-27 11:14:55 +0900

    feat : add Input component

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,45 @@
+import { ChangeEvent, ReactNode } from 'react';
+import tw, { TwStyle } from 'twin.macro';
+
+type Props = {
+  placeholder?: string;
+  children?: ReactNode;
+  divStyle?: TwStyle;
+  inputStyle?: TwStyle;
+  icon?: any;
+  disabled?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const style = {
+  default: tw`relative w-fit bg-white py-2.5 pl-4 pr-2.5 border-[1px] rounded-[0.5rem] shadow-[0px 12px 20px rgba(0, 0, 0, 0.1)] flex items-center`,
+  disabled: tw`bg-gray-100`,
+};
+
+export function Input({
+  placeholder,
+  children,
+  divStyle,
+  inputStyle,
+  icon,
+  disabled,
+  onChange,
+}: Props) {
+  return (
+    <div css={[style.default, disabled && style.disabled, divStyle]}>
+      {icon}
+      <input
+        placeholder={placeholder}
+        disabled={disabled}
+        onChange={onChange}
+        css={[
+          tw` w-[14.5rem]  placeholder:text-b1 placeholder:text-gray-600`,
+          disabled && style.disabled,
+          inputStyle,
+        ]}
+        type="text"
+      />
+      {children}
+    </div>
+  );
+}
