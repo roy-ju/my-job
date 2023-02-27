75799b6b88a7a50e258b8b42820f1b4c7cfdb31f
joelkim-negocio <joel.kim@negocio.co.kr>
2023-02-27 15:35:31 +0900

    feat: add listing, danji, region markers

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,4 +1,5 @@
 export { default as ClosablePanel } from './ClosablePanel';
 export { default as RegionMarker } from './map_markers/RegionMarker';
 export { default as DanjiMarker } from './map_markers/DanjiMarker';
+export { default as ListingMarker } from './map_markers/ListingMarker';
 export { default as GlobalNavigation } from './GlobalNavigation';
--- src/components/molecules/map_markers/DanjiMarker/index.tsx
+++ src/components/molecules/map_markers/DanjiMarker/index.tsx
@@ -10,78 +10,87 @@ export default React.memo(
     variant,
     area,
     price,
-    count,
+    count = 0,
   }: {
     variant: VariantKey;
     area: number;
     price: number;
     count: number;
   }) => (
-    <div tw="w-fit relative min-w-[3rem]">
-      <div tw="flex flex-col h-[54px] rounded-lg">
-        <div tw="flex">
-          <div
-            css={[
-              tw`relative h-[23px] bg-white pl-2 pr-3 flex flex-1 items-center justify-start rounded-tl-lg`,
-              count === 0 && tw`rounded-tr-lg`,
-            ]}
-          >
-            {count !== 0 && (
-              <div
-                css={[
-                  tw`bg-white absolute left-px top-px w-full h-full z-[-1] rounded-tl-lg`,
-                  count === 0 && tw`rounded-tr-lg`,
-                ]}
-              />
-            )}
-            <Numeral
-              css={[
-                tw`text-[10px] leading-[10px]`,
-                { color: variants[variant].textColor },
-              ]}
-              suffix="평"
-            >
-              {area}
-            </Numeral>
-          </div>
-          {count !== 0 && (
-            <div tw="relative">
-              <div tw="absolute w-full h-[2px] left-0 bottom-[-1px] bg-white z-[-1]" />
-              <MarkerRoundedCorner />
-              <div tw="absolute top-[-8px] left-[12px] text-info text-white font-bold bg-black px-2 h-5 rounded-[40px] shadow-[0px_8px_12px_rgba(0, 0, 0, 0.08)]">
-                {count}
-              </div>
-            </div>
-          )}
-        </div>
+    <div tw="relative w-fit">
+      {/* Content */}
+      <div tw="min-w-[3rem] h-[62px] pb-2 flex flex-col">
         <div
           css={[
-            tw`flex items-center justify-start flex-1 pl-2 pr-3 rounded-br-lg shadow-[0px_8px_12px_rgba(0, 0, 0, 0.08)]`,
-            { backgroundColor: variants[variant].bgColor },
+            tw`h-[23px] flex items-center justify-start pl-2`,
+            count !== 0 && tw`pr-6`,
           ]}
         >
+          <Numeral
+            css={[
+              tw`text-[10px] leading-[10px]`,
+              { color: variants[variant].textColor },
+            ]}
+            suffix="평"
+          >
+            {area}
+          </Numeral>
+        </div>
+        <div tw="flex flex-1 items-center justify-start pl-2 pr-3">
           <Numeral tw="text-b2 text-white font-bold" koreanNumber falsy="-">
             {price}
           </Numeral>
         </div>
       </div>
-      <div style={{ transform: 'translateY(-1px)' }}>
-        <MarkerTail color={variants[variant].bgColor} />
+      {/* Background */}
+      <div tw="absolute top-0 left-0 flex flex-col w-full h-full z-[-1]">
+        <div tw="relative flex">
+          <div
+            css={[
+              tw`flex-1 h-[23px] bg-white rounded-tl-lg`,
+              count === 0 && tw`rounded-tr-lg`,
+            ]}
+          />
+          <div tw="absolute left-0 bottom-0 w-full h-[2px] bg-white" />
+          {count !== 0 && (
+            <>
+              <div tw="absolute top-0 right-[23px] h-[23px] w-[2px] bg-white" />
+              <div tw="relative">
+                <MarkerRoundedCorner color="#FFF" />
+                <div tw="absolute h-5 px-2 top-[-8px] left-[12px] rounded-[40px] bg-black text-white text-info font-bold">
+                  {count}
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+        <div
+          css={[
+            tw`flex-1 rounded-br-lg`,
+            { backgroundColor: variants[variant].bgColor },
+          ]}
+        />
+        <div
+          css={{ color: variants[variant].bgColor }}
+          style={{ transform: 'translateY(-1px)' }}
+        >
+          <MarkerTail />
+        </div>
+        <div
+          css={[
+            tw`rounded-b-lg`,
+            {
+              zIndex: -1,
+              position: 'absolute',
+              bottom: '8px',
+              left: '0px',
+              height: '2px',
+              width: '90%',
+              boxShadow: '1px 3px 6px 1px rgba(0,0,0,0.5)',
+            },
+          ]}
+        />
       </div>
-      <div
-        css={[
-          tw`rounded-b-lg`,
-          {
-            zIndex: -1,
-            position: 'absolute',
-            bottom: '8px',
-            left: '0px',
-            height: '2px',
-            width: '90%',
-            boxShadow: '3px 3px 6px 1px rgba(0,0,0,0.5)',
-          },
-        ]}
-      />
     </div>
   ),
 );
