78ef03898ff400f105864b4bbfd600a01e1d486d
Yenny-Kim <yenny.kim@negocio.co.kr>
2023-02-28 11:17:45 +0900

    feat : add RadioButton component

---
+++ src/components/atoms/RadioButton/index.stories.tsx
@@ -0,0 +1,34 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import { useState } from 'react';
+import { RadioButton } from '.';
+
+export default {
+  title: 'atoms/RadioButton',
+  component: RadioButton,
+} as ComponentMeta<typeof RadioButton>;
+
+export const Default: ComponentStory<typeof RadioButton> = () => {
+  const [value, setValue] = useState<string | null>(null);
+
+  return (
+    <div tw="flex gap-4">
+      <RadioButton
+        label="연필"
+        currentValue={value}
+        onClick={(v: any) => setValue(v)}
+      />
+      <RadioButton
+        label="지우개"
+        currentValue={value}
+        onClick={(v: any) => setValue(v)}
+      />
+      <RadioButton
+        label="필통"
+        currentValue={value}
+        onClick={(v: any) => setValue(v)}
+      />
+    </div>
+  );
+};
+
+Default.args = {};
