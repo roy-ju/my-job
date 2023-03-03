d1fdffc1cb265dde1cb0b7732fb395231293ca52
Yenny-Kim <yenny.kim@negocio.co.kr>
2023-03-03 17:34:24 +0900

    feat : update MapSearchInput

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,7 +1,7 @@
-import { ChangeEvent, ReactNode } from 'react';
+import { ChangeEvent, HTMLAttributes, ReactNode } from 'react';
 import tw, { TwStyle } from 'twin.macro';
 
-type Props = {
+interface Props extends HTMLAttributes<HTMLInputElement> {
   placeholder?: string;
   children?: ReactNode;
   divStyle?: TwStyle;
@@ -9,10 +9,11 @@ type Props = {
   icon?: any;
   disabled?: boolean;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-};
+  value?: string;
+}
 
 const style = {
-  default: tw`relative w-fit bg-white py-2.5 pl-4 pr-2.5 border-[1px] rounded-[0.5rem] shadow-[0px 12px 20px rgba(0, 0, 0, 0.1)] flex items-center`,
+  default: tw`relative w-fit bg-white py-2.5 pl-4 pr-2.5 border-[1px] rounded-[0.5rem]  flex items-center`,
   disabled: tw`bg-gray-100`,
 };
 
@@ -24,14 +25,20 @@ function Input({
   icon,
   disabled,
   onChange,
+  onFocus,
+  onBlur,
+  value,
 }: Props) {
   return (
     <div css={[style.default, disabled && style.disabled, divStyle]}>
       {icon}
       <input
+        value={value}
         placeholder={placeholder}
         disabled={disabled}
         onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
         css={[
           tw` w-[14.6rem] h-[2.25rem]  placeholder:text-b1 placeholder:text-gray-600`,
           disabled && style.disabled,
