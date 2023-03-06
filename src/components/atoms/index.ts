4fe556f8fbb76942dce0142e56ac9318b82261ef
joelkim-negocio <joel.kim@negocio.co.kr>
2023-03-06 10:40:00 +0900

    feat: remove toggle

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -5,4 +5,3 @@ export { default as Checkbox } from './Checkbox';
 export { default as Input } from './Input';
 export { default as Moment } from './Moment';
 export { default as Radio } from './Radio';
-export { default as Toggle } from './Toggle';
