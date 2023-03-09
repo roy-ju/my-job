fb138d2bf4ce3f916e6061fa05c9680449d8b807
joelkim-negocio <joel.kim@negocio.co.kr>
2023-03-09 11:22:07 +0900

    feat: filter overflow x

--- src/components/organisms/MapFilter/index.tsx
+++ src/components/organisms/MapFilter/index.tsx
@@ -4,6 +4,7 @@ import tw, { styled } from 'twin.macro';
 import FilterIcon from '@/assets/icons/filter.svg';
 import { useControlled } from '@/hooks/utils';
 import { useCallback } from 'react';
+import ChevronDownIcon from '@/assets/icons/chevron_down.svg';
 
 const RealestateTypeTabButton = styled(
   ({ size = 'bigger', variant = 'ghost', ...props }: ButtonProps) => (
@@ -69,13 +70,24 @@ export default function MapFilter({
         </RealestateTypeTabButton>
       </div>
       <div tw="w-full h-px bg-gray-300" />
-      <div tw="flex items-center gap-2 p-4 overflow-x-hidden">
-        <FilterButton isSelected tw="p-2">
-          <FilterIcon />
-        </FilterButton>
-        <FilterButton isSelected>유형</FilterButton>
-        <FilterButton>거래종류</FilterButton>
-        <FilterButton>가격</FilterButton>
+      <div tw="relative">
+        <div tw="flex items-center gap-2 p-4 overflow-x-hidden">
+          <FilterButton isSelected tw="p-2">
+            <FilterIcon />
+          </FilterButton>
+          <FilterButton isSelected>유형</FilterButton>
+          <FilterButton>거래종류</FilterButton>
+          <FilterButton>거래종류</FilterButton>
+          <FilterButton>거래종류</FilterButton>
+          <FilterButton>거래종류</FilterButton>
+          <FilterButton>가격</FilterButton>
+        </div>
+        <div tw="absolute top-0 right-0 h-full flex items-center">
+          <div tw="w-10 h-full bg-gradient-to-l from-white to-transparent" />
+          <Button size="none" variant="ghost" tw="w-10 h-full pr-2 bg-white">
+            <ChevronDownIcon />
+          </Button>
+        </div>
       </div>
     </div>
   );
