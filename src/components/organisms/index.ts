59b8360a489e255c5c3b1838c85f8d4a281fb015
Yenny-Kim <yenny.kim@negocio.co.kr>
2023-03-10 15:32:55 +0900

    fix : remove MapTabBar component

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -3,7 +3,6 @@ export { default as GlobalNavigation } from './GlobalNavigation';
 export { default as RegionMarker } from './map_markers/RegionMarker';
 export { default as DanjiMarker } from './map_markers/DanjiMarker';
 export { default as ListingMarker } from './map_markers/ListingMarker';
-export { default as MapTabBar } from './MapTabBar';
 export { default as MapPriceSelect } from './MapPriceSelect';
 export { default as MapSearchTextField } from './MapSearchTextField';
 export { default as MapFilter } from './MapFilter';
