44e894c34963cee38260e316c95936eacd6dbe6e
joelkim-negocio <joel.kim@negocio.co.kr>
2023-03-22 09:40:02 +0900

    fix: marker animations

--- src/components/organisms/map_markers/SchoolMarker/index.tsx
+++ src/components/organisms/map_markers/SchoolMarker/index.tsx
@@ -26,16 +26,16 @@ function SchoolMarker({
   }
 
   return (
-    <button
-      type="button"
-      onClick={onClick}
-      css={[
-        tw`animate-scale will-change-transform [text-rendering: optimizeSpeed] min-w-[77px] w-fit flex items-center h-8 bg-white rounded-[26px] relative shadow-[7px 7px 7px rgba(0, 0, 0, 0.1)]`,
-        selected && tw`animate-bounce`,
-      ]}
-    >
-      <School css={[tw`absolute top-0 left-0`, createIconColor()]} />
-      <span tw="text-info font-bold pl-10 pr-4 whitespace-nowrap text-gray-1000">{name}</span>
+    <button type="button" onClick={onClick} tw="animate-scale will-change-transform [text-rendering: optimizeSpeed]">
+      <div
+        css={[
+          tw`min-w-[77px] w-fit flex items-center h-8 bg-white rounded-[26px] relative shadow-[7px 7px 7px rgba(0, 0, 0, 0.1)]`,
+          selected && tw`animate-bounce`,
+        ]}
+      >
+        <School css={[tw`absolute top-0 left-0`, createIconColor()]} />
+        <span tw="text-info font-bold pl-10 pr-4 whitespace-nowrap text-gray-1000">{name}</span>
+      </div>
     </button>
   );
 }
