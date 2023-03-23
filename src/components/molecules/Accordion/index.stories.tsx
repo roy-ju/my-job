42390f8594589bc627be1f654c581fde4d2fdbe1
joelkim-negocio <joel.kim@negocio.co.kr>
2023-03-23 18:02:18 +0900

    feat: implement Dropdown component

--- src/components/molecules/Accordion/index.stories.tsx
+++ src/components/molecules/Accordion/index.stories.tsx
@@ -8,12 +8,12 @@ export default {
 
 export const Default: ComponentStory<typeof Accordion> = () => (
   <Accordion>
-    <Accordion.Summary tw="px-5 py-4">This is Summary</Accordion.Summary>
-    <Accordion.Details tw="px-5 py-6">
-      <div tw="w-full">
-        THis is
+    <Accordion.Summary tw="px-5 py-4 font-bold">네고 진행중</Accordion.Summary>
+    <Accordion.Details tw="px-5 pt-1 pb-6">
+      <div tw="w-full text-b2 text-gray-700">
+        매물방문을 하시거나, 중개사가 허용할 경우 거래참여 (가격을 제안) 할 수 있습니다.
         <br />
-        the content
+        (어느 쪽이든) 가격을 수락할 때까지 네고는 시간의 제한없이 지속됩니다.
       </div>
     </Accordion.Details>
   </Accordion>
---
+++ src/components/molecules/Dropdown/index.stories.tsx
@@ -0,0 +1,47 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import Dropdown from '.';
+
+export default {
+  title: 'molecules/Dropdown',
+  component: Dropdown,
+} as ComponentMeta<typeof Dropdown>;
+
+export const Default: ComponentStory<typeof Dropdown> = (args) => (
+  <div tw="flex flex-col gap-2">
+    <Dropdown {...args}>
+      <Dropdown.Option value="매매가격">매매가격</Dropdown.Option>
+      <Dropdown.Option value="전월세가격">전월세가격</Dropdown.Option>
+    </Dropdown>
+  </div>
+);
+
+Default.args = {
+  variant: 'outlined',
+  size: 'big',
+};
+
+export const PlaceholderAndLabel: ComponentStory<typeof Dropdown> = () => (
+  <div tw="flex flex-col gap-2">
+    <Dropdown placeholder="가격을 선택해주세요">
+      <Dropdown.Option value="매매가격">매매가격</Dropdown.Option>
+      <Dropdown.Option value="전월세가격">전월세가격</Dropdown.Option>
+    </Dropdown>
+    <Dropdown label="가격타입">
+      <Dropdown.Option value="매매가격">매매가격</Dropdown.Option>
+      <Dropdown.Option value="전월세가격">전월세가격</Dropdown.Option>
+    </Dropdown>
+  </div>
+);
+
+export const ManyItems: ComponentStory<typeof Dropdown> = (args) => (
+  <Dropdown {...args}>
+    {Array(40)
+      .fill(0)
+      .map((_, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <Dropdown.Option key={index} value={`${index}`}>
+          {index}
+        </Dropdown.Option>
+      ))}
+  </Dropdown>
+);
