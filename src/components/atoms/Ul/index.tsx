80650585598267386b7b19266596f9d25b06a9be
joelkim-negocio <joel.kim@negocio.co.kr>
2023-03-28 17:47:10 +0900

    fix: ul component

--- src/components/atoms/Ul/index.tsx
+++ src/components/atoms/Ul/index.tsx
@@ -2,9 +2,10 @@ import tw, { styled } from 'twin.macro';
 
 const Ul = styled.ul`
   ${tw`leading-5 text-gray-700 text-info`}
+  padding-left: 0.6ch;
   li {
     list-style-type: '-';
-    padding-inline-start: 4px;
+    padding-inline-start: 2px;
     &:not(:last-of-type) {
       padding-bottom: 0.5em;
     }
