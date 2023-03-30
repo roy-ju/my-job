b2e70c67d4f96224104d4b25795ab5e7fd827522
이준근 <jay.lee@ijungeun-ui-MacBookPro.local>
2023-03-30 15:37:11 +0900

    Refactor ServiceContactListItem

--- src/components/organisms/ServiceContactListItem/index.tsx
+++ src/components/organisms/ServiceContactListItem/index.tsx
@@ -1,24 +1,28 @@
-import React, { ReactNode, Children, useMemo, useContext } from 'react';
+import React, { ReactNode, Children } from 'react';
 import { Accordion } from '@/components/molecules';
-import ServiceContactListItemContext from './ServiceContactListItemContext';
 
 interface ServiceContactListItemProps {
-  contents: string;
-  createdTime: string;
-  didReply: boolean;
   children: ReactNode;
 }
 
-function ServiceContactListItemUser() {
-  const { contents, createdTime, didReply } = useContext(ServiceContactListItemContext);
+interface ServiceContactListItemUserProps {
+  contents: string;
+  createdTime: string;
+  didReply?: true;
+}
+interface ServiceContactListItemAdminProps {
+  contents: string;
+  createdTime: string;
+}
 
+function ServiceContactListItemUser({ contents, createdTime, didReply }: ServiceContactListItemUserProps) {
   return (
-    <div tw="flex pl-5 py-5 pr-2">
+    <div tw="flex justify-between pl-5 py-5 pr-2 w-[23.75rem]">
       <div tw="basis-64">
-        <p tw="text-b2 text-left mb-2">{contents}</p>
+        <p tw="text-b2 text-left mb-2 break-all">{contents}</p>
         <span tw="text-info text-gray-700 text-left block">{createdTime}</span>
       </div>
-      <div tw=" flex-1 text-right">
+      <div tw="text-right">
         {didReply ? (
           <span tw="text-b2 text-green-1000">답변 완료</span>
         ) : (
@@ -28,28 +32,23 @@ function ServiceContactListItemUser() {
     </div>
   );
 }
-function ServiceContacatListItemAdmin() {
-  const { contents, createdTime } = useContext(ServiceContactListItemContext);
-
+function ServiceContacatListItemAdmin({ contents, createdTime }: ServiceContactListItemAdminProps) {
   return (
     <div tw="p-5">
-      <p tw="text-b2 mb-2">{contents}</p>
+      <p tw="text-b2 mb-2 break-all">{contents}</p>
       <span tw="text-info text-gray-700">{`네고시오 운영팀 | ${createdTime}`}</span>
     </div>
   );
 }
 
-function ServiceContactListItemMain({ contents, createdTime, didReply, children }: ServiceContactListItemProps) {
+function ServiceContactListItemMain({ children }: ServiceContactListItemProps) {
   const childrenArray = Children.toArray(children);
-  const context = useMemo(() => ({ contents, createdTime, didReply }), [contents, createdTime, didReply]);
 
   return (
-    <ServiceContactListItemContext.Provider value={context}>
-      <Accordion>
-        <Accordion.Summary hideArrow>{childrenArray[0]}</Accordion.Summary>
-        {childrenArray[1] && <Accordion.Details>{childrenArray[1]}</Accordion.Details>}
-      </Accordion>
-    </ServiceContactListItemContext.Provider>
+    <Accordion>
+      <Accordion.Summary hideArrow>{childrenArray[0]}</Accordion.Summary>
+      {childrenArray[1] && <Accordion.Details>{childrenArray[1]}</Accordion.Details>}
+    </Accordion>
   );
 }
 
