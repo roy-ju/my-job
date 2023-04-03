40a232e994381d02f3339c725a19557b817b5a9e
roy-ju <roy.ju@negocio.co.kr>
2023-04-03 14:27:53 +0900

    feat add MobileMap 2차 추가

--- src/components/atoms/BottomBezel/index.tsx
+++ src/components/atoms/BottomBezel/index.tsx
@@ -1,3 +1,3 @@
 import tw, { styled } from 'twin.macro';
 
-export default styled.div(() => [tw`w-52 h-5 bg-gray-900 rounded-100 mx-auto`]);
+export default styled.div(() => [tw`w-52 h-[5px] bg-gray-900 rounded-100 mx-auto`]);
