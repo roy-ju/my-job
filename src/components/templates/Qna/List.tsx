cc6a9ca40079fd4040288bd323d85dac2d79752d
Jay Lee <jay.lee@negocio.co.kr>
2023-04-04 16:15:26 +0900

    feat: 서비스문의, 자주 묻는 질문 구분선

--- src/components/templates/Qna/List.tsx
+++ src/components/templates/Qna/List.tsx
@@ -3,9 +3,6 @@ import QnaListItem from '@/components/organisms/QnaListItem';
 
 const ListContainer = styled.div`
   ${tw`max-h-full min-h-0 overflow-x-hidden overflow-y-auto`}
-  & > div {
-    ${tw`border-b border-gray-100`}
-  }
 `;
 
 export interface IQnaItem {
@@ -20,17 +17,20 @@ export interface IQnaItem {
 export default function List({ list }: { list: IQnaItem[] }) {
   return (
     <ListContainer>
-      {list.map((item) => (
-        <QnaListItem key={item.id}>
-          <QnaListItem.User
-            userMessage={item.user_message}
-            createdTime={item.created_time}
-            didReply={!!item.admin_message}
-          />
-          {item.admin_message ? (
-            <QnaListItem.Admin adminMessage={item.admin_message} responseTime={item.admin_response_time} />
-          ) : null}
-        </QnaListItem>
+      {list.map((item, i) => (
+        <>
+          {!!i && <div tw="border-t mx-5 border-gray-100" />}
+          <QnaListItem key={item.id}>
+            <QnaListItem.User
+              userMessage={item.user_message}
+              createdTime={item.created_time}
+              didReply={!!item.admin_message}
+            />
+            {item.admin_message ? (
+              <QnaListItem.Admin adminMessage={item.admin_message} responseTime={item.admin_response_time} />
+            ) : null}
+          </QnaListItem>
+        </>
       ))}
     </ListContainer>
   );
