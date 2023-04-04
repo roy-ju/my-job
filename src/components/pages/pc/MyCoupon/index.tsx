4490f5b1ab3cb5d6572c9cb6bd4ec12e6d482b00
Jay Lee <jay.lee@negocio.co.kr>
2023-04-04 09:33:49 +0900

    feat pages/myCoupon

--- src/components/pages/pc/MyCoupon/index.tsx
+++ src/components/pages/pc/MyCoupon/index.tsx
@@ -2,46 +2,15 @@ import { Panel } from '@/components/atoms';
 import { useAuth } from '@/hooks/services';
 import { useRouter } from '@/hooks/utils';
 import { memo } from 'react';
-import { ServiceContact as ServiceContactTemplate } from '@/components/templates';
+import { MyCoupon as MyCouponTemplate } from '@/components/templates';
 
 interface Props {
   depth: number;
   panelWidth?: string;
 }
 
-const mock = [
-  {
-    admin_message: 'string',
-    admin_response_time: 'string',
-    created_time: 'string',
-    id: 0,
-    user_id: 0,
-    user_message: 'string',
-  },
-  {
-    admin_message: 'string',
-    admin_response_time: 'string',
-    created_time: 'string',
-    id: 1,
-    user_id: 1,
-    user_message: 'string',
-  },
-  {
-    admin_message: 'string',
-    admin_response_time: 'string',
-    created_time: 'string',
-    id: 2,
-    user_id: 2,
-    user_message: 'string',
-  },
-];
-
-export default memo(({ depth, panelWidth }: Props) => {
-  const router = useRouter(depth);
-  const { user, isLoading } = useAuth();
-  return (
-    <Panel width={panelWidth}>
-      <ServiceContactTemplate list={mock} />
-    </Panel>
-  );
-});
+export default memo(({ depth, panelWidth }: Props) => (
+  <Panel width={panelWidth}>
+    <MyCouponTemplate hasData={false} />
+  </Panel>
+));
