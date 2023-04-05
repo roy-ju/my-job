b80e3328c9d22ffe1b3c5b177233f420a73ee41d
roy-ju <roy.ju@negocio.co.kr>
2023-04-05 23:59:50 +0900

    feat: update filter

--- src/hooks/utils/usePlatform.ts
+++ src/hooks/utils/usePlatform.ts
@@ -3,9 +3,13 @@ import { useMemo } from 'react';
 
 export default function usePlatform() {
   const { pathname } = useRouter();
+  
   return useMemo(() => {
-    const firstSegment = pathname.split('/')[0];
-    if (firstSegment === 'mobile') return 'mobile';
+    
+    const firstSegment = pathname.split('/')[1];
+    
+    if (firstSegment === 'm') return 'mobile';
+    
     return 'pc';
   }, [pathname]);
 }
