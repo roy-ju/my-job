3b6c0379cd0ee132081210a90ed33d9c5c37adbf
roy-ju <jujihong2@gmail.com>
2023-04-05 10:58:02 +0900

    feat: add map

--- src/components/molecules/FullScreenDialog/index.tsx
+++ src/components/molecules/FullScreenDialog/index.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { ReactNode, useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { ReactNode, useCallback, useEffect, useRef, useState } from 'react';
 
 import { FullScreenDialog as FullScreenDialogProps } from '@/states/fullscreenDialog';
 import useFullScreenDialogStore from '@/hooks/recoil/useFullScreenDialog';
 import { useRouter } from 'next/router';
 import { useScrollLock } from '@/hooks/utils/useScrollLock';
+import { useIsomorphicLayoutEffect } from '@/hooks/utils';
 
 type Props = {
   onChange: (active: boolean) => void;
@@ -34,8 +35,8 @@ function FullScreenDialog({ body, onClose }: FullScreenDialogProps) {
   }, [onClose]);
 
   return (
-    <div tw="absolute left-auto top-0 w-[100vw] h-[100%] overflow-hidden [z-index: 1300]">
-      <div tw="absolute w-[100vw] h-[100%] overflow-y-scroll overflow-x-hidden pointer-events-none">
+    <div tw="absolute w-[100vw] h-[100%] [z-index: 1300]">
+      <div tw="absolute w-[100vw] h-[100%] overflow-y-scroll overflow-x-hidden pointer-events-auto">
         <div tw="relative w-[100vw] h-[100%]">
           <div tw="pointer-events-auto w-[100vw]  overflow-x-hidden max-w-mobile min-h-[100%] my-0 mx-auto bg-white">
             {body && React.cloneElement(body, { onClose: handleClose })}
@@ -55,12 +56,12 @@ function FullScreenDialogs({ onChange }: Props) {
 
   const { pathname } = useRouter();
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     closeAll();
   }, [pathname]);
 
   return (
-    <div tw="absolute left-0 top-0 w-[100vw] h-[100%] overflow-hidden [z-index: 1300]" id="fullScreenDialog-container">
+    <div tw="absolute left-0 top-0 w-[100vw] h-[100%] [z-index: 1300]" id="fullScreenDialog-container">
       {dialogStates.map((dialog) => (
         <FullScreenDialog
           key={dialog.id}
