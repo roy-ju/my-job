d435e36ebffc82a956b679fe2853f6bfde2a510d
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-05 12:20:34 +0900

    fix: login with kakao

--- src/lib/kakao/login.ts
+++ src/lib/kakao/login.ts
@@ -2,7 +2,7 @@ export default function loginWithKakao(state?: string) {
   if (typeof Kakao !== 'undefined') {
     Kakao.Auth.authorize({
       redirectUri: `${window.location.origin}/callback/kakaoLogin`,
-      state,
+      state: state ?? '',
     });
   }
 }
