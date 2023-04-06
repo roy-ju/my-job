3d03587810df8e2b0cefa055535ab02ed439467b
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-06 12:24:05 +0900

    feat: verify myself

--- src/components/pages/pc/VerifyCi/index.tsx
+++ src/components/pages/pc/VerifyCi/index.tsx
@@ -1,7 +1,13 @@
 import { updateCi } from '@/apis/user/updateCi';
 import { Panel } from '@/components/atoms';
+import { OverlayPresenter } from '@/components/molecules';
+import { VerifyCiPopup } from '@/components/organisms';
 import { VerifyCi } from '@/components/templates';
+import ErrorCodes from '@/constants/error_codes';
+import { useAuth } from '@/hooks/services';
+import { useRouter } from '@/hooks/utils';
 import useNiceId, { NiceResponse } from '@/lib/nice/useNiceId';
+import Routes from '@/router/routes';
 import { memo, useCallback, useState } from 'react';
 
 interface Props {
@@ -9,23 +15,34 @@ interface Props {
   panelWidth?: string;
 }
 
-export default memo(({ panelWidth }: Props) => {
+export default memo(({ depth, panelWidth }: Props) => {
+  const { mutate: mutateUser } = useAuth();
+  const router = useRouter(depth);
+
   const { request } = useNiceId();
   const [errorCode, setErrorCode] = useState<number | null>(null);
 
-  const handleNiceResponse = useCallback(async (res: NiceResponse) => {
-    const updateCiRes = await updateCi({
-      encData: res.encData,
-      kie: res.kie,
-      integrityValue: res.integrityValue,
-      tokenVersionId: res.tokenVersionId,
-      type: Number(res.type),
-    });
+  const handleNiceResponse = useCallback(
+    async (res: NiceResponse) => {
+      const updateCiRes = await updateCi({
+        encData: res.encData,
+        kie: res.kie,
+        integrityValue: res.integrityValue,
+        tokenVersionId: res.tokenVersionId,
+        type: Number(res.type),
+      });
 
-    if (updateCiRes?.error_code) {
-      setErrorCode(updateCiRes?.error_code);
-    }
-  }, []);
+      if (updateCiRes?.error_code) {
+        setErrorCode(updateCiRes?.error_code);
+      }
+
+      if (!updateCiRes?.error_code) {
+        mutateUser(false);
+        router.replace(Routes.VerifyCiSuccess);
+      }
+    },
+    [router, mutateUser],
+  );
 
   const handleVerifyPhone = useCallback(() => {
     request('phone', handleNiceResponse);
@@ -35,11 +52,19 @@ export default memo(({ panelWidth }: Props) => {
     request('ipin', handleNiceResponse);
   }, [handleNiceResponse, request]);
 
-  console.log(errorCode);
-
   return (
     <Panel width={panelWidth}>
       <VerifyCi onClickIPinVerification={handleVerifyIPin} onClickPhoneVerification={handleVerifyPhone} />
+      {errorCode === ErrorCodes.DUPLCATED_CI && (
+        <OverlayPresenter>
+          <VerifyCiPopup.DuplicatedCi onClickClose={() => setErrorCode(null)} />
+        </OverlayPresenter>
+      )}
+      {errorCode === ErrorCodes.UNDER_NINETEEN && (
+        <OverlayPresenter>
+          <VerifyCiPopup.Under19 onClickClose={() => setErrorCode(null)} />
+        </OverlayPresenter>
+      )}
     </Panel>
   );
 });
