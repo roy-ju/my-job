7d71b75332044d50d0b0afb8bb040d525ebc6613
Jay Lee <jay.lee@negocio.co.kr>
2023-04-11 17:47:09 +0900

    chore: DateInput atoms에서 molcules로 스토리북 이동

--- src/components/molecules/DateInput/index.stories.tsx
+++ src/components/molecules/DateInput/index.stories.tsx
@@ -2,7 +2,7 @@ import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import DateInput from '.';
 
 export default {
-  title: 'atoms/DateInput',
+  title: 'molecules/DateInput',
   component: DateInput,
 } as ComponentMeta<typeof DateInput>;
 
