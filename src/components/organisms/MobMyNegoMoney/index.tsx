c4e03e99b6159b797cf09a9bb1c70655ad304c38
roy-ju <roy.ju@negocio.co.kr>
2023-04-11 23:08:33 +0900

    feat: 네고머니 네고포인트 쿠폰

--- src/components/organisms/MobMyNegoMoney/index.tsx
+++ src/components/organisms/MobMyNegoMoney/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Numeral } from '@/components/atoms';
+import NoData from '@/components/templates/MobMyNegoMoney/Nodata';
 
 export interface MyNegoMoneyProps {
   totalMoney?: number;
@@ -6,26 +7,27 @@ export interface MyNegoMoneyProps {
 
 export default function MobMyNegoMoney({ totalMoney = 0 }: MyNegoMoneyProps) {
   return (
-    <div tw="pt-6 pb-9 px-5 bg-white">
-      <div tw="text-b1 leading-none font-bold mb-3">보유 포인트</div>
-      <div tw="mb-3.5">
-        <Numeral tw="text-h1 text-nego-1000 font-bold leading-[30px]">{totalMoney}</Numeral>
-        <span tw="text-h3 leading-none text-nego-1000 font-bold pl-1">P</span>
+    <>
+      <div tw="pt-6 pb-9 px-5 bg-white">
+        <div tw="text-b1 leading-none font-bold mb-3">나의 네고머니</div>
+        <div tw="mb-3.5">
+          <Numeral tw="text-h1 text-nego-1000 font-bold leading-[30px]">{totalMoney}</Numeral>
+          <span tw="text-h1 [line-height: 1] leading-none text-nego-1000 font-bold pl-0">원</span>
+        </div>
+        <div tw="flex gap-2 mb-1 text-b2 leading-5">
+          <Button disabled variant="outlined" tw="w-full mb-4">
+            인출
+          </Button>
+          <Button variant="secondary" tw="w-full mb-4">
+            충전
+          </Button>
+        </div>
       </div>
-      <div tw="mb-1 text-b2 leading-5">
-        <span tw="text-gray-700">총 획득 포인트</span>
-        <span tw="pl-1 text-nego-1000" />
+      <div tw="h-3 bg-gray-100 shrink-0" />
+      <div tw="mt-[2.5rem] px-5">
+        <span tw="text-b1 font-bold [line-height: 1] text-gray-1000 ">지난 내역</span>
+        <NoData />
       </div>
-      <div tw="mb-5 text-b2 leading-5">
-        <span tw="text-gray-700">사용된 포인트</span>
-        <span tw="pl-1 text-nego-1000" />
-      </div>
-      <Button disabled variant="outlined" tw="w-full mb-4">
-        네고머니로 전환하기
-      </Button>
-      <div tw="text-info text-gray-700">
-        네고포인트는 이후에 네고머니로 전환하거나 서비스 내에서 서비스 구매를 위해 사용할 수 있습니다.
-      </div>
-    </div>
+    </>
   );
 }
--- src/components/templates/MobMyNegoMoney/index.tsx
+++ src/components/templates/MobMyNegoMoney/index.tsx
@@ -4,12 +4,14 @@ import React from 'react';
 
 export interface NegoMoneyProps {
   totalMoney?: number;
+  onClickBack?: () => void;
 }
 
-export default function MobMyNegoMoney({ totalMoney }: NegoMoneyProps) {
+export default function MobMyNegoMoney({ totalMoney, onClickBack }: NegoMoneyProps) {
   return (
-    <div tw="h-full flex flex-col">
+    <div tw="w-full max-w-mobile mx-auto fixed right-0 left-0 h-full flex flex-col bg-white">
       <NavigationHeader>
+        <NavigationHeader.BackButton onClick={onClickBack} />
         <NavigationHeader.Title>네고머니</NavigationHeader.Title>
       </NavigationHeader>
       <div tw="flex-1 flex flex-col min-h-0">
