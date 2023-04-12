48382ed291aeda96b96d32f4e9380229433698e3
Jay Lee <jay.lee@negocio.co.kr>
2023-04-12 11:40:49 +0900

    chore: organisms/TranditionCondition

--- src/components/organisms/TransactionCondition/index.stories.tsx
+++ src/components/organisms/TransactionCondition/index.stories.tsx
@@ -13,18 +13,31 @@ export const Default: ComponentStory<typeof TransactionCondition> = () => (
     <TransactionCondition>
       <TransactionCondition.List>
         <TransactionCondition.Item label="거래종류" value="월세" />
-        <TransactionCondition.Item label="희망가" value="3억 1,000만/100만" />
+        <TransactionCondition.Item label="희망가" value={100_000} />
         <TransactionCondition.Section title="지급일정" hasToolTip>
-          <TransactionCondition.Item label="계약금" value={1} />
-          <TransactionCondition.Item label="중도금" value={1} />
-          <TransactionCondition.Item label="잔금" value={1} />
-          <TransactionCondition.Item label="실제지급총액" value={1} />
+          <TransactionCondition.Item label="계약금" value={1_000_000} />
+          <TransactionCondition.Item label="중도금" value={1_000_000} />
+          <TransactionCondition.Item label="잔금" value={1_000_000} />
+          <TransactionCondition.Item
+            label="실제지급총액"
+            value={20_000_000}
+            value2={[
+              { name: 'Jay', amount: 10_000_000 },
+              { name: '조엘', amount: 10_000_000 },
+            ]}
+          />
         </TransactionCondition.Section>
         <TransactionCondition.Section title="선순위 담보권" hasToolTip>
-          <TransactionCondition.Item label="선순위담보권" value={{ name: 'Jay', amount: 1 }} />
+          <TransactionCondition.Item
+            label="선순위담보권"
+            value={[
+              { name: 'Jay', amount: 10_000_000 },
+              { name: '조엘', amount: 10_000_000 },
+            ]}
+          />
         </TransactionCondition.Section>
         <TransactionCondition.Section title="세부정보">
-          <TransactionCondition.Item label="입주가능시기" value="2022.03.03 이전" />
+          <TransactionCondition.Item label="입주가능시기" value="2022-03-03" />
           <TransactionCondition.Item label="전세자금대출" value={false} />
           <TransactionCondition.Item label="임대기간" value={2} />
           <TransactionCondition.Item label="임대할부분" value="전체" />
