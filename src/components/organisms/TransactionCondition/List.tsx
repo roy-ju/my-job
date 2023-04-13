005c54516249cfd738f984c1d920eeb2500b27c1
Jay Lee <jay.lee@negocio.co.kr>
2023-04-13 15:57:28 +0900

    fix: organisms/TransactionCondition key prop 추가

--- src/components/organisms/TransactionCondition/List.tsx
+++ src/components/organisms/TransactionCondition/List.tsx
@@ -1,22 +1,23 @@
 import React, { Children, ReactNode } from 'react';
-import { v4 as uuid4 } from 'uuid';
 
 interface ListProps {
   children?: ReactNode;
 }
 
+/* eslint-disable */
+
 export default function List({ children }: ListProps) {
   const childrenArray = Children.toArray(children);
 
   return (
-    <React.Fragment key={uuid4()}>
+    <>
       {Array.isArray(childrenArray) &&
         childrenArray.map((child, i) => (
-          <>
+          <React.Fragment key={i}>
             {!!i && <div tw="border-t mx-5 border-gray-100" />}
             {child}
-          </>
+          </React.Fragment>
         ))}
-    </React.Fragment>
+    </>
   );
 }
