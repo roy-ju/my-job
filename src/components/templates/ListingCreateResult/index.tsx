b338085e6ccd1570f0353c098ec688f1d36e8057
Jay Lee <jay.lee@negocio.co.kr>
2023-04-13 14:34:40 +0900

    chore: props 수정

--- src/components/templates/ListingCreateResult/index.tsx
+++ src/components/templates/ListingCreateResult/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Loading, Separator } from '@/components/atoms';
 import { NavigationHeader } from '@/components/molecules';
-import { AgentCardItem, ListingCreateResultStatus } from '@/components/organisms';
+import { AgentCardItem, ListingCreateResultStatus, TransactionCondition } from '@/components/organisms';
 import { AgentCarouselItem } from '@/components/organisms/AgentCardCarousel';
 import { AddressListItem } from '@/components/organisms/ListingCreateResultStatus/MultipleAddressesFound';
 import { ListingStatus } from '@/constants/enums';
@@ -22,7 +22,35 @@ export interface ListingCreateResultProps {
   addressLine2?: string;
 
   buyOrRent?: number;
+  tradePrice?: number;
+  deposit?: number;
+  monthlyRentFee?: number;
+  interimAmount1?: number;
+  interimAmount2?: number;
+  interimAmount3?: number;
+  interimAmountNegotiable1?: boolean;
+  interimAmountNegotiable2?: boolean;
+  interimAmountNegotiable3?: boolean;
+  interimAmountPaymentTime1?: string;
+  interimAmountPaymentTime2?: string;
+  interimAmountPaymentTime3?: string;
+  interimAmountPaymentTimeType1?: number;
+  interimAmountPaymentTimeType2?: number;
+  interimAmountPaymentTimeType3?: number;
+  contractAmount?: number;
+  contractAmountNegotiable?: boolean;
+  remainingAmount?: number;
+  remainingAmountPaymentTime?: string;
+  remainingAmountPaymentTimeType?: number;
+  debtSuccessions?: { name: string; amount: number }[];
+  collaterals?: { name: string; amount: number }[];
+  moveInDate?: string;
+  moveInDateType?: number;
+  rentTermYear?: number;
+  rentTermMonth?: number;
   rentArea?: string;
+  specialTerms?: string;
+  jeonsaeLoan?: boolean;
 
   addressList?: AddressListItem[];
   agents?: AgentCarouselItem[];
@@ -46,8 +74,6 @@ export default function ListingCreateResult({
   listingStatus,
   addressLine1,
   addressLine2,
-  buyOrRent,
-  rentArea,
   addressList,
   agents,
   onClickMyListings,
@@ -55,6 +81,8 @@ export default function ListingCreateResult({
   onSelectAddress,
   onSelectAgent,
   onClickStartOver,
+
+  ...conditionItemProps
 }: ListingCreateResultProps) {
   if (isLoading) {
     return (
@@ -70,6 +98,8 @@ export default function ListingCreateResult({
         <NavigationHeader.Title>매물등록 신청 결과</NavigationHeader.Title>
       </NavigationHeader>
       <div tw="flex-1 min-h-0 overflow-auto">
+        <ListingCreateResultStatus.OwnerConsent />
+
         <div tw="pt-6 pb-10">
           {listingStatus === ListingStatus.VerifyAddress && <ListingCreateResultStatus.VerifyingAddress />}
           {listingStatus === ListingStatus.WaitingForAgentCompletion && (
@@ -87,8 +117,8 @@ export default function ListingCreateResult({
             <ListingCreateResultStatus.Duplicated
               addressLine1={addressLine1 ?? ''}
               addressLine2={addressLine2 ?? ''}
-              buyOrRent={buyOrRent ?? 0}
-              rentArea={rentArea}
+              buyOrRent={conditionItemProps.buyOrRent ?? 0}
+              rentArea={conditionItemProps.rentArea}
             />
           )}
           {listingStatus === ListingStatus.MultipleAddressesFound && (
@@ -105,7 +135,7 @@ export default function ListingCreateResult({
         </div>
         <Separator />
         {agentName && (
-          <div tw="pt-10 px-5">
+          <div tw="py-10 px-5">
             <div tw="text-b1 font-bold mb-4">중개사 정보</div>
             <div tw="rounded-xl bg-gray-100">
               <AgentCardItem defaultExpanded>
@@ -125,7 +155,84 @@ export default function ListingCreateResult({
             </div>
           </div>
         )}
-        <div tw="pt-10 px-5 pb-10">
+        <Separator />
+        <div tw="px-5">
+          <div tw="mt-10 mb-4 text-b1 font-bold leading-none">거래조건</div>
+          <TransactionCondition>
+            <TransactionCondition.List>
+              {!!conditionItemProps.buyOrRent && <TransactionCondition.Item label="거래종류" {...conditionItemProps} />}
+              {!!(conditionItemProps.tradePrice || conditionItemProps.deposit) && (
+                <TransactionCondition.Item label="희망가" {...conditionItemProps} />
+              )}
+
+              {!!(
+                conditionItemProps.contractAmount ||
+                conditionItemProps.interimAmount1 ||
+                conditionItemProps.remainingAmount ||
+                conditionItemProps.tradePrice ||
+                conditionItemProps.deposit
+              ) && (
+                <TransactionCondition.Section title="지급일정" hasToolTip>
+                  {!!conditionItemProps.contractAmount && (
+                    <TransactionCondition.Item label="계약금" {...conditionItemProps} />
+                  )}
+                  {!!conditionItemProps.interimAmount1 && (
+                    <TransactionCondition.Item label="중도금" {...conditionItemProps} />
+                  )}
+                  {!!conditionItemProps.remainingAmount && (
+                    <TransactionCondition.Item label="잔금" {...conditionItemProps} />
+                  )}
+                  {!!(conditionItemProps.tradePrice || conditionItemProps.deposit) && (
+                    <TransactionCondition.Item label="실제지급총액" {...conditionItemProps} />
+                  )}
+                </TransactionCondition.Section>
+              )}
+
+              {!!conditionItemProps.collaterals?.length && (
+                <TransactionCondition.Section title="선순위 담보권" hasToolTip>
+                  <TransactionCondition.Item label="선순위담보권" {...conditionItemProps} />
+                </TransactionCondition.Section>
+              )}
+              {!!conditionItemProps.debtSuccessions?.length && (
+                <TransactionCondition.Section title="채무승계 희망금액" hasToolTip>
+                  <TransactionCondition.Item label="채무승계희망금액" {...conditionItemProps} />
+                </TransactionCondition.Section>
+              )}
+
+              {!!(
+                conditionItemProps.moveInDate ||
+                conditionItemProps.jeonsaeLoan ||
+                conditionItemProps.rentTermMonth ||
+                conditionItemProps.rentTermYear ||
+                conditionItemProps.rentArea
+              ) && (
+                <TransactionCondition.Section title="세부정보">
+                  {conditionItemProps.moveInDate && (
+                    <TransactionCondition.Item label="입주가능시기" {...conditionItemProps} />
+                  )}
+                  {conditionItemProps.jeonsaeLoan && (
+                    <TransactionCondition.Item label="전세자금대출" {...conditionItemProps} />
+                  )}
+                  {(conditionItemProps.rentTermYear || conditionItemProps.rentTermMonth) && (
+                    <TransactionCondition.Item label="임대기간" {...conditionItemProps} />
+                  )}
+                  {conditionItemProps.rentArea && (
+                    <TransactionCondition.Item label="임대할부분" {...conditionItemProps} />
+                  )}
+                </TransactionCondition.Section>
+              )}
+              {!!conditionItemProps.specialTerms && (
+                <TransactionCondition.Item label="특약조건" {...conditionItemProps} />
+              )}
+            </TransactionCondition.List>
+          </TransactionCondition>
+        </div>
+        <div tw="py-10 flex items-center justify-center">
+          <Button variant="ghost" size="none" tw="underline text-info" onClick={() => {}}>
+            입력정보 수정/중개사 재선택
+          </Button>
+        </div>
+        <div tw=" px-5 pb-10">
           <Button size="bigger" tw="w-full" onClick={onClickMyListings}>
             나의 거래 목록
           </Button>
