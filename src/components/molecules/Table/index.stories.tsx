39f72848a9a58d84370210038c001057670c8984
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-13 17:54:48 +0900

    feat: Table Component

--- src/components/molecules/Table/index.stories.tsx
+++ src/components/molecules/Table/index.stories.tsx
@@ -10,35 +10,41 @@ export const Default: ComponentStory<typeof Table> = () => (
   <Table>
     <Table.Body>
       <Table.Row>
-        <Table.Head>거래종류</Table.Head>
-        <Table.Data>월세</Table.Data>
+        <Table.Head>매물번호</Table.Head>
+        <Table.Data>T22208230002</Table.Data>
       </Table.Row>
       <Table.Row>
-        <Table.Head>희망가</Table.Head>
-        <Table.Data>3억 1,000만 / 100만</Table.Data>
+        <Table.Head>부동산 종류</Table.Head>
+        <Table.Data>아파트</Table.Data>
       </Table.Row>
     </Table.Body>
     <Table.Group>
-      <Table.GroupSummary>하이</Table.GroupSummary>
+      <Table.GroupSummary>세부정보</Table.GroupSummary>
       <Table.GroupDetails>
         <Table.Row>
-          <Table.Head>개인정보 관리 책임자</Table.Head>
-          <Table.Data>JIHYO KIM</Table.Data>
+          <Table.Head>면적</Table.Head>
+          <Table.Data>공급 114㎡ / 전용 97㎡</Table.Data>
         </Table.Row>
         <Table.Row>
-          <Table.Head>사업자 등록번호</Table.Head>
-          <Table.Data>11</Table.Data>
+          <Table.Head>방향</Table.Head>
+          <Table.Data>남향</Table.Data>
         </Table.Row>
       </Table.GroupDetails>
     </Table.Group>
     <Table.Body>
       <Table.Row>
-        <Table.Head>개인정보 관리 책임자</Table.Head>
-        <Table.Data>SAM LEE</Table.Data>
+        <Table.Head>매물번호</Table.Head>
+        <Table.Data>T22208230002</Table.Data>
       </Table.Row>
       <Table.Row>
-        <Table.Head>사업자 등록번호</Table.Head>
-        <Table.Data>124354656867856</Table.Data>
+        <Table.Head>부동산 종류</Table.Head>
+        <Table.Data>아파트</Table.Data>
+      </Table.Row>
+      <Table.Row>
+        <Table.Head>기타 옵션</Table.Head>
+        <Table.Data>
+          에어컨, 붙박이장, 세탁기, 전기오븐, 인덕션, 식기세척기, 냉장고, 건조기, 디지털도어락, 욕조
+        </Table.Data>
       </Table.Row>
     </Table.Body>
   </Table>
