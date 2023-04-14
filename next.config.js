8f566f8ba997567c103d96267c3f492f822eb89e
Jay Lee <jay.lee@negocio.co.kr>
2023-04-14 16:01:44 +0900

    chore: next/image 관련 설정

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const nextConfig = withTwin({
       },
       {
         protocol: 'https',
+        hostname: 'agent-test2.negocio.kr',
+        port: '',
+      },
+      {
+        protocol: 'https',
         hostname: 'negocio-agent-photos.s3.ap-northeast-2.amazonaws.com',
         port: '',
       },
