174b9875917d0a1cfdc2850638ffdd6e1cf9dc66
roy-ju <jujihong2@gmail.com>
2023-04-18 17:11:08 +0900

    feat: 단지상세관련 업데이트

--- src/components/organisms/DanjiDetail/index.tsx
+++ src/components/organisms/DanjiDetail/index.tsx
@@ -1,9 +1,11 @@
 import React, { ReactNode } from 'react';
 import ActiveListingInfo from './ActiveListingInfo';
 import BasicInfo from './BasicInfo';
+import DetailInfo from './DetailInfo';
 import RealPriceInfo from './RealPriceInfo';
 import RealPricesList from './RealPricesList';
 import RealPricesPyoungList from './RealPricesPyoungList';
+import SchoolInfo from './SchoolInfo';
 
 interface DanjiDetailSectionProps {
   children: ReactNode;
@@ -15,8 +17,10 @@ function DanjiDetailSection({ children }: DanjiDetailSectionProps) {
 
 export default Object.assign(DanjiDetailSection, {
   Info: BasicInfo,
+  DetailInfo,
   ActiveInfo: ActiveListingInfo,
   RealPriceInfo,
   RealPricesPyoungList,
   RealPricesList,
+  SchoolInfo,
 });
