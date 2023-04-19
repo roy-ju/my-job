09afcf8672beb380cbfbc49233ca88abbfec1a5a
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-19 09:53:56 +0900

    feat: 매물상세 중개사 채팅 CTA 버튼 연결

--- src/components/templates/ListingDetail/index.tsx
+++ src/components/templates/ListingDetail/index.tsx
@@ -1,7 +1,7 @@
 import { GetListingDetailResponse } from '@/apis/listing/getListingDetail';
 import { PersistentBottomBar } from '@/components/atoms';
 import { NavigationHeader } from '@/components/molecules';
-import { ListingCtaButtons } from '@/components/organisms';
+import { ListingCtaButtons, PhotoHero } from '@/components/organisms';
 
 import HeartOutlinedIcon from '@/assets/icons/heart_outlined.svg';
 import ShareIcon from '@/assets/icons/share.svg';
@@ -15,26 +15,28 @@ export interface ListingDetailProps {
 
   onNavigateToParticipateBidding?: () => void;
   onNavigateToUpdateBidding?: () => void;
+  onNavigateToChatRoom?: () => void;
 }
 
 export default function ListingDetail({
   listingDetail,
   onNavigateToParticipateBidding,
   onNavigateToUpdateBidding,
+  onNavigateToChatRoom,
 }: ListingDetailProps) {
   const scrollContainer = useRef<HTMLDivElement | null>(null);
 
-  const [isHeaderActive, setIsHeaderActive] = useState(true);
+  const [isHeaderActive, setIsHeaderActive] = useState(false);
 
-  useScroll(scrollContainer, () => {
-    setIsHeaderActive(true);
+  useScroll(scrollContainer, ({ scrollY }) => {
+    setIsHeaderActive(scrollY > 0.1);
   });
 
   return (
     <div tw="relative flex flex-col h-full">
       <NavigationHeader
         css={[
-          tw`absolute top-0 left-0 w-full text-white transition-colors bg-transparent`,
+          tw`absolute top-0 left-0 z-50 w-full text-white transition-colors bg-transparent`,
           isHeaderActive && tw`bg-white text-gray-1000`,
         ]}
       >
@@ -49,8 +51,7 @@ export default function ListingDetail({
         </div>
       </NavigationHeader>
       <div tw="flex-1 min-h-0 overflow-auto" ref={scrollContainer}>
-        {/* <div tw="w-full h-[256px] bg-gray-700 flex items-center justify-center font-bold">IMAGE SECTION</div> */}
-        <div tw="h-[2000px]" />
+        <PhotoHero itemSize={0} />
       </div>
       <PersistentBottomBar>
         <ListingCtaButtons
@@ -58,6 +59,7 @@ export default function ListingDetail({
           buttonSize="bigger"
           onNavigateToParticipateBidding={onNavigateToParticipateBidding}
           onNavigateToUpdateBidding={onNavigateToUpdateBidding}
+          onNavigateToChatRoom={onNavigateToChatRoom}
         />
       </PersistentBottomBar>
     </div>
