f20411af6212992863a2f223fa9463ca74e38e7a
roy-ju <roy.ju@negocio.co.kr>
2023-04-19 18:07:37 +0900

    feat: update tabs

--- src/components/templates/RealPriceDetail/RealPriceDetailContent.tsx
+++ src/components/templates/RealPriceDetail/RealPriceDetailContent.tsx
@@ -1,5 +1,7 @@
 import { GetDanjiDetailResponse } from '@/apis/danji/danjiDetail';
+import { Separator } from '@/components/atoms';
 import React from 'react';
+import { RealPriceAllListWrraper } from './RealPriceListAll/RealPriceAllListWrraper';
 import { RealPriceTAndVChartWrraper } from './RealPriceTAndVChart/RealPriceTAndVChartWrraper';
 import { RealPriceTradeTrendChartWrraper } from './RealPriceTradeTrends/RealPriceTradeTrendChartWrraper';
 import { RealPriceTrendChartWrraper } from './RealPriceTrendsChart/RealPriceTrendChartWrraper';
@@ -15,11 +17,11 @@ export default function RealPriceDetailContent({
   selectedYear?: number;
 }) {
   return (
-    <div tw="pt-6 px-5">
-      <div tw="mb-10">
+    <div tw="pt-6">
+      <div tw="mb-10 px-5">
         <span tw="font-bold text-b1 [line-height: 19px]">실거래가 추이</span>
       </div>
-      <div tw="flex flex-col gap-10">
+      <div tw="flex flex-col gap-10 px-5 pb-14">
         <RealPriceTrendChartWrraper danji={danji} buyOrRent={buyOrRent} selectedYear={selectedYear} />
 
         <RealPriceValuesChartWrraper danji={danji} buyOrRent={buyOrRent} selectedYear={selectedYear} />
@@ -28,6 +30,8 @@ export default function RealPriceDetailContent({
 
         <RealPriceTradeTrendChartWrraper danji={danji} buyOrRent={buyOrRent} selectedYear={selectedYear} />
       </div>
+      <Separator tw="min-h-[8px] min-w-[1px] bg-gray-300" />
+      <RealPriceAllListWrraper danji={danji} buyOrRent={buyOrRent} />
     </div>
   );
 }
