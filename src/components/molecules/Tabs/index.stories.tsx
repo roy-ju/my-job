0e4044ce597e689e8f25f6550363fecde64af362
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-19 19:53:12 +0900

    fix: Tabs 컴포넌트 애니메이션 이상

--- src/components/molecules/Tabs/index.stories.tsx
+++ src/components/molecules/Tabs/index.stories.tsx
@@ -1,4 +1,5 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
+import { useState } from 'react';
 import Tabs from '.';
 
 export default {
@@ -9,6 +10,18 @@ export default {
 export const Default: ComponentStory<typeof Tabs> = () => (
   <div tw="flex flex-col gap-2">
     <Tabs>
+      <Tabs.Tab value={0}>거래정보</Tabs.Tab>
+      <Tabs.Tab value={1}>단지정보</Tabs.Tab>
+      <Tabs.Tab value={2}>거래정보</Tabs.Tab>
+      <Tabs.Indicator />
+    </Tabs>
+    <Tabs>
+      <Tabs.Tab value={0}>거래정보</Tabs.Tab>
+      <Tabs.Tab value={1}>단지정보</Tabs.Tab>
+      <Tabs.Tab value={2}>거래정보</Tabs.Tab>
+      <Tabs.Indicator />
+    </Tabs>
+    <Tabs variant="contained">
       <Tabs.Tab value={0}>거래정보</Tabs.Tab>
       <Tabs.Tab value={1}>단지정보</Tabs.Tab>
       <Tabs.Tab value={2}>거래정보</Tabs.Tab>
@@ -28,3 +41,19 @@ export const Default: ComponentStory<typeof Tabs> = () => (
     </Tabs>
   </div>
 );
+
+export const LayoutRoot = () => {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div>
+      {index === 1 && <div tw="bg-green-600 h-[200px]" />}
+      <Tabs variant="contained" value={index} onChange={(i) => setIndex(i)}>
+        <Tabs.Tab value={0}>거래정보</Tabs.Tab>
+        <Tabs.Tab value={1}>단지정보</Tabs.Tab>
+        <Tabs.Tab value={2}>거래정보</Tabs.Tab>
+        <Tabs.Indicator />
+      </Tabs>
+    </div>
+  );
+};
