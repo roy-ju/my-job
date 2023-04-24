594d06ce2b8a94969fc14c5500139aa7867a3ef0
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-24 13:24:14 +0900

    fix: 나의 추천요청 레이아웃 수정

--- src/components/pages/pc/SuggestRequestedList/index.tsx
+++ src/components/pages/pc/SuggestRequestedList/index.tsx
@@ -3,10 +3,11 @@ import useAPI_GetMySuggestList from '@/apis/suggest/getMySuggestList';
 import { resumeSuggest } from '@/apis/suggest/resumeSuggest';
 import { stopSuggest } from '@/apis/suggest/stopSuggest';
 import { Loading, Panel } from '@/components/atoms';
+import { OverlayPresenter, Popup } from '@/components/molecules';
 import { SuggestRequestedList } from '@/components/templates';
 import { useRouter } from '@/hooks/utils';
 import Routes from '@/router/routes';
-import { memo, useCallback, useState } from 'react';
+import { memo, useCallback, useRef, useState } from 'react';
 
 interface Props {
   depth: number;
@@ -15,6 +16,10 @@ interface Props {
 
 export default memo(({ panelWidth, depth }: Props) => {
   const router = useRouter(depth);
+
+  const popupDataRef = useRef(0);
+
+  const [popup, setPopup] = useState('none');
 
   const { data, isLoading, mutate } = useAPI_GetMySuggestList();
 
@@ -32,11 +37,31 @@ export default memo(({ panelWidth, depth }: Props) => {
   }, []);
 
   const handleClickDelete = useCallback(async () => {
+    setPopup('delete');
+  }, []);
+
+  const handleStopSuggest = useCallback(async () => {
+    if (popupDataRef.current) {
+      await stopSuggest(popupDataRef.current);
+      mutate();
+      setPopup('none');
+    }
+  }, [mutate]);
+
+  const handleResumeSuggest = useCallback(async () => {
+    if (popupDataRef.current) {
+      await resumeSuggest(popupDataRef.current);
+      mutate();
+      setPopup('none');
+    }
+  }, [mutate]);
+
+  const handleDeleteConfirm = useCallback(async () => {
     const items = Object.entries(itemsToDelete)
       .filter(([_, checked]) => checked)
       .map(([id]) => Number(id));
     if (items) {
-      console.log(items);
+      setPopup('none');
       await deleteSuggests(items);
       await mutate();
     }
@@ -45,10 +70,11 @@ export default memo(({ panelWidth, depth }: Props) => {
   const handleChangeSuggestChecked = useCallback(
     (id: number, checked: boolean) => {
       if (listStyle === 'default') {
+        popupDataRef.current = id;
         if (checked) {
-          stopSuggest(id);
+          setPopup('suggestStop');
         } else {
-          resumeSuggest(id);
+          setPopup('suggestResume');
         }
       } else {
         setItemsToDelete((prev) => ({
@@ -80,6 +106,45 @@ export default memo(({ panelWidth, depth }: Props) => {
         onChangeListStyle={handleChangeListStyle}
         onChangeSuggestChecked={handleChangeSuggestChecked}
       />
+      {popup === 'delete' && (
+        <OverlayPresenter>
+          <Popup>
+            <Popup.ContentGroup tw="gap-5 py-12">
+              <Popup.Title tw="text-center">선택한 항목을 목록에서 삭제하시겠습니까?</Popup.Title>
+            </Popup.ContentGroup>
+            <Popup.ButtonGroup>
+              <Popup.CancelButton onClick={() => setPopup('none')}>취소</Popup.CancelButton>
+              <Popup.ActionButton onClick={handleDeleteConfirm}>확인</Popup.ActionButton>
+            </Popup.ButtonGroup>
+          </Popup>
+        </OverlayPresenter>
+      )}
+      {popup === 'suggestStop' && (
+        <OverlayPresenter>
+          <Popup>
+            <Popup.ContentGroup tw="gap-5 py-12">
+              <Popup.Title tw="text-center">선택한 요청의 매물 추천을 그만 받으시겠습니까?</Popup.Title>
+            </Popup.ContentGroup>
+            <Popup.ButtonGroup>
+              <Popup.CancelButton onClick={() => setPopup('none')}>취소</Popup.CancelButton>
+              <Popup.ActionButton onClick={handleStopSuggest}>확인</Popup.ActionButton>
+            </Popup.ButtonGroup>
+          </Popup>
+        </OverlayPresenter>
+      )}
+      {popup === 'suggestResume' && (
+        <OverlayPresenter>
+          <Popup>
+            <Popup.ContentGroup tw="gap-5 py-12">
+              <Popup.Title tw="text-center">선택한 요청의 매물 추천을 다시 받으시겠습니까?</Popup.Title>
+            </Popup.ContentGroup>
+            <Popup.ButtonGroup>
+              <Popup.CancelButton onClick={() => setPopup('none')}>취소</Popup.CancelButton>
+              <Popup.ActionButton onClick={handleResumeSuggest}>확인</Popup.ActionButton>
+            </Popup.ButtonGroup>
+          </Popup>
+        </OverlayPresenter>
+      )}
     </Panel>
   );
 });
