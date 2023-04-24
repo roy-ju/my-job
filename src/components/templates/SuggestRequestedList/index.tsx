fbac3da63c5d324181466b44cf79a427b57e34d6
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-24 13:24:14 +0900

    fix: 나의 추천요청 레이아웃 수정

--- src/components/templates/SuggestRequestedList/index.tsx
+++ src/components/templates/SuggestRequestedList/index.tsx
@@ -7,6 +7,7 @@ import { useCallback } from 'react';
 import tw, { styled } from 'twin.macro';
 
 const ListContainer = styled.div`
+  width: 100%;
   & > div:not(:last-of-type) > button > div {
     ${tw`border-b border-b-gray-300`}
   }
@@ -40,25 +41,29 @@ export default function SuggestRequestedList({
     <div tw="h-full flex flex-col">
       <NavigationHeader>
         <NavigationHeader.Title>나의 추천 요청</NavigationHeader.Title>
-        {listStyle === 'default' && (
-          <NavigationHeader.Button tw="text-b2 underline" onClick={() => onChangeListStyle?.('delete')}>
-            선택삭제
-          </NavigationHeader.Button>
-        )}
-        {listStyle === 'delete' && (
-          <div tw="flex gap-3">
-            <NavigationHeader.Button tw="text-b2 underline" onClick={() => onChangeListStyle?.('default')}>
-              취소
-            </NavigationHeader.Button>
-            <NavigationHeader.Button tw="text-b2 underline" onClick={onClickDelete}>
-              삭제하기
-            </NavigationHeader.Button>
+        {Boolean(list?.length) && (
+          <div>
+            {listStyle === 'default' && (
+              <NavigationHeader.Button tw="text-b2 underline" onClick={() => onChangeListStyle?.('delete')}>
+                선택삭제
+              </NavigationHeader.Button>
+            )}
+            {listStyle === 'delete' && (
+              <div tw="flex gap-3">
+                <NavigationHeader.Button tw="text-b2 underline" onClick={() => onChangeListStyle?.('default')}>
+                  취소
+                </NavigationHeader.Button>
+                <NavigationHeader.Button tw="text-b2 underline" onClick={onClickDelete}>
+                  삭제하기
+                </NavigationHeader.Button>
+              </div>
+            )}
           </div>
         )}
       </NavigationHeader>
-      <div tw="flex flex-col flex-1 min-h-0 overflow-auto">
+      <div tw="flex flex-col flex-1 min-h-0">
         {list?.length !== 0 ? (
-          <div>
+          <div tw="flex flex-col flex-1 min-h-0">
             {listStyle === 'default' && (
               <div tw="pb-5 px-5 pt-1">
                 <Button onClick={onClickSuggestRegional} tw="w-full" variant="secondary">
@@ -67,17 +72,19 @@ export default function SuggestRequestedList({
               </div>
             )}
             {listStyle === 'default' && <div tw="px-5 text-end text-info">추천 그만받기</div>}
-            <ListContainer>
-              {list?.map((item) => (
-                <SuggestRequestedListItem
-                  key={item.suggest_id}
-                  item={item}
-                  onChange={handleListItemCheckedStateChange(item.suggest_id)}
-                  inputType={listStyle === 'delete' ? 'checkbox' : 'switch'}
-                  defaultChecked={listStyle === 'default' ? item.status === SuggestStatus.Stopped : undefined}
-                />
-              ))}
-            </ListContainer>
+            <div tw="flex flex-1 min-h-0 overflow-auto">
+              <ListContainer>
+                {list?.map((item) => (
+                  <SuggestRequestedListItem
+                    key={item.suggest_id}
+                    item={item}
+                    onChange={handleListItemCheckedStateChange(item.suggest_id)}
+                    inputType={listStyle === 'delete' ? 'checkbox' : 'switch'}
+                    checked={listStyle === 'default' ? item.status === SuggestStatus.Stopped : undefined}
+                  />
+                ))}
+              </ListContainer>
+            </div>
           </div>
         ) : (
           <div tw="py-7">
