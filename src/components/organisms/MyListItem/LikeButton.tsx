b317b730144ab4b16a213f9ba6437a29048158c5
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-25 10:54:09 +0900

    fix: 등록한 매물 validate dom issue, 탭 폰트사이즈 수정

--- src/components/organisms/MyListItem/LikeButton.tsx
+++ src/components/organisms/MyListItem/LikeButton.tsx
@@ -3,14 +3,14 @@ import tw from 'twin.macro';
 
 export default function LikeButton({ onLike, isFavorite }: { isFavorite: boolean; onLike?: () => void }) {
   return (
-    <button
-      type="button"
+    <span
+      role="presentation"
       onClick={(e) => {
         e.stopPropagation();
         onLike?.();
       }}
     >
       <Heart css={[tw`text-white stroke-1 stroke-black`, isFavorite && tw`text-red-800 stroke-0`]} />
-    </button>
+    </span>
   );
 }
--- src/components/templates/MyRegisteredListings/index.tsx
+++ src/components/templates/MyRegisteredListings/index.tsx
@@ -45,16 +45,24 @@ export default function MyRegisteredListings({
       </NavigationHeader>
       <Tabs value={tab} onChange={onChangeListingTab}>
         <Tabs.Tab value={1}>
-          등록신청 <span tw="text-gray-1000 font-bold">{myRegisteringListingCount}</span>
+          <span tw="text-b2">
+            등록신청 <span tw="text-gray-1000 font-bold">{myRegisteringListingCount}</span>
+          </span>
         </Tabs.Tab>
         <Tabs.Tab value={2}>
-          거래중 <span tw="text-gray-1000 font-bold">{myActiveListingCount}</span>
+          <span tw="text-b2">
+            거래중 <span tw="text-gray-1000 font-bold">{myActiveListingCount}</span>
+          </span>
         </Tabs.Tab>
         <Tabs.Tab value={3}>
-          거래성사 <span tw="text-gray-1000 font-bold">{myContractCompleteListingCount}</span>
+          <span tw="text-b2">
+            거래성사 <span tw="text-gray-1000 font-bold">{myContractCompleteListingCount}</span>
+          </span>
         </Tabs.Tab>
         <Tabs.Tab value={4}>
-          지난거래 <span tw="text-gray-1000 font-bold">{myCancelledListingCount}</span>
+          <span tw="text-b2">
+            지난거래 <span tw="text-gray-1000 font-bold">{myCancelledListingCount}</span>
+          </span>
         </Tabs.Tab>
         <Tabs.Indicator />
       </Tabs>
