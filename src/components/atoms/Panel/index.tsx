b45888d4f5098756d4c9224a2b3f54e4582eb6cd
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-26 14:37:41 +0900

    feat: animation

--- src/components/atoms/Panel/index.tsx
+++ src/components/atoms/Panel/index.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react';
 import tw from 'twin.macro';
+import { motion } from 'framer-motion';
 
 interface Props {
   width?: string;
@@ -9,9 +10,15 @@ interface Props {
 
 export default function Panel({ width = '380px', zIndex = 1, children }: Props) {
   return (
-    <div tw="h-full bg-white shadow" style={{ width, zIndex }}>
-      {/* <div css={[tw`w-full h-full overflow-x-hidden overflow-y-auto`, { width }]}>{children}</div> */}
+    <motion.div
+      initial={{ x: '-380px', opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ type: 'spring', damping: 30, stiffness: 200 }}
+      layout
+      tw="h-full bg-white shadow"
+      style={{ width, zIndex }}
+    >
       <div css={[tw`w-full h-full overflow-y-auto`, { width }]}>{children}</div>
-    </div>
+    </motion.div>
   );
 }
