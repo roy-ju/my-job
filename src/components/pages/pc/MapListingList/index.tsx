f3f0dec46c3970db189382cd257d0b392d006d6d
joelkim-negocio <joel.kim@negocio.co.kr>
2023-04-28 14:14:27 +0900

    feat: 목록에서 관심설정

--- src/components/pages/pc/MapListingList/index.tsx
+++ src/components/pages/pc/MapListingList/index.tsx
@@ -3,6 +3,9 @@ import { MapListingList } from '@/components/templates';
 import { memo, useCallback } from 'react';
 import { useRouter } from '@/hooks/utils';
 import Routes from '@/router/routes';
+import { addFavorite } from '@/apis/listing/addListingFavroite';
+import { toast } from 'react-toastify';
+import { removeFavorite } from '@/apis/listing/removeListingFavorite';
 import useMapListingList from './useMapListingList';
 
 interface Props {
@@ -22,9 +25,24 @@ export default memo(({ depth, panelWidth }: Props) => {
     [router],
   );
 
+  const onToggleFav = useCallback((id: number, active: boolean) => {
+    if (active) {
+      addFavorite(id);
+      toast.success('관심을 설정했습니다.');
+    } else {
+      removeFavorite(id);
+      toast.success('관심을 해제했습니다.');
+    }
+  }, []);
+
   return (
     <Panel width={panelWidth}>
-      <MapListingList data={data} isLoading={isLoading} onClickListing={onClickListing} />
+      <MapListingList
+        data={data}
+        isLoading={isLoading}
+        onClickListing={onClickListing}
+        onToggleFavorite={onToggleFav}
+      />
     </Panel>
   );
 });
