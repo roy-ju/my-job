9e09033a65c75120145966d388178b7f935b65f3
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-03 15:05:19 +0900

    feat: faq api 구현 및 연동

--- src/components/organisms/ListingDetailSection/Faq.tsx
+++ src/components/organisms/ListingDetailSection/Faq.tsx
@@ -2,13 +2,13 @@ import useAPI_Internal_GetFaqList from '@/apis/internal/getFaq';
 import FaqList from '../FaqList';
 
 export default function Faq() {
-  const { data } = useAPI_Internal_GetFaqList('거래에 참여하고 싶어요.');
+  const { data } = useAPI_Internal_GetFaqList();
 
   return (
     <div>
       <div tw="text-b1 font-bold">거래가이드 & FAQ</div>
       <div tw="mt-5 -mx-5">
-        <FaqList list={data} />
+        <FaqList list={data?.['[집주인] 매물을 등록하고 싶어요.'] ?? []} />
       </div>
     </div>
   );
