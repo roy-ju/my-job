161833d837930c3d9a4cd04a09741578443c6215
Jay Lee <jay.lee@negocio.co.kr>
2023-05-03 16:53:00 +0900

    feat: 지도 매물목록 구분선

--- src/components/templates/MapListingList/index.tsx
+++ src/components/templates/MapListingList/index.tsx
@@ -5,6 +5,8 @@ import { MapSearchListResponse } from '@/apis/map/mapSearchList';
 import { MyListItem } from '@/components/organisms';
 import { AnimatePresence, motion } from 'framer-motion';
 import { DefaultListingImage } from '@/constants/strings';
+import tw from 'twin.macro';
+import React from 'react';
 
 function NoData() {
   return (
@@ -20,6 +22,8 @@ function NoData() {
     </div>
   );
 }
+
+const Divider = tw.div`border-b border-gray-300 mx-5`;
 
 interface Props {
   isLoading?: boolean;
@@ -48,33 +52,35 @@ export default function MapListingList({ data, isLoading, onClickListing, onTogg
           )}
           {!isLoading && Boolean(data?.length) && (
             <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.1 }}>
-              {data?.map((item) => (
-                <MyListItem.Listing
-                  key={item.listing_id}
-                  listingId={item.listing_id}
-                  listingTitle={item.listing_title}
-                  tradeOrDepositPrice={item.trade_or_deposit_price}
-                  monthlyRentFee={item.monthly_rent_fee}
-                  realestateType={item.realestate_type}
-                  jeonyongArea={item.jeonyong_area}
-                  totalFloor={item.total_floor}
-                  quickSale={item.quick_sale ?? false}
-                  isParticipating={item.is_participating}
-                  participantsCount={item.participants_count}
-                  isFavorite={item.is_favorite}
-                  eubmyundong={item.eubmyundong}
-                  labelText={item.label_text}
-                  viewCount={item.view_count}
-                  thumbnailFullPath={item.thumbnail_full_path ?? DefaultListingImage[item.realestate_type]}
-                  buyOrRent={item.buy_or_rent}
-                  floorDescription={item.floor_description}
-                  direction={item.direction}
-                  statusText=""
-                  onClickListingItem={(id) => () => {
-                    onClickListing?.(id);
-                  }}
-                  onToggleListingLike={onToggleFavorite}
-                />
+              {data?.map((item, i) => (
+                <React.Fragment key={item.listing_id}>
+                  {i > 0 && <Divider />}
+                  <MyListItem.Listing
+                    listingId={item.listing_id}
+                    listingTitle={item.listing_title}
+                    tradeOrDepositPrice={item.trade_or_deposit_price}
+                    monthlyRentFee={item.monthly_rent_fee}
+                    realestateType={item.realestate_type}
+                    jeonyongArea={item.jeonyong_area}
+                    totalFloor={item.total_floor}
+                    quickSale={item.quick_sale ?? false}
+                    isParticipating={item.is_participating}
+                    participantsCount={item.participants_count}
+                    isFavorite={item.is_favorite}
+                    eubmyundong={item.eubmyundong}
+                    labelText={item.label_text}
+                    viewCount={item.view_count}
+                    thumbnailFullPath={item.thumbnail_full_path ?? DefaultListingImage[item.realestate_type]}
+                    buyOrRent={item.buy_or_rent}
+                    floorDescription={item.floor_description}
+                    direction={item.direction}
+                    statusText=""
+                    onClickListingItem={(id) => () => {
+                      onClickListing?.(id);
+                    }}
+                    onToggleListingLike={onToggleFavorite}
+                  />
+                </React.Fragment>
               ))}
             </motion.div>
           )}
