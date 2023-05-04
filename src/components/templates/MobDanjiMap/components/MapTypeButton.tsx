5e6a99f325044bdd6a6fcafb314b0997712efb57
roy-ju <roy.ju@negocio.co.kr>
2023-05-04 16:08:38 +0900

    feat: FullScreenMap MapStreet MapTypeButton

--- src/components/templates/MobDanjiMap/components/MapTypeButton.tsx
+++ src/components/templates/MobDanjiMap/components/MapTypeButton.tsx
@@ -39,11 +39,11 @@ export function MapTypeButton({
 
       {type === 'road' && (
         <StyledDiv
-          tw="absolute p-2.5 [z-index: 100] [transform: translate3d(0,0,0)] [box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.14)] [border-radius: 8px] cursor-pointer hover:border-gray-1000 hover:border-b hover:border-l hover:border-r hover:border-t"
+          tw="absolute px-2 py-1 [z-index: 100] [transform: translate3d(0,0,0)] [box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.14)] [border-radius: 8px] cursor-pointer bg-white [font-size: 12px] font-bold hover:border-gray-1000 hover:border-b hover:border-l hover:border-r hover:border-t"
           style={{ bottom, right }}
           onClick={() => danjiMapTypeStore.makeGeneralMap()}
         >
-          지도로 보기
+          지도
         </StyledDiv>
       )}
     </>
