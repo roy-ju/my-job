4d2c4c9077cd005e9cf8ba66c3a9094aa6736ff6
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-04 14:42:01 +0900

    feat: 오픈그래프 이미지

--- src/utils/getHtmlMetas.ts
+++ src/utils/getHtmlMetas.ts
@@ -1,7 +1,9 @@
-// import Routes from '@/router/routes';
+import { BuyOrRent } from '@/constants/enums';
+import Paths from '@/constants/paths';
 import axios from '@/lib/axios';
 import Routes from '@/router/routes';
 import { ParsedUrlQuery } from 'querystring';
+import formatNumberInKorean from './formatNumberInKorean';
 
 export default async function getHtmlMetas(query: ParsedUrlQuery) {
   const targetRoute = query.depth2 ?? query.depth1;
@@ -10,9 +12,20 @@ export default async function getHtmlMetas(query: ParsedUrlQuery) {
     try {
       const { data } = await axios.post('/listing/detail', { listing_id: Number(query.listingID) });
 
+      let description = data?.display_address;
+
+      if (data?.listing?.buy_or_rent === BuyOrRent.Wolsae) {
+        description = `${formatNumberInKorean(data?.trade_or_deposit_price)}/${formatNumberInKorean(
+          data?.monthly_rent_fee,
+        )}, ${data?.display_address}`;
+      } else {
+        description = `${formatNumberInKorean(data?.trade_or_deposit_price)}, ${data?.display_address}`;
+      }
+
       return {
         title: data?.listing?.listing_title,
-        description: data?.display_address,
+        description,
+        ogImagePath: Paths.DEFAULT_OPEN_GRAPH_IMAGE_2,
       };
     } catch (e) {
       return {};
@@ -29,6 +42,7 @@ export default async function getHtmlMetas(query: ParsedUrlQuery) {
       return {
         title: data?.name,
         description: data?.road_name_address ?? data?.jibun_address,
+        ogImagePath: Paths.DEFAULT_OPEN_GRAPH_IMAGE_3,
       };
     } catch (e) {
       return {};
