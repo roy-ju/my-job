f4911e4b2c2393b61a73555b7da360cb94f8c5ba
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-09 10:30:44 +0900

    feat: version info and oss

--- src/components/templates/VersionInfo/index.tsx
+++ src/components/templates/VersionInfo/index.tsx
@@ -11,6 +11,19 @@ export default function VersionInfo({ onClickBack }: Props) {
         {onClickBack && <NavigationHeader.BackButton onClick={onClickBack} />}
         <NavigationHeader.Title>버전정보</NavigationHeader.Title>
       </NavigationHeader>
+      <div tw="flex-1 py-16">
+        <div tw="flex flex-col justify-center items-center">
+          <div
+            tw="w-[60px] h-[60px] rounded-lg bg-gray-300 mb-3 bg-center bg-no-repeat bg-cover"
+            style={{
+              backgroundImage: `url('https://negocio-common.s3.ap-northeast-2.amazonaws.com/appicon.png')`,
+            }}
+          />
+          <div tw="text-h3 font-bold">네고시오</div>
+          <div tw="text-b2 text-gray-700">2.0</div>
+          <div tw="text-b2 mt-7">최신 버전 입니다.</div>
+        </div>
+      </div>
     </div>
   );
 }
