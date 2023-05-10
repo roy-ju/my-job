ff6b5f6f5256e28fc7b9b450407d0d7dd454948b
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-10 15:46:50 +0900

    fix: make listing nullable

--- src/components/pages/mobile/UpdateBiddingForm/useUpdateBiddingForm.ts
+++ src/components/pages/mobile/UpdateBiddingForm/useUpdateBiddingForm.ts
@@ -188,12 +188,12 @@ export default function useUpdateBiddingForm() {
   }, [setNextForm]);
 
   const handleSubmitInterimAmount = useCallback(() => {
-    if (data?.listing.buy_or_rent === BuyOrRent.Buy) {
+    if (data?.listing?.buy_or_rent === BuyOrRent.Buy) {
       setNextForm(Forms.RemainingAmount);
     } else {
       setNextForm(Forms.MoveInDate);
     }
-  }, [setNextForm, data?.listing.buy_or_rent]);
+  }, [setNextForm, data?.listing?.buy_or_rent]);
 
   const handleSubmitRemainingAmount = useCallback(() => {
     setNextForm(Forms.Etc);
@@ -277,7 +277,7 @@ export default function useUpdateBiddingForm() {
     setNextButtonDisabled(false);
 
     const currentForm = forms[forms.length - 1];
-    const buyOrRent = data?.listing.buy_or_rent ?? 0;
+    const buyOrRent = data?.listing?.buy_or_rent ?? 0;
     if (currentForm === Forms.Price) {
       if (type === 1) {
         const priceNum = Number(price) ?? 0;
@@ -336,7 +336,7 @@ export default function useUpdateBiddingForm() {
     type,
     price,
     monthlyRentFee,
-    data?.listing.buy_or_rent,
+    data?.listing?.buy_or_rent,
     canHaveMoreContractAmount,
     contractAmount,
     canHaveMoreInterimAmount,
