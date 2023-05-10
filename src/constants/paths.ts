865dfca2c723ffa758f6e461208c9b4081d7e679
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-10 09:29:18 +0900

    fix: 매물 종류별로 기본 이미지 다르게 설정해주기

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -8,6 +8,12 @@ const Paths = {
     'https://negocio-common.s3.ap-northeast-2.amazonaws.com/user_mobile/dasaedae_default.webp',
   DEFAULT_APARTMENT_IMAGE_LG_PATH:
     'https://negocio-common.s3.ap-northeast-2.amazonaws.com/user_mobile/apartment_default_lg.png',
+  DEFAULT_OFFICETEL_IMAGE_LG_PATH:
+    'https://negocio-common.s3.ap-northeast-2.amazonaws.com/user_mobile/officetel_default_lg.png',
+  DEFAULT_DANDOK_IMAGE_LG_PATH:
+    'https://negocio-common.s3.ap-northeast-2.amazonaws.com/user_mobile/dandok_default_lg.png',
+  DEFAULT_DASAEDAE_IMAGE_LG_PATH:
+    'https://negocio-common.s3.ap-northeast-2.amazonaws.com/user_mobile/dasaedae_default_lg.png',
 
   DEFAULT_OPEN_GRAPH_IMAGE_1: 'https://negocio-common.s3.ap-northeast-2.amazonaws.com/user_share/negocio1.png',
   DEFAULT_OPEN_GRAPH_IMAGE_2: 'https://negocio-common.s3.ap-northeast-2.amazonaws.com/user_share/negocio2.png',
