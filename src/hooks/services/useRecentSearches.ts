0ba427025839280028ba9e56542128ba72bbba5b
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-10 10:54:27 +0900

    feat: pc 지도 최근검색

---
+++ src/hooks/services/useRecentSearches.ts
@@ -0,0 +1,35 @@
+import { useCallback } from 'react';
+import { useLocalStorage } from '../utils';
+
+export interface RecentSearchType {
+  id: string;
+}
+
+export default function useRecentSearches<T extends RecentSearchType>() {
+  const [items, setItems] = useLocalStorage<T[]>('negocio_pc_recent_searches', []);
+
+  const remove = useCallback(
+    (id: string) => {
+      setItems((prev) => prev.filter((item) => item.id !== id));
+    },
+    [setItems],
+  );
+
+  const add = useCallback(
+    (item: T) => {
+      setItems((prev) => [item, ...prev]);
+    },
+    [setItems],
+  );
+
+  const clear = useCallback(() => {
+    setItems([]);
+  }, [setItems]);
+
+  return {
+    items,
+    remove,
+    add,
+    clear,
+  };
+}
