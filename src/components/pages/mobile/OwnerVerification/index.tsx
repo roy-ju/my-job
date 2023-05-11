9fa6a917d429a1ee61fccaf034724604fdab8d66
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-11 17:24:17 +0900

    fix: 매물등록신청 소유자동의

--- src/components/pages/mobile/OwnerVerification/index.tsx
+++ src/components/pages/mobile/OwnerVerification/index.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { useCallback } from 'react';
 import ErrorCodes from '@/constants/error_codes';
 import Routes from '@/router/routes';
+import { OverlayPresenter, Popup } from '@/components/molecules';
 
 export default function OwnerVerification() {
   const router = useRouter();
@@ -14,7 +15,7 @@ export default function OwnerVerification() {
   const { data, isLoading } = useAPI_GetAgreementInfo(loi, token);
 
   const handleVerify = useCallback(() => {
-    router.replace(`/${Routes.EntryMobile}/ov/ci?t=${router.query.t}&loi=${router.query.loi}`);
+    router.push(`/${Routes.EntryMobile}/ov/ci?t=${router.query.t}&loi=${router.query.loi}`);
   }, [router]);
 
   return (
@@ -29,6 +30,18 @@ export default function OwnerVerification() {
           />
         )}
         {data?.error_code === ErrorCodes.ALREADY_VERIFIED && <CompleteTemplate onClickHome={() => router.push('/m')} />}
+        {data?.error_code === ErrorCodes.LISTING_DOES_NOT_EXIST && (
+          <OverlayPresenter>
+            <Popup>
+              <Popup.ContentGroup>
+                <Popup.Title>해당 매물등록신청이 유효하지 않습니다.</Popup.Title>
+              </Popup.ContentGroup>
+              <Popup.ButtonGroup>
+                <Popup.ActionButton onClick={() => router.push(`/${Routes.EntryMobile}`)}>확인</Popup.ActionButton>
+              </Popup.ButtonGroup>
+            </Popup>
+          </OverlayPresenter>
+        )}
       </div>
     </>
   );
