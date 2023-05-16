fdc982fd97325411246359907f9231b17d48182f
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-16 18:16:54 +0900

    feat: add comments

--- src/apis/map/mapSearchList.ts
+++ src/apis/map/mapSearchList.ts
@@ -118,6 +118,7 @@ function getKey(mapToggleValue: number, bounds: MapBounds | null, filter: Filter
     const previousLength = previousPageData?.list?.length ?? 0;
     // const totalCount = previousPageData?.count ?? 0;
 
+    // if (previousPageData && previousLength <= totalCount) return null;
     if (previousPageData && previousLength === 0) return null;
 
     if (listingIDs) {
