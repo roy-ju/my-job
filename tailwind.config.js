0c66c9d686071bebf37ef956a7ad30996e5a0854
roy-ju <jujihong2@gmail.com>
2023-05-17 16:09:33 +0900

    feat: tailwind.config.js update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  future: { hoverOnlyWhenSupported: true },
   theme: {
     extend: {
       fontFamily: {
