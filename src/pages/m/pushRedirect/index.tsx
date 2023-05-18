e63a0391623734174d443e9bb7e01ce257f55cfc
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-18 12:06:57 +0900

    feat: push redirect

--- src/pages/m/pushRedirect/index.tsx
+++ src/pages/m/pushRedirect/index.tsx
@@ -1,3 +1,5 @@
+import { Loading, MobileContainer } from '@/components/atoms';
+import Routes from '@/router/routes';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
@@ -6,10 +8,26 @@ const Page: NextPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    console.log(router.query);
-  }, [router.query]);
+    const { data } = router.query;
+    if (typeof data === 'string') {
+      const parsed = JSON.parse(data as string);
 
-  return <div />;
+      if (parsed.chat_room_id) {
+        router.replace(`/${Routes.EntryMobile}/${Routes.ChatRoom}?chatRoomID=${parsed.chat_room_id}`);
+        return;
+      }
+
+      router.replace(`/${Routes.EntryMobile}`);
+    }
+  }, [router]);
+
+  return (
+    <MobileContainer>
+      <div tw="py-20">
+        <Loading />
+      </div>
+    </MobileContainer>
+  );
 };
 
 export default Page;
