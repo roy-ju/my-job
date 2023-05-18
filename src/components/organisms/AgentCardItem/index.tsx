3112a26be40edfd422808c1218fabca446f6cb1d
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-18 09:20:39 +0900

    fix: 중개사 카드 스타일 수정

--- src/components/organisms/AgentCardItem/index.tsx
+++ src/components/organisms/AgentCardItem/index.tsx
@@ -32,8 +32,8 @@ function AgentCardItem({ children, defaultExpanded = false }: AgentCardItemProps
     });
 
   return (
-    <div tw="rounded-lg border border-gray-300 pt-4 pb-3 h-[234px]" css={[!expanded && tw`h-auto`]}>
-      <div tw="px-4">
+    <div tw="flex flex-col rounded-lg border border-gray-300 pt-4 pb-3 h-[234px]" css={[!expanded && tw`h-auto`]}>
+      <div tw="px-4 flex-1 min-h-0">
         {profileContents}
         {expanded && detailContents}
       </div>
