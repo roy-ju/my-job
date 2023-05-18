7746f5ffd1ab74632021eb295e9ca93053396b58
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-18 09:57:51 +0900

    fix: 매물상세 접근불가 팝업 문구 수정

--- src/components/pages/mobile/ListingDetail/index.tsx
+++ src/components/pages/mobile/ListingDetail/index.tsx
@@ -306,7 +306,11 @@ export default memo(() => {
       <OverlayPresenter>
         <Popup>
           <Popup.ContentGroup tw="py-10">
-            <Popup.Title>유효하지 않은 페이지입니다.</Popup.Title>
+            <Popup.Title>
+              거래가 종료되어
+              <br />
+              매물 상세 정보를 확인할 수 없습니다.
+            </Popup.Title>
           </Popup.ContentGroup>
           <Popup.ButtonGroup>
             <Popup.ActionButton onClick={() => router.back()}>확인</Popup.ActionButton>
