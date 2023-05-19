7d8279df5e441696aba9668e9140d549fd0f6ea2
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-19 11:13:34 +0900

    feat: 제안 취소

--- src/components/pages/mobile/UpdateBiddingForm/index.tsx
+++ src/components/pages/mobile/UpdateBiddingForm/index.tsx
@@ -2,6 +2,7 @@ import { Loading, MobileContainer } from '@/components/atoms';
 import { BiddingForm } from '@/components/templates';
 import { memo } from 'react';
 import { useRouter } from 'next/router';
+import { OverlayPresenter, Popup } from '@/components/molecules';
 import useUpdateBiddingForm from './useUpdateBiddingForm';
 
 export default memo(() => {
@@ -46,6 +47,9 @@ export default memo(() => {
     handleChangeEtcs,
     description,
     handleChangeDescription,
+    handleCancelBidding,
+    popup,
+    setPopup,
   } = useUpdateBiddingForm();
 
   return (
@@ -93,7 +97,21 @@ export default memo(() => {
           onChangeEtcs={handleChangeEtcs}
           description={description}
           onChangeDescription={handleChangeDescription}
+          onClickCancelBidding={() => setPopup('cancelBidding')}
         />
+      )}
+      {popup === 'cancelBidding' && (
+        <OverlayPresenter>
+          <Popup>
+            <Popup.ContentGroup tw="py-10">
+              <Popup.Title>제안을 취소하시겠습니까?</Popup.Title>
+            </Popup.ContentGroup>
+            <Popup.ButtonGroup>
+              <Popup.CancelButton onClick={() => setPopup('none')}>취소</Popup.CancelButton>
+              <Popup.ActionButton onClick={handleCancelBidding}>제안 취소하기</Popup.ActionButton>
+            </Popup.ButtonGroup>
+          </Popup>
+        </OverlayPresenter>
       )}
     </MobileContainer>
   );
