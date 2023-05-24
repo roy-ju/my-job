5914e6f00b522fad87be30002e5cdce2ce703339
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-24 15:32:06 +0900

    fix: ga test

---
+++ src/lib/gtag/index.ts
@@ -0,0 +1,23 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
+
+export function pageview(url: string) {
+  window.gtag('config', GA_TRACKING_ID, { page_path: url });
+}
+
+export function event({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string;
+  category: string;
+  label: string;
+  value: any;
+}) {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value,
+  });
+}
