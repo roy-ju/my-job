86d0ee625f7364980d5e286707a83d534d7db277
joelkim-negocio <joel.kim@negocio.co.kr>
2023-05-24 11:33:19 +0900

    fix: 워딩수정

--- src/components/organisms/ListingDetailSection/RealestateDocument.tsx
+++ src/components/organisms/ListingDetailSection/RealestateDocument.tsx
@@ -100,7 +100,7 @@ export default function RealestateDocument({ data }: Props) {
             </tbody>
           </StyledTable>
         ) : (
-          <div tw="mb-10 text-info text-gray-700">기록사항이 없습니다.</div>
+          <div tw="mb-10 text-info text-gray-700">기록사항 없음</div>
         )}
       </div>
       <div>
@@ -127,7 +127,7 @@ export default function RealestateDocument({ data }: Props) {
             </tbody>
           </StyledTable>
         ) : (
-          <div tw="text-info text-gray-700">기록사항이 없습니다.</div>
+          <div tw="text-info text-gray-700">기록사항 없음</div>
         )}
       </div>
     </div>
