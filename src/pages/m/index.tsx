6817ba9d90e8e19386da8b41cec507dc755aa915
roy-ju <jujihong2@gmail.com>
2023-05-30 17:05:02 +0900

    feat: update

--- src/pages/m/index.tsx
+++ src/pages/m/index.tsx
@@ -1,6 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Home } from '@/components/pages/mobile';
+// import Maintenence from '@/layouts/Maintenence';
 import type { NextPage } from 'next';
+// import dynamic from 'next/dynamic';
+// import Head from 'next/head';
 
 const Page: NextPage = () => <Home />;
+
+// const Maintenence = dynamic(() => import('@/layouts/Maintenence'), { ssr: false });
+
+// const Page: NextPage = () => (
+//   <>
+//     <Head>
+//       <meta name="viewport" content="width=device-width, initial-scale=1" />
+//     </Head>
+//     <Maintenence />
+//   </>
+// );
 
 export default Page;
