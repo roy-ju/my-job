0d2be08cf3b0b8abb8d7bfea2cb7700707fc0343
Jay Lee <jay.lee@negocio.co.kr>
2023-05-31 12:40:07 +0900

    feat: 마케팅 캐러셀

--- src/components/templates/landings/Intro/IntroSectionThree.tsx
+++ src/components/templates/landings/Intro/IntroSectionThree.tsx
@@ -18,21 +18,41 @@ const StyledP = styled.p`
 `;
 
 export default function IntroSectionThree({ recommend }: { recommend?: boolean }) {
-  const [slideStatus, setSlideStatus] = useState('');
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+
+  const cardWidth = 320;
+  const gap = 16;
+  const translateDuration = 500;
+  const carouselX = -currentSlideIndex * (cardWidth + gap);
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (!slideStatus) {
-        setSlideStatus('prev');
-        return;
-      }
-      setSlideStatus(slideStatus === 'prev' ? 'next' : 'prev');
+    const max = 2;
+
+    const intervalId = setInterval(() => {
+      setCurrentSlideIndex((prev) => {
+        if (prev < max) {
+          return prev + 1;
+        }
+        return prev;
+      });
     }, 3000);
-    return () => clearInterval(interval);
-  }, [slideStatus]);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (currentSlideIndex === 2) {
+      setTimeout(() => setCurrentSlideIndex(0), translateDuration);
+    }
+  }, [translateDuration, currentSlideIndex]);
+
+  console.log(currentSlideIndex);
 
   const slide1 = (
     <>
-      <motion.div tw="shrink-0">
+      <motion.div tw="shrink-0 [border-radius: 32px] overflow-hidden">
         <Image
           tw="[box-shadow: -8px 12px 16px rgba(0, 0, 0, 0.15)] [border-radius: 32px] h-full"
           alt=""
@@ -41,7 +61,7 @@ export default function IntroSectionThree({ recommend }: { recommend?: boolean }
           src={MypageImage}
         />
       </motion.div>
-      <motion.div tw="shrink-0">
+      <motion.div tw="shrink-0 [border-radius: 32px] overflow-hidden">
         <Image
           tw="[box-shadow: -8px 12px 16px rgba(0, 0, 0, 0.15)] [border-radius: 32px] h-full"
           alt=""
@@ -50,34 +70,32 @@ export default function IntroSectionThree({ recommend }: { recommend?: boolean }
           src={MypageImage2}
         />
       </motion.div>
-    </>
-  );
-  const slide2 = (
-    <>
-      <motion.div tw="shrink-0">
-        <Image
-          tw="[box-shadow: -8px 12px 16px rgba(0, 0, 0, 0.15)] [border-radius: 32px] h-full"
-          alt=""
-          width={320}
-          height={257}
-          src={MypageImage2}
-        />
-      </motion.div>
-      <motion.div tw="shrink-0">
+
+      <motion.div tw="shrink-0 [border-radius: 32px] overflow-hidden">
         <Image
           tw="[box-shadow: -8px 12px 16px rgba(0, 0, 0, 0.15)] [border-radius: 32px] h-full"
           alt=""
           width={320}
           height={257}
           src={MypageImage}
+        />
+      </motion.div>
+
+      <motion.div tw="shrink-0 [border-radius: 32px] overflow-hidden">
+        <Image
+          tw="[box-shadow: -8px 12px 16px rgba(0, 0, 0, 0.15)] [border-radius: 32px] h-full"
+          alt=""
+          width={320}
+          height={257}
+          src={MypageImage2}
         />
       </motion.div>
     </>
   );
 
   return (
-    <div css={recommend ? tw`bg-white` : tw`bg-nego-100`} tw="overflow-hidden">
-      <section tw="max-w-[1280px] xl:mx-auto relative px-5 py-[60px]  text-center md:text-left md:px-10 md:py-[120px] xl:px-20 md:flex md:flex-col md:flex-wrap md:[height: 1157px] ">
+    <div css={recommend ? tw`bg-white` : tw`bg-nego-100`} tw="overflow-hidden relative">
+      <section tw="max-w-[1280px] xl:mx-auto  px-5 py-[60px]  text-center md:text-left md:py-[120px] md:pl-10  xl:pl-20 md:pr-0 xl:pr-0 md:flex md:flex-col md:flex-wrap md:[height: 1157px] overflow-hidden">
         <div tw="mb-7 md:order-1 md:mb-[60px]">
           <p tw="font-bold text-base md:text-2xl md:leading-[34px] leading-6 text-nego-800 mb-2 md:mb-3">
             #중개사 #매물추천받기
@@ -87,7 +105,7 @@ export default function IntroSectionThree({ recommend }: { recommend?: boolean }
             <br /> 매물을 추천 받으세요
           </h1>
         </div>
-        <div tw="mb-[60px] md:order-3 md:ml-auto">
+        <div tw="mb-[60px] md:order-3 md:ml-auto md:pr-10  xl:pr-20">
           <Image tw="mx-auto" alt="" width={320} height={620} src={PhoneImage} />
         </div>
         <div tw="flex flex-col gap-7 md:gap-10  mb-10 md:mb-0 md:order-2">
@@ -114,8 +132,18 @@ export default function IntroSectionThree({ recommend }: { recommend?: boolean }
             <StyledP>채팅을 통한 추가 협의/방문예약</StyledP>
           </div>
         </div>
-        <div tw="md:order-4 md:ml-auto">
-          <div tw="flex gap-4  mx-auto w-[320px] h-[257px]">{slideStatus === 'next' ? slide2 : slide1}</div>
+        <div tw="md:order-4 overflow-hidden flex">
+          <div tw="mx-auto w-[320px] md:w-[360px] xl:w-[400px] h-[257px] overflow-hidden md:ml-auto md:mr-0">
+            <div
+              tw="flex gap-4"
+              style={{
+                transform: `translateX(calc(${carouselX}px))`,
+                transition: currentSlideIndex === 0 ? '' : `transform ${translateDuration}ms ease-in-out`,
+              }}
+            >
+              {slide1}
+            </div>
+          </div>
         </div>
       </section>
     </div>
