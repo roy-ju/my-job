53d2f09dea5201b1c3509da1b695d7c93330796c
Jay Lee <jay.lee@negocio.co.kr>
2023-05-31 09:41:01 +0900

    feat: 마케팅페이지

--- src/components/templates/landings/Intro/IntroSectionThree.tsx
+++ src/components/templates/landings/Intro/IntroSectionThree.tsx
@@ -94,49 +94,48 @@ export default function IntroSectionThree({ recommend }: { recommend?: boolean }
   );
 
   return (
-    <section
-      css={recommend ? tw`bg-white` : tw`bg-nego-100`}
-      tw="relative px-5 py-[60px]  text-center md:text-left md:px-10 md:py-[120px] xl:px-20 md:flex md:flex-col md:flex-wrap md:[height: 1157px] overflow-hidden"
-    >
-      <div tw="mb-7 md:order-1 md:mb-[60px]">
-        <p tw="font-bold text-base md:text-2xl md:leading-[34px] leading-6 text-nego-800 mb-2 md:mb-3">
-          #중개사 #매물추천받기
-        </p>
-        <h1 tw="font-bold text-2xl md:text-[44px] md:leading-[64px] leading-[34px]">
-          원하는 가격에
-          <br /> 매물을 추천 받으세요
-        </h1>
-      </div>
-      <div tw="mb-[60px] md:order-3 md:ml-auto">
-        <Image tw="mx-auto" alt="" width={320} height={620} src={PhoneImage} />
-      </div>
-      <div tw="flex flex-col gap-7 md:gap-10  mb-10 md:mb-0 md:order-2">
-        <div>
-          <p tw="font-bold text-base md:text-[28px] md:leading-[36px]  leading-6 text-gray-900 mb-2 md:mb-4">
-            Step 1. 추천 요청 발송
+    <div css={recommend ? tw`bg-white` : tw`bg-nego-100`}>
+      <section tw="max-w-[1280px] xl:mx-auto relative px-5 py-[60px]  text-center md:text-left md:px-10 md:py-[120px] xl:px-20 md:flex md:flex-col md:flex-wrap md:[height: 1157px] overflow-hidden">
+        <div tw="mb-7 md:order-1 md:mb-[60px]">
+          <p tw="font-bold text-base md:text-2xl md:leading-[34px] leading-6 text-nego-800 mb-2 md:mb-3">
+            #중개사 #매물추천받기
           </p>
-          <StyledP>매물 추천 받을 지역/단지 선택</StyledP>
-          <StyledP>매물 종류 선택</StyledP>
-          <StyledP>원하는 가격 넣기</StyledP>
+          <h1 tw="font-bold text-2xl md:text-[44px] md:leading-[64px] leading-[34px]">
+            원하는 가격에
+            <br /> 매물을 추천 받으세요
+          </h1>
         </div>
-        <div>
-          <p tw="font-bold text-base md:text-[28px] md:leading-[36px]  leading-6 text-gray-900 mb-2 md:mb-4">
-            Step 2. 가격 제안
-          </p>
-          <StyledP>마이페이지에서 확인</StyledP>
-          <StyledP>매물상세 정보 확인</StyledP>
-          <StyledP>바로 네고 협의 → 중개사와 채팅가능</StyledP>
+        <div tw="mb-[60px] md:order-3 md:ml-auto">
+          <Image tw="mx-auto" alt="" width={320} height={620} src={PhoneImage} />
         </div>
-        <div>
-          <p tw="font-bold text-base md:text-[28px] md:leading-[36px] leading-6 text-gray-900 mb-2 md:mb-4">
-            Step 3. 중개사와 협의
-          </p>
-          <StyledP>채팅을 통한 추가 협의/방문예약</StyledP>
+        <div tw="flex flex-col gap-7 md:gap-10  mb-10 md:mb-0 md:order-2">
+          <div>
+            <p tw="font-bold text-base md:text-[28px] md:leading-[36px]  leading-6 text-gray-900 mb-2 md:mb-4">
+              Step 1. 추천 요청 발송
+            </p>
+            <StyledP>매물 추천 받을 지역/단지 선택</StyledP>
+            <StyledP>매물 종류 선택</StyledP>
+            <StyledP>원하는 가격 넣기</StyledP>
+          </div>
+          <div>
+            <p tw="font-bold text-base md:text-[28px] md:leading-[36px]  leading-6 text-gray-900 mb-2 md:mb-4">
+              Step 2. 가격 제안
+            </p>
+            <StyledP>마이페이지에서 확인</StyledP>
+            <StyledP>매물상세 정보 확인</StyledP>
+            <StyledP>바로 네고 협의 → 중개사와 채팅가능</StyledP>
+          </div>
+          <div>
+            <p tw="font-bold text-base md:text-[28px] md:leading-[36px] leading-6 text-gray-900 mb-2 md:mb-4">
+              Step 3. 중개사와 협의
+            </p>
+            <StyledP>채팅을 통한 추가 협의/방문예약</StyledP>
+          </div>
         </div>
-      </div>
-      <div tw="md:order-4 md:ml-auto">
-        <div tw="flex gap-4  mx-auto w-[320px] h-[257px]">{slideStatus === 'next' ? slide2 : slide1}</div>
-      </div>
-    </section>
+        <div tw="md:order-4 md:ml-auto">
+          <div tw="flex gap-4  mx-auto w-[320px] h-[257px]">{slideStatus === 'next' ? slide2 : slide1}</div>
+        </div>
+      </section>
+    </div>
   );
 }
