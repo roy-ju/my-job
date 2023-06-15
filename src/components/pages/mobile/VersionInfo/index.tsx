bc62ef7e2db7952fad8041bc76ba5c68a228f66f
roy-ju <roy.ju@negocio.co.kr>
2023-06-15 09:33:16 +0900

    feat: versionInfoPage test

--- src/components/pages/mobile/VersionInfo/index.tsx
+++ src/components/pages/mobile/VersionInfo/index.tsx
@@ -1,14 +1,25 @@
 import { MobileContainer } from '@/components/atoms';
 import { VersionInfo } from '@/components/templates';
+import { useIsomorphicLayoutEffect } from '@/hooks/utils';
 import { useRouter } from 'next/router';
-import { memo } from 'react';
+import { memo, useState } from 'react';
 
 export default memo(() => {
   const router = useRouter();
 
+  const [userUsedVersion, setUserUsedVersion] = useState('');
+
+  useIsomorphicLayoutEffect(() => {
+    if (localStorage.getItem('negocio_native_app_version')) {
+      const value = localStorage.getItem('negocio_native_app_version');
+
+      setUserUsedVersion(value || '');
+    }
+  }, []);
+
   return (
     <MobileContainer>
-      <VersionInfo onClickBack={() => router.back()} />
+      <VersionInfo onClickBack={() => router.back()} userUsedVersion={userUsedVersion} />
     </MobileContainer>
   );
 });
