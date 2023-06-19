7f1b5a461cab62945e48a86d6f659bac78d94636
roy-ju <jujihong2@gmail.com>
2023-06-19 12:57:02 +0900

    feat: SEO Optimization

--- src/utils/getHtmlMetas.ts
+++ src/utils/getHtmlMetas.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { BuyOrRent } from '@/constants/enums';
 import Paths from '@/constants/paths';
 import axios from '@/lib/axios';
@@ -26,6 +27,8 @@ export default async function getHtmlMetas(query: ParsedUrlQuery) {
 
       let description = data?.display_address;
 
+      let convertedDescription = '';
+
       if (data?.listing?.buy_or_rent === BuyOrRent.Wolsae) {
         description = `${formatNumberInKorean(data?.trade_or_deposit_price)}/${formatNumberInKorean(
           data?.monthly_rent_fee,
@@ -34,10 +37,34 @@ export default async function getHtmlMetas(query: ParsedUrlQuery) {
         description = `${formatNumberInKorean(data?.trade_or_deposit_price)}, ${data?.display_address}`;
       }
 
+      const convertedTitle =
+        `'${data?.display_address || ''} ${data?.listing?.listing_title}'시세, 실거래가, 호가, 매물, 주변 정보` || '';
+
+      if (data?.listing?.buy_or_rent === BuyOrRent.Wolsae) {
+        convertedDescription = `'${data?.display_address || ''} ${
+          data?.listing?.listing_title
+        } / ${formatNumberInKorean(data?.trade_or_deposit_price)}/${formatNumberInKorean(
+          data?.monthly_rent_fee,
+        )}'의 매물 기본정보, 실거래가/시세, 호가, 매매/전세/월세/원룸/투룸 매물, 주변학군/생활/교통 정보를 보여드립니다.`;
+      } else {
+        convertedDescription = `'${data?.display_address || ''} ${
+          data?.listing?.listing_title
+        } / ${formatNumberInKorean(
+          data?.trade_or_deposit_price,
+        )}'의 매물 기본정보, 실거래가/시세, 호가, 매매/전세/월세/원룸/투룸 매물, 주변 학군/생활/교통 정보를 보여드립니다.`;
+      }
+
       return {
-        title: data?.listing?.listing_title ?? '',
-        description: description ?? '',
+        ogTitle: data?.listing?.listing_title ?? '',
+        ogDescription: description ?? '',
+        title: convertedTitle ?? '',
+        description: convertedDescription ?? '',
         ogImagePath: Paths.DEFAULT_OPEN_GRAPH_IMAGE_2,
+        ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+        ogType: 'website',
+        keyWords: `${data?.listing?.listing_title || ''}, ${
+          process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+        }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
       };
     } catch (e) {
       return defaultMeta;
@@ -51,10 +78,24 @@ export default async function getHtmlMetas(query: ParsedUrlQuery) {
         realestate_type: Number(query.rt),
       });
 
+      const roadAddress = data?.road_name_address ?? data?.jibun_address ?? '';
+      const convertedTitle = `'${roadAddress} ${data.name}'시세, 실거래가, 단지, 매물, 주변 정보`;
+      const convertedDescription =
+        `'${roadAddress || ''} ${
+          data?.name
+        }'단지, 단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.` || '';
+
       return {
-        title: data?.name ?? '',
-        description: data?.road_name_address ?? data?.jibun_address ?? '',
+        title: convertedTitle ?? '',
+        description: convertedDescription ?? '',
+        ogTitle: data?.name ?? '',
+        ogDescription: data?.road_name_address ?? data?.jibun_address ?? '',
         ogImagePath: Paths.DEFAULT_OPEN_GRAPH_IMAGE_3,
+        ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+        ogType: 'website',
+        keyWords: `${data?.name || ''}, ${
+          process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+        }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
       };
     } catch (e) {
       return defaultMeta;
