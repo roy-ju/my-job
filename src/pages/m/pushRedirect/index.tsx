2bdde0dba579c3f21446298a1bc76ebaa2692612
roy-ju <roy.ju@negocio.co.kr>
2023-07-18 17:22:53 +0900

    feat: testPushRedirect

--- src/pages/m/pushRedirect/index.tsx
+++ src/pages/m/pushRedirect/index.tsx
@@ -1,26 +1,35 @@
 import { Loading, MobileContainer } from '@/components/atoms';
-import Routes from '@/router/routes';
+// import Routes from '@/router/routes';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 // 네이티브 앱에서 푸시알림 클릭후 리디렉트 핸들링
 const Page: NextPage = () => {
   const router = useRouter();
 
+  const [text, setText] = useState('');
+
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const url = window.location.href;
-      router.replace(url);
-    } else {
-      router.replace(`/${Routes.EntryMobile}`);
+    const { data } = router.query;
+    if (typeof data === 'string') {
+      const parsed = JSON.parse(data as string);
+
+      if (parsed?.url && typeof parsed.url === 'string') {
+        setText(parsed?.url || '');
+      }
     }
+
+    // else {
+    //   router.replace(`/${Routes.EntryMobile}`);
+    // }
   }, [router]);
 
   return (
     <MobileContainer>
       <div tw="py-20">
         <Loading />
+        <p>{text}</p>
       </div>
     </MobileContainer>
   );
