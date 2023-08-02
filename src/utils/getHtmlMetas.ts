2bc13e651ee57d6a4f189a467b8f91a3cdbf7019
roy-ju <jujihong2@gmail.com>
2023-08-02 14:43:42 +0900

    feat: update getHtmlMetas and map

--- src/utils/getHtmlMetas.ts
+++ src/utils/getHtmlMetas.ts
@@ -16,6 +16,261 @@ export default async function getHtmlMetas(query: ParsedUrlQuery) {
 
   const targetRoute = query.depth2 ?? query.depth1;
 
+  if (targetRoute === Routes.DanjiRecommendation || targetRoute === Routes.DanjiRecommendationSuccess) {
+    return {
+      title: `단지추천 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '단지추천',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (
+    targetRoute === Routes.SuggestDetail ||
+    targetRoute === Routes.SuggestReceivedList ||
+    targetRoute === Routes.SuggestRegionalForm ||
+    targetRoute === Routes.SuggestRegionalSuccess ||
+    targetRoute === Routes.SuggestRegionalSummary ||
+    targetRoute === Routes.SuggestRequestedList
+  ) {
+    return {
+      title: `매물추천 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '매물추천',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (
+    targetRoute === Routes.My ||
+    targetRoute === Routes.MyAddress ||
+    targetRoute === Routes.MyAddressDetail ||
+    targetRoute === Routes.MyAddressVerifying ||
+    targetRoute === Routes.MyDetail
+  ) {
+    return {
+      title: `마이페이지 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '마이페이지',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (targetRoute === Routes.MyRegisteredListingList) {
+    return {
+      title: `나의 매물 현황 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '마이페이지',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (targetRoute === Routes.MyParticipatingListings) {
+    return {
+      title: `나의 거래 현황 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '마이페이지',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (targetRoute === Routes.Login) {
+    return {
+      title: `로그인 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '로그인',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (targetRoute === Routes.Deregister) {
+    return {
+      title: `탈퇴하기 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '탈퇴하기',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (targetRoute === Routes.MyFavoriteList) {
+    return {
+      title: `관심목록 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '관심목록',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (
+    targetRoute === Routes.Chat ||
+    targetRoute === Routes.ChatRoom ||
+    targetRoute === Routes.ChatRoomList ||
+    targetRoute === Routes.ChatRoomReport
+  ) {
+    return {
+      title: `채팅 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '관심목록',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `부동산 법률 상담, ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (
+    targetRoute === Routes.ListingCreateAddress ||
+    targetRoute === Routes.ListingCreateAddressDetail ||
+    targetRoute === Routes.ListingCreateForm ||
+    targetRoute === Routes.ListingCreateSummary ||
+    targetRoute === Routes.ListingCreateUpdateAddress ||
+    targetRoute === Routes.ListingCreateUpdateAddressDetail ||
+    targetRoute === Routes.ListingCreateSummary ||
+    targetRoute === Routes.ListingCreateResult
+  ) {
+    return {
+      title: `매물등록 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '매물등록',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (targetRoute === Routes.NotificationList || targetRoute === Routes.NotificationSettings) {
+    return {
+      title: `알림 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '알림',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `부동산 법률 상담, ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
+  if (targetRoute === Routes.NoticeDetail || targetRoute === Routes.NoticeList) {
+    return {
+      title: `공지사항 | ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test'
+          ? '(TEST) 부동산 가격협상 앱 네고시오'
+          : '부동산 가격협상 앱 네고시오'
+      }`,
+      description: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogTitle: '공지사항',
+      ogDescription: '단지 기본정보, 실거래가/시세, 호가, 매물, 주변학군/생활/교통 정보를 보여드립니다.',
+      ogImagePath: AppConfig.ogImagePath,
+      ogSiteName: process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오',
+      ogType: 'website',
+      keyWords: `부동산 법률 상담, ${
+        process.env.NEXT_PUBLIC_APP_ENVIRONMENT === 'test' ? '네고시오(TEST)' : '네고시오'
+      }, 부동산, 아파트 실거래가, 아파트 시세, 오피스텔 실거래가, 오피스텔 시세, 실거래가, 시세, 호가, 단지, 매매, 전세, 월세, 원룸, 투룸, 교통, 환경, 주변`,
+    };
+  }
+
   if (targetRoute === Routes.ListingDetail) {
     try {
       const { data: statusData } = await axios.post('/listing/status', { listing_id: Number(query.listingID) });
