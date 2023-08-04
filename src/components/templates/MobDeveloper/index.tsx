cfb3350bcdc5056077aa627c2c24da8a92430ced
Jay Lee <jay.lee@negocio.co.kr>
2023-08-04 14:45:28 +0900

    fix: remove mobile localstorage logic

--- src/components/templates/MobDeveloper/index.tsx
+++ src/components/templates/MobDeveloper/index.tsx
@@ -5,7 +5,6 @@ import { useControlled } from '@/hooks/utils';
 import getCurrentPosition from '@/utils/getCurrentPosition';
 import { useCallback } from 'react';
 import { toast } from 'react-toastify';
-import Keys from '@/constants/storage_keys';
 
 interface Props {
   userNickname?: string;
@@ -54,11 +53,6 @@ export default function MobDeveloper({
             </Dropdown.Option>
           ))}
         </Dropdown>
-        {localStorage.getItem(Keys.ACCESS_TOKEN) && (
-          <TextField variant="outlined">
-            <TextField.TextArea disabled label="액세스 토큰" value={localStorage.getItem(Keys.ACCESS_TOKEN) ?? ''} />
-          </TextField>
-        )}
         <Button
           onClick={() => {
             getCurrentPosition(
