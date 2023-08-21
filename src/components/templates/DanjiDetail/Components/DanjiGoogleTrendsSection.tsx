e475993759be82c2b444b584d15d65f5528a250f
roy-ju <roy.ju@negocio.co.kr>
2023-08-21 14:19:42 +0900

    feat: update Logic

---
+++ src/components/templates/DanjiDetail/Components/DanjiGoogleTrendsSection.tsx
@@ -0,0 +1,55 @@
+import { useIsomorphicLayoutEffect } from '@/hooks/utils';
+
+function DanjiGoogleTrendsSection({ keyword }: { keyword?: string }) {
+  console.log(window);
+
+  useIsomorphicLayoutEffect(() => {
+    function gtrendsQueries() {
+      if (!keyword) return;
+
+      window?.trends.embed.renderExploreWidgetTo(
+        document.getElementById('googleRelatedQueryWidget'),
+        'RELATED_QUERIES',
+        {
+          comparisonItem: [{ keyword, geo: '', time: 'today 12-m' }],
+          category: 0,
+          property: '',
+        },
+        {
+          exploreQuery: 'q=%ED%97%AC%EB%A6%AC%98%EC%95%88&date=today 12-m',
+          guestPath: 'https://trends.google.com:443/trends/embed/',
+        },
+      );
+    }
+
+    function gtrendsTimeseries() {
+      if (!keyword) return;
+
+      window?.trends.embed.renderExploreWidgetTo(
+        document.getElementById('googleTimeseriesWidget'),
+        'TIMESERIES',
+        {
+          comparisonItem: [{ keyword, geo: '', time: 'today 12-m' }],
+          category: 0,
+          property: '',
+        },
+        {
+          exploreQuery: 'q=%ED%97%AC%EB%A6%AC%98%EC%95%88&date=today 12-m',
+          guestPath: 'https://trends.google.com:443/trends/embed/',
+        },
+      );
+    }
+
+    gtrendsQueries();
+    gtrendsTimeseries();
+  }, [keyword]);
+
+  return (
+    <>
+      <div id="googleTimeseriesWidget" />
+      <div id="googleRelatedQueryWidget" />
+    </>
+  );
+}
+
+export default DanjiGoogleTrendsSection;
