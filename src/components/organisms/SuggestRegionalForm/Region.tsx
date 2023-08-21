585e634a0788b59a864c357c2dc95c19d2f5dbb7
Jay Lee <jay.lee@negocio.co.kr>
2023-08-21 18:16:31 +0900

    chore: 지역추천 문구 수정

--- src/components/organisms/SuggestRegionalForm/Region.tsx
+++ src/components/organisms/SuggestRegionalForm/Region.tsx
@@ -9,7 +9,7 @@ export default function Region({ region, onClickOpenRegionList }: RegionProps) {
   return (
     <div>
       <div tw="mb-4">
-        <div tw="font-bold mb-1">어느 지역 매물을 추천받고 싶은지 선택해 주세요</div>
+        <div tw="font-bold mb-1">추천 받고 싶은 위치를 선택해 주세요.</div>
         <div tw="text-info text-gray-700">추천은 법정동을 기준으로 합니다.</div>
       </div>
 
