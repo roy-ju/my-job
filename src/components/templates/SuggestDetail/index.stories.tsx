2c0668a237b394a8c0d2d0092decf391b5cd41b8
roy-ju <roy.ju@negocio.co.kr>
2023-08-29 16:41:50 +0900

    feat: tempSuggest

--- src/components/templates/SuggestDetail/index.stories.tsx
+++ src/components/templates/SuggestDetail/index.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Panel } from '@/components/atoms';
 import SuggestDetail from '.';
