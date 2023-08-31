fd452bcd2bd61c99df68bdba800c555a94fb68f3
roy-ju <roy.ju@negocio.co.kr>
2023-08-31 18:20:29 +0900

    feat: updateTempSuggest

--- src/components/templates/SuggestUpdate/index.tsx
+++ src/components/templates/SuggestUpdate/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NavigationHeader } from '@/components/molecules';
 import React from 'react';
 import { DanjiOrRegionalType } from '@/constants/enums';
