1ae313154b35fa03fbd7a86a667ab05f4b2a7649
Jay Lee <jay.lee@negocio.co.kr>
2023-09-11 16:09:54 +0900

    feat: SuggestRequestedList

--- src/components/pages/pc/SuggestRequestedList/index.tsx
+++ src/components/pages/pc/SuggestRequestedList/index.tsx
@@ -1,13 +1,9 @@
-import { deleteSuggests } from '@/apis/suggest/deleteSuggests';
 import useAPI_GetMySuggestList from '@/apis/suggest/getMySuggestList';
 import { Loading, Panel } from '@/components/atoms';
-import { OverlayPresenter, Popup } from '@/components/molecules';
 import { SuggestRequestedList } from '@/components/templates';
 import { useRouter } from '@/hooks/utils';
 import Routes from '@/router/routes';
-import { memo, useCallback, useState } from 'react';
-import { toast } from 'react-toastify';
-import { mutate as otherMutate } from 'swr';
+import { memo, useCallback } from 'react';
 
 interface Props {
   depth: number;
@@ -17,16 +13,14 @@ interface Props {
 export default memo(({ panelWidth, depth }: Props) => {
   const router = useRouter(depth);
 
-  const [popup, setPopup] = useState('none');
-
-  const { data, isLoading, mutate, increamentPageNumber } = useAPI_GetMySuggestList();
-
-  const [itemsToDelete, setItemsToDelete] = useState<Record<number, boolean>>({});
-
-  const [listStyle, setListStyle] = useState<'default' | 'delete'>('default');
+  const { data, isLoading, increamentPageNumber } = useAPI_GetMySuggestList();
 
   const handleClickRecommendationForm = useCallback(() => {
-    router.replace(Routes.RecommendationForm);
+    router.replace(Routes.RecommendationForm, {
+      searchParams: {
+        back: router.asPath,
+      },
+    });
   }, [router]);
 
   const handleClickSuggestItem = useCallback(
@@ -38,40 +32,6 @@ export default memo(({ panelWidth, depth }: Props) => {
       });
     },
     [router],
-  );
-
-  const handleChangeListStyle = useCallback((style: 'default' | 'delete') => {
-    setItemsToDelete({});
-    setListStyle(style);
-  }, []);
-
-  const handleClickDelete = useCallback(async () => {
-    setPopup('delete');
-  }, []);
-
-  const handleDeleteConfirm = useCallback(async () => {
-    const items = Object.entries(itemsToDelete)
-      .filter(([_, checked]) => checked)
-      .map(([id]) => Number(id));
-    if (items) {
-      setPopup('none');
-      await deleteSuggests(items);
-      await mutate();
-      toast.success('추천 요청을 삭제했습니다.', { toastId: 'success_delete' });
-      otherMutate('/my/dashboard/info');
-    }
-  }, [itemsToDelete, mutate]);
-
-  const handleChangeSuggestChecked = useCallback(
-    (id: number, checked: boolean) => {
-      if (listStyle === 'delete') {
-        setItemsToDelete((prev) => ({
-          ...prev,
-          [id]: checked,
-        }));
-      }
-    },
-    [listStyle],
   );
 
   if (isLoading) {
@@ -87,32 +47,11 @@ export default memo(({ panelWidth, depth }: Props) => {
   return (
     <Panel width={panelWidth}>
       <SuggestRequestedList
-        listStyle={listStyle}
         list={data}
         onClickRecommendationForm={handleClickRecommendationForm}
         onClickSuggestItem={handleClickSuggestItem}
-        onClickDelete={handleClickDelete}
-        onChangeListStyle={handleChangeListStyle}
-        onChangeSuggestChecked={handleChangeSuggestChecked}
         onNext={increamentPageNumber}
       />
-      {popup === 'delete' && (
-        <OverlayPresenter>
-          <Popup>
-            <Popup.ContentGroup tw="gap-5 py-12">
-              <Popup.Title tw="text-center">
-                선택한 항목을 목록에서
-                <br />
-                삭제하시겠습니까?
-              </Popup.Title>
-            </Popup.ContentGroup>
-            <Popup.ButtonGroup>
-              <Popup.CancelButton onClick={() => setPopup('none')}>취소</Popup.CancelButton>
-              <Popup.ActionButton onClick={handleDeleteConfirm}>확인</Popup.ActionButton>
-            </Popup.ButtonGroup>
-          </Popup>
-        </OverlayPresenter>
-      )}
     </Panel>
   );
 });
--- src/components/templates/SuggestRequestedList/index.tsx
+++ src/components/templates/SuggestRequestedList/index.tsx
@@ -1,11 +1,8 @@
 import { GetMySuggestListResponse } from '@/apis/suggest/getMySuggestList';
 import { Button, InfiniteScroll } from '@/components/atoms';
-import { NavigationHeader } from '@/components/molecules';
-import { SuggestRequestedListItem, SuggestRequestedListNoData } from '@/components/organisms';
-import { SuggestStatus } from '@/constants/enums';
-import { useCallback } from 'react';
+import { NavigationHeader, NoDataUI } from '@/components/molecules';
+import { SuggestRequestedListItem } from '@/components/organisms';
 import tw, { styled } from 'twin.macro';
-import { checkPlatform } from '@/utils/checkPlatform';
 
 const ListContainer = styled(InfiniteScroll)`
   width: 100%;
@@ -16,11 +13,7 @@ const ListContainer = styled(InfiniteScroll)`
 
 interface Props {
   list?: GetMySuggestListResponse['list'];
-  listStyle?: 'default' | 'delete';
   onClickRecommendationForm?: () => void;
-  onClickDelete?: () => void;
-  onChangeListStyle?: (style: 'default' | 'delete') => void;
-  onChangeSuggestChecked?: (id: number, checked: boolean) => void;
   onClickSuggestItem?: (id: number) => void;
   onNext?: () => void;
   onClickBack?: () => void;
@@ -28,76 +21,44 @@ interface Props {
 
 export default function SuggestRequestedList({
   list,
-  listStyle = 'default',
   onClickRecommendationForm,
-  onClickDelete,
-  onChangeListStyle,
-  onChangeSuggestChecked,
   onClickSuggestItem,
   onNext,
   onClickBack,
 }: Props) {
-  const handleListItemCheckedStateChange = useCallback(
-    (id: number) => (checked: boolean) => {
-      onChangeSuggestChecked?.(id, checked);
-    },
-    [onChangeSuggestChecked],
-  );
-
   return (
     <div tw="h-full flex flex-col">
       <NavigationHeader>
         {onClickBack && <NavigationHeader.BackButton onClick={onClickBack} />}
-        <NavigationHeader.Title>중개사가 추천한 매물</NavigationHeader.Title>
-        {Boolean(list?.length) && (
-          <div>
-            {listStyle === 'default' && (
-              <NavigationHeader.Button tw="text-b2 underline" onClick={() => onChangeListStyle?.('delete')}>
-                선택삭제
-              </NavigationHeader.Button>
-            )}
-            {listStyle === 'delete' && (
-              <div tw="flex gap-3">
-                <NavigationHeader.Button tw="text-b2 underline" onClick={() => onChangeListStyle?.('default')}>
-                  취소
-                </NavigationHeader.Button>
-                <NavigationHeader.Button tw="text-b2 underline" onClick={onClickDelete}>
-                  삭제하기
-                </NavigationHeader.Button>
-              </div>
-            )}
-          </div>
-        )}
+        <NavigationHeader.Title>내가 등록한 구하기</NavigationHeader.Title>
       </NavigationHeader>
       <div tw="flex flex-col flex-1 min-h-0">
         {list?.length !== 0 ? (
           <div tw="flex flex-col flex-1 min-h-0">
-            {checkPlatform() !== 'pc' && listStyle === 'default' && (
-              <div tw="p-5">
-                <Button onClick={onClickRecommendationForm} tw="w-full" variant="secondary">
-                  새로운 매물 추천 받아보기
-                </Button>
-              </div>
-            )}
+            <div tw="p-5">
+              <Button onClick={onClickRecommendationForm} tw="w-full" variant="secondary">
+                새로운 매물 구하기
+              </Button>
+            </div>
             <div tw="flex flex-1 min-h-0 overflow-auto">
               <ListContainer onNext={onNext}>
                 {list?.map((item) => (
                   <SuggestRequestedListItem
                     key={item.suggest_id}
                     item={item}
-                    onChange={handleListItemCheckedStateChange(item.suggest_id)}
                     onClick={() => onClickSuggestItem?.(item.suggest_id)}
-                    inputType={listStyle === 'delete' ? 'checkbox' : 'none'}
-                    checked={listStyle === 'default' ? item.status === SuggestStatus.Active : undefined}
                   />
                 ))}
               </ListContainer>
             </div>
           </div>
         ) : (
-          <div tw="py-7">
-            <SuggestRequestedListNoData onClick={onClickRecommendationForm} />
-          </div>
+          <NoDataUI
+            title="구하는 글이 없습니다."
+            body="구하기 글을 작성하고 원하는 곳의 매물을 추천받아보세요."
+            buttonText="새로운 매물 구하기"
+            onClick={onClickRecommendationForm}
+          />
         )}
       </div>
     </div>
