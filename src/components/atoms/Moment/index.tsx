111ccada496646324da7d65f4711941cbb04db3c
Jay Lee <jay.lee@negocio.co.kr>
2023-09-13 12:16:57 +0900

    chore: 공통 timestamp 포맷

--- src/components/atoms/Moment/index.tsx
+++ src/components/atoms/Moment/index.tsx
@@ -2,6 +2,7 @@ import moment from 'moment';
 import 'moment/locale/ko';
 
 import React, { HTMLProps } from 'react';
+import { formatCreatedTime } from '@/utils/formatLastMessageTime';
 
 const DAY_IN_MINUTE = 1440;
 
@@ -18,27 +19,34 @@ export default React.memo(({ format, children, ...spanProps }: Props) => {
   const date = moment(children);
   const diffMinutesFromNow = moment().diff(date, 'minutes');
 
+  if (children && !format) return <span {...spanProps}>{formatCreatedTime(children)}</span>;
+
   let contents = '';
 
-  if (format === 'calendar') {
-    contents = date.locale('ko').calendar();
-  } else if (format === 'relative') {
-    if (diffMinutesFromNow < 1) {
-      contents = '조금 전';
-    } else if (diffMinutesFromNow < DAY_IN_MINUTE) {
-      contents = date.fromNow();
-    } else {
-      contents = date.format('YYYY.MM.DD');
-    }
-  } else if (format === 'message') {
-    if (diffMinutesFromNow < 1) contents = '조금 전';
-    else if (diffMinutesFromNow < DAY_IN_MINUTE) {
-      contents = date.locale('ko').format('a hh:mm');
-    } else {
-      contents = date.locale('ko').format('YYYY.MM.DD');
-    }
-  } else {
-    contents = date.locale('ko').format(format);
+  switch (format) {
+    case 'calendar':
+      contents = date.locale('ko').calendar();
+      break;
+    case 'relative':
+      if (diffMinutesFromNow < 1) {
+        contents = '조금 전';
+      } else if (diffMinutesFromNow < DAY_IN_MINUTE) {
+        contents = date.fromNow();
+      } else {
+        contents = date.format('YYYY.MM.DD');
+      }
+      break;
+    case 'message':
+      if (diffMinutesFromNow < 1) contents = '조금 전';
+      else if (diffMinutesFromNow < DAY_IN_MINUTE) {
+        contents = date.locale('ko').format('a hh:mm');
+      } else {
+        contents = date.locale('ko').format('YYYY.MM.DD');
+      }
+      break;
+    default:
+      contents = date.locale('ko').format(format);
+      break;
   }
 
   return <span {...spanProps}>{contents}</span>;
