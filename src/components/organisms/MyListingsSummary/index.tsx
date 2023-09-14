89f5e239516bc82294f95361883971246a3302c1
Jay Lee <jay.lee@negocio.co.kr>
2023-09-14 17:22:03 +0900

    feat: 마이페이지 디자인 변경

--- src/components/organisms/MyListingsSummary/index.tsx
+++ src/components/organisms/MyListingsSummary/index.tsx
@@ -1,8 +1,13 @@
 import { Button } from '@/components/atoms';
 import ChevronLeftIcon from '@/assets/icons/chevron_left_24.svg';
-import HouseIcon from '@/assets/icons/house_28.svg';
 import { GetDashboardInfoResponse } from '@/apis/my/getDashboardInfo';
-
+import SaveIcon from '@/assets/icons/my_save_24.svg';
+import RecommendationIcon from '@/assets/icons/my_recommendation_24.svg';
+import SaleIcon from '@/assets/icons/my_sale_24.svg';
+import HomeIcon from '@/assets/icons/my_home_24.svg';
+import ChevronIcon from '@/assets/icons/my_chevron_16.svg';
+import OfferIcon from '@/assets/icons/my_offer_24.svg';
+// Save, Recommendation, Sale, Offer, My-Home
 export interface MyListingsSummaryProps {
   dashboardInfo?: GetDashboardInfoResponse | null;
   onClickMyAddress?: () => void;
@@ -28,19 +33,33 @@ export default function MyListingsSummary({
       <div tw="flex flex-col gap-2 mb-6">
         <button
           onClick={onClickRequestedSuggests}
-          tw="rounded-lg bg-nego-100 text-gray-1000 h-12 px-5 flex justify-between items-center"
+          tw="rounded-lg bg-gray-100 text-gray-1000 h-12 px-5 flex justify-between items-center"
           type="button"
         >
-          <span tw="text-info">내가 등록한 구하기</span>
-          <span tw="text-b1 font-bold">{dashboardInfo?.suggest_sent_count}</span>
+          <div tw="flex gap-2 items-center">
+            <SaveIcon /> <span tw="text-b2 text-gray-1000">내가 등록한 구하기</span>
+          </div>
+          <div tw="flex gap-1 items-center">
+            <span tw="text-b1 font-bold text-nego-1000 min-w-[12px] text-center">
+              {dashboardInfo?.suggest_sent_count}
+            </span>
+            <ChevronIcon tw="mb-[2px]" />
+          </div>
         </button>
         <button
           onClick={onClickSuggestRecommendedList}
           tw="rounded-lg bg-gray-100 text-gray-1000 h-12 px-5 flex justify-between items-center"
           type="button"
         >
-          <span tw="text-info">타인 구하기에 대한 나의 추천</span>
-          <span tw="text-b1 font-bold">{dashboardInfo?.suggest_recommended_count}</span>
+          <div tw="flex gap-2 items-center">
+            <RecommendationIcon /> <span tw="text-b2 text-gray-1000">타인 구하기에 대한 나의 추천</span>
+          </div>
+          <div tw="flex gap-1 items-center">
+            <span tw="text-b1 font-bold text-nego-1000 min-w-[12px] text-center">
+              {dashboardInfo?.suggest_recommended_count}
+            </span>
+            <ChevronIcon tw="mb-[2px]" />
+          </div>
         </button>
       </div>
 
@@ -88,15 +107,20 @@ export default function MyListingsSummary({
               onClickMyRegisteredListings?.(firstPriorityItem.tab);
             }
           }}
-          tw="rounded-lg bg-nego-100 text-gray-1000 h-12 px-5 flex justify-between items-center"
+          tw="rounded-lg bg-gray-100 text-gray-1000 h-12 px-5 flex justify-between items-center"
           type="button"
         >
-          <span tw="text-info">내가 등록한 매물</span>
-          <span tw="text-b1 font-bold">
-            {(dashboardInfo?.my_registering_listing_count ?? 0) +
-              (dashboardInfo?.my_active_listing_count ?? 0) +
-              (dashboardInfo?.my_contract_complete_listing_count ?? 0)}
-          </span>
+          <div tw="flex gap-2 items-center">
+            <SaleIcon /> <span tw="text-b2 text-gray-1000">내가 등록한 매물</span>
+          </div>
+          <div tw="flex gap-1 items-center">
+            <span tw="text-b1 font-bold text-nego-1000 min-w-[12px] text-center">
+              {(dashboardInfo?.my_registering_listing_count ?? 0) +
+                (dashboardInfo?.my_active_listing_count ?? 0) +
+                (dashboardInfo?.my_contract_complete_listing_count ?? 0)}
+            </span>
+            <ChevronIcon tw="mb-[2px]" />
+          </div>
         </button>
         <button
           onClick={() => {
@@ -143,19 +167,25 @@ export default function MyListingsSummary({
           tw="rounded-lg bg-gray-100 text-gray-1000 h-12 px-5 flex justify-between items-center"
           type="button"
         >
-          <span tw="text-info">타인 매물에 대한 나의 가격제안</span>
-          <span tw="text-b1 font-bold">
-            {(dashboardInfo?.bidding_submitted_count ?? 0) +
-              (dashboardInfo?.bidding_accepted_count ?? 0) +
-              (dashboardInfo?.bidding_pre_contract_complete_count ?? 0) +
-              (dashboardInfo?.bidding_past_count ?? 0)}
-          </span>
+          <div tw="flex gap-2">
+            <OfferIcon />
+            <span tw="text-b2 text-gray-1000">타인 매물에 대한 나의 가격제안</span>
+          </div>
+          <div tw="flex gap-1 items-center">
+            <span tw="text-b1 font-bold text-nego-1000 min-w-[12px] text-center">
+              {(dashboardInfo?.bidding_submitted_count ?? 0) +
+                (dashboardInfo?.bidding_accepted_count ?? 0) +
+                (dashboardInfo?.bidding_pre_contract_complete_count ?? 0) +
+                (dashboardInfo?.bidding_past_count ?? 0)}
+            </span>
+            <ChevronIcon tw="mb-[2px]" />
+          </div>
         </button>
       </div>
 
-      <Button variant="ghost" tw="w-full border border-nego-800 h-[60px] hover:bg-nego-100" onClick={onClickMyAddress}>
+      <Button variant="ghost" tw="w-full border border-gray-300 h-[60px]" onClick={onClickMyAddress}>
         <div tw="text-start w-full h-full flex items-center">
-          <HouseIcon tw="mr-3" />
+          <HomeIcon tw="mr-3" />
           <div tw="flex-1 flex flex-col gap-0.5">
             <div tw="text-b2 leading-4 font-bold">주소 등록 ・ 집주인 인증</div>
             <div tw="text-info leading-4.5 text-gray-700">간편하게 주소등록하고, 집주인 인증받으세요.</div>
