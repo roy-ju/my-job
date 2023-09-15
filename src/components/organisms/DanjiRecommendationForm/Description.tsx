32defcf7525fb71ea06f755ec02bc5bd3b0fbd46
Jay Lee <jay.lee@negocio.co.kr>
2023-09-15 15:33:42 +0900

    feat: 단지/지역 추천 요청 style

--- src/components/organisms/DanjiRecommendationForm/Description.tsx
+++ src/components/organisms/DanjiRecommendationForm/Description.tsx
@@ -1,6 +1,7 @@
 import { TextField } from '@/components/molecules';
 import { useControlled } from '@/hooks/utils';
 import { useCallback, ChangeEventHandler } from 'react';
+import { BuyOrRent } from '@/constants/enums';
 
 export interface DescriptionProps {
   description?: string;
@@ -8,7 +9,11 @@ export interface DescriptionProps {
   buyOrRent?: number;
 }
 
-export default function Description({ description: descriptionProp, onChangeDescription }: DescriptionProps) {
+export default function Description({
+  description: descriptionProp,
+  onChangeDescription,
+  buyOrRent,
+}: DescriptionProps) {
   const [description, setDescription] = useControlled({
     controlled: descriptionProp,
     default: '',
@@ -27,14 +32,18 @@ export default function Description({ description: descriptionProp, onChangeDesc
 
   return (
     <div tw="border-t-gray-300">
-      <div tw="font-bold mb-1">네고를 위한 추가 조건이 있다면 알려주세요. (선택)</div>
-      <div tw="text-info text-gray-700 mb-4">자세히 알려주실수록 거래가능성이 높아져요.</div>
+      <div tw="font-bold mb-1">찾는 집의 종류, 거래조건 등을 알려주세요. (선택)</div>
+      <div tw="text-info text-gray-700 mb-4">원하는 조건을 자세히 알려주실수록 제안받을 매물이 많아져요.</div>
       <TextField variant="outlined" size="medium">
         <TextField.TextArea
           value={description}
           onChange={handleChangeDescription}
           tw="min-h-[98px] placeholder:[font-size: 14px] placeholder:[line-height: 22px] py-4"
-          placeholder="원하는 학군, 지하철역에서 거리, 세대수, 방향, 층수, 주차&#13;&#10;등 희망조건을 상세하게 적어주세요."
+          placeholder={
+            buyOrRent === BuyOrRent.Buy
+              ? '원하는 층, 방향, 대금지급 조건 및 네고를 위해 제시할 내용 등도 상세하게 적어주세요.'
+              : '원하는 학군, 전철역에서 거리, 세대수, 방향,  층수, 주차 등 희망조건을 상세하게 적어주세요.'
+          }
           spellCheck="false"
         />
       </TextField>
