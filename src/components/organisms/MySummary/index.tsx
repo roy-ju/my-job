0ba27f949c54474a0d42ea52e29994af8ee91693
Jay Lee <jay.lee@negocio.co.kr>
2023-09-18 15:30:52 +0900

    chore: mypage chevron

--- src/components/organisms/MySummary/index.tsx
+++ src/components/organisms/MySummary/index.tsx
@@ -1,6 +1,6 @@
 import { Avatar } from '@/components/atoms';
 import { StaticImageData } from 'next/image';
-import ChevronLeftIcon from '@/assets/icons/chevron_left_24.svg';
+import ChevronIcon from '@/assets/icons/my_chevron_16.svg';
 import { theme } from 'twin.macro';
 import defaultAvatar from '@/../public/static/images/default_avatar.png';
 
@@ -25,12 +25,7 @@ export default function MySummary({ nickname, profileImagePath, onClickMyDetail 
       >
         <Avatar size={48} src={profileImagePath || defaultAvatar} alt={`${nickname} 프로필 사진`} />
         <div tw="text-b1 font-bold ml-3 mr-1">{nickname ?? '김네고'}</div>
-        <ChevronLeftIcon
-          width={16}
-          height={16}
-          color={theme`colors.gray.700`}
-          style={{ transform: 'rotate(180deg)' }}
-        />
+        <ChevronIcon tw="mb-[2px]" />
       </button>
     </div>
   );
