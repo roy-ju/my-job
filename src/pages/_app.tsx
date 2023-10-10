ab152c474640dbf7abbe0b3eb1504c6ae2042160
roy-ju <jujihong2@gmail.com>
2023-10-10 10:48:41 +0900

    feat: 메타태그 관련 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,41 +25,6 @@ import { useRouter } from 'next/router';
 import AppVersionChecker from '@/providers/AppVersionChecker';
 import '../styles/globalFont.css';
 
-const schema = {
-  '@context': 'https://schema.org',
-  '@type': 'ItemList',
-  itemListElement: [
-    {
-      '@type': 'SiteNavigationElement',
-      position: 1,
-      name: '지역매물 추천 서비스',
-      description: '원하는 지역의 원하는 매물을 추천 받아보세요.',
-      url: 'https://www.negocio.co.kr/suggestRegionalForm',
-    },
-    {
-      '@type': 'SiteNavigationElement',
-      position: 2,
-      name: '지도 검색',
-      description: '원하는 지역에 원하는 매물과 단지를 검색해 보세요.',
-      url: 'https://www.negocio.co.kr/map',
-    },
-    {
-      '@type': 'SiteNavigationElement',
-      position: 3,
-      name: '집주인 매물등록',
-      description: '소유하신 매물을 등록해 보세요.',
-      url: 'https://www.negocio.co.kr/listingCreateAddress',
-    },
-    {
-      '@type': 'SiteNavigationElement',
-      position: 4,
-      name: '네고시오 가이드',
-      description: '네고시오에 대한 궁금증은 이곳에서 해결하세요.',
-      url: 'https://www.negocio.co.kr/intro',
-    },
-  ],
-};
-
 export type NextPageWithLayout<P = { children?: ReactNode }, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactNode, pageProps: any, prevPage?: ReactNode) => ReactNode;
   getComponent?: (pageProps: any) => ReactNode;
@@ -102,7 +67,6 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
         <title>{AppConfig.title}</title>
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1" />
         <meta name="naver-site-verification" content={`${process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION}`} />
-        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }} />
       </Head>
 
       {/* Google Tag */}
