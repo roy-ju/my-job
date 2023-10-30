6eb8553fa799c82d0ac546b5fc6d3c955040d139
roy-ju <roy.ju@negocio.co.kr>
2023-10-30 18:30:18 +0900

    feat: emit error fix

--- src/components/pages/mobile/My/ServiceInfoWrraper.tsx
+++ src/components/pages/mobile/My/ServiceInfoWrraper.tsx
@@ -1,18 +1,3 @@
-import { MobServiceInfo } from '@/components/templates';
-import Routes from '@/router/routes';
-import { useRouter } from 'next/router';
-import { useCallback } from 'react';
-
 export default function ServiceInfoWrraper() {
-  const router = useRouter();
-
-  const handleClickBusinessInfo = useCallback(() => {
-    router.push(`/${Routes.EntryMobile}/${Routes.BusinessInfo}`);
-  }, [router]);
-
-  const handleClickBack = () => {
-    router.back();
-  };
-
-  return <MobServiceInfo onClickBusinessInfo={handleClickBusinessInfo} onClickBack={handleClickBack} />;
+  return null;
 }
