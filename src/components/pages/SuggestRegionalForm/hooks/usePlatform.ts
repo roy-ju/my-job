591f91bedfc1b36459921f5a527a8a824760a479
roy-ju <jujihong2@gmail.com>
2023-10-31 14:31:39 +0900

    feat: listingCreateV3 test

--- src/components/pages/SuggestRegionalForm/hooks/usePlatform.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-
-import { PlatformContext } from "../provider/PlatfromProvider";
-
-export default function usePlatform() {
-  return useContext(PlatformContext);
-}
