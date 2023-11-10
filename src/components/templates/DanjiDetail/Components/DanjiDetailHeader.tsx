b224bde3bfa10be91bc807a843fd58cd4771ed2a
Jay Lee <jay.lee@negocio.co.kr>
2023-11-10 17:08:11 +0900

    feat: 단지상세 헤더 좋아요 optimistic ui 적용

--- src/components/templates/DanjiDetail/Components/DanjiDetailHeader.tsx
+++ src/components/templates/DanjiDetail/Components/DanjiDetailHeader.tsx
@@ -2,6 +2,8 @@ import { GetDanjiDetailResponse } from '@/apis/danji/danjiDetail';
 import { NavigationHeader, OverlayPresenter } from '@/components/molecules';
 import React, { useCallback, useState } from 'react';
 import tw from 'twin.macro';
+import { mutate } from 'swr';
+import axios from '@/lib/axios';
 
 import ShareIcon from '@/assets/icons/share.svg';
 import HeartFilledIcon from '@/assets/icons/heart.svg';
@@ -17,7 +19,6 @@ import Routes from '@/router/routes';
 export default function DanjiDetailHeader({
   isHeaderActive,
   danji,
-  handleMutateDanji,
 }: {
   isHeaderActive: boolean;
   danji?: GetDanjiDetailResponse;
@@ -25,10 +26,8 @@ export default function DanjiDetailHeader({
 }) {
   const router = useRouter(1);
   const [popup, setPopup] = useState(false);
-  const [isFavorite, setIsFavorite] = useState(!!danji?.is_favorite);
 
   const { user, isLoading: isAuthLoading } = useAuth();
-
   const onClickFavorite = useCallback(async () => {
     if (!danji || isAuthLoading) return;
 
@@ -48,30 +47,42 @@ export default function DanjiDetailHeader({
       return;
     }
 
-    if (user) {
-      if (!isFavorite) {
+    async function danjiFavoriteAddOptimistic() {
+      if (danji) {
         await danjiFavoriteAdd({
           danji_id: danji.danji_id,
         });
-        toast.success('관심단지로 추가되었습니다.', { toastId: 'toast-danji-favorite' });
-        setIsFavorite(true);
+        const { data: updatedData } = await axios.post('/danji/detail', { danji_id: danji?.danji_id });
+        return updatedData;
+      }
+    }
 
-        if (handleMutateDanji) {
-          handleMutateDanji();
-        }
-      } else {
+    async function danjiFavoriteRemoveOptimistic() {
+      if (danji) {
         await danjiFavoriteRemove({
           danji_id: danji.danji_id,
         });
-        toast.success('관심단지가 해제되었습니다.', { toastId: 'toast-danji-favorite' });
-        setIsFavorite(false);
-
-        if (handleMutateDanji) {
-          handleMutateDanji();
-        }
+        const { data: updatedData } = await axios.post('/danji/detail', { danji_id: danji?.danji_id });
+        return updatedData;
       }
     }
-  }, [danji, isAuthLoading, user, isFavorite, handleMutateDanji, router]);
+
+    if (user && danji) {
+      if (!danji.is_favorite) {
+        await mutate(['/danji/detail', { danji_id: danji.danji_id }], danjiFavoriteAddOptimistic, {
+          optimisticData: { ...danji, is_favorite: true },
+          rollbackOnError: true,
+        });
+        toast.success('관심단지로 추가되었습니다.', { toastId: 'toast-danji-favorite' });
+      } else {
+        await mutate(['/danji/detail', { danji_id: danji.danji_id }], danjiFavoriteRemoveOptimistic, {
+          optimisticData: { ...danji, is_favorite: false },
+          rollbackOnError: true,
+        });
+        toast.success('관심단지가 해제되었습니다.', { toastId: 'toast-danji-favorite' });
+      }
+    }
+  }, [danji, isAuthLoading, user, router]);
 
   const handleClickShare = useCallback(() => setPopup(true), []);
 
