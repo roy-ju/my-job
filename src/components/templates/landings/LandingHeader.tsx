7231c248808ea3bda607cd181297363de1a86c0d
roy-ju <roy.ju@negocio.co.kr>
2023-12-12 15:22:17 +0900

    feat: landingHeader 수정

--- src/components/templates/landings/LandingHeader.tsx
+++ src/components/templates/landings/LandingHeader.tsx
@@ -5,19 +5,19 @@ import { useRouter } from 'next/router';
 import tw from 'twin.macro';
 
 export default function LandingHeader() {
-  const { pathname } = useRouter();
+  const { pathname, push } = useRouter();
 
   return (
     <div tw="max-w-[1280px] xl:mx-auto">
       <header tw="flex items-center justify-between py-4 px-5 md:py-6 md:px-10 xl:px-20">
-        <button type="button" onClick={() => window.location.reload()} aria-label="네고시오 로고">
+        <button type="button" onClick={() => push('/')} aria-label="네고시오 로고">
           <LogoIcon tw="w-20 h-6 md:[width: 110px] md:[height: 34px]" />
         </button>
         <div tw="flex gap-3 font-bold text-xs md:text-base leading-5 text-gray-800">
           <Link
             tw="[letter-spacing: -0.25px] hover:opacity-70 transition-opacity"
             css={pathname.includes('/') && tw`text-nego-1000`}
-            href="/"
+            href="/intro"
           >
             서비스 소개
           </Link>
