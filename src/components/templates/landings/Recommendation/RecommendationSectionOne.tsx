b651065deb5d3de0b9927d9b21e6ee8ab44c3ab7
roy-ju <roy.ju@negocio.co.kr>
2023-12-15 15:34:12 +0900

    feat: RecommendationSectionOne 수정

--- src/components/templates/landings/Recommendation/RecommendationSectionOne.tsx
+++ src/components/templates/landings/Recommendation/RecommendationSectionOne.tsx
@@ -33,24 +33,26 @@ export default function SectionOne() {
                 src={TitleImage}
               />
             ) : (
-              <Image
-                tw="mx-auto"
-                width={348}
-                height={200}
-                alt="부동산 네고, 눈치보지 말고 쉽고 편하게"
-                src={TitleImage2}
-              />
+              <Image tw="" width={348} height={200} alt="부동산 네고, 눈치보지 말고 쉽고 편하게" src={TitleImage2} />
             )}
           </h1>
-          <p tw="text-base md:text-2xl md:leading-[34px] leading-6 text-gray-900 mb-3 md:mb-5">
+          <p tw="text-center text-base md:text-left md:text-2xl md:leading-[34px] leading-6 text-gray-900 mb-3 md:mb-5">
             <span tw="text-nego-800 font-bold">100% 네고</span>가능한 매물을 찾아드려요!
           </p>
-          <Link
-            tw="block mx-auto md:mr-auto md:ml-0 mb-5 bg-nego-800 text-white rounded-full w-[173px] md:w-[280px] py-3 md:py-[16px] font-bold md:text-[20px] text-sm leading-[22px] md:leading-[28px] hover:opacity-70 transition-opacity"
-            href="/recommendGuide"
-          >
-            네고 매물 추천받기
-          </Link>
+          <div tw="flex items-center gap-5">
+            <Link
+              tw="border border-nego-800 block mx-auto md:mr-auto md:ml-0 mb-5 bg-white text-nego-800 rounded-full w-[148px] md:w-[230px] py-3 md:py-[16px] font-bold md:text-[20px] text-sm leading-[22px] md:leading-[28px] hover:opacity-70 transition-opacity"
+              href="/"
+            >
+              네고시오 바로가기
+            </Link>
+            <Link
+              tw="block mx-auto md:mr-auto md:ml-0 mb-5 bg-nego-800 text-white rounded-full w-[148px] md:w-[230px] py-3 md:py-[16px] font-bold md:text-[20px] text-sm leading-[22px] md:leading-[28px] hover:opacity-70 transition-opacity"
+              href="/recommendGuide"
+            >
+              네고 매물 추천받기
+            </Link>
+          </div>
         </div>
         <div tw="h-[332px] overflow-hidden md:h-auto ">
           <Image
