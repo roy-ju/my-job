1c3945b47151774cfc7707eb19accd2e5ff9abc2
roy-ju <jujihong2@gmail.com>
2023-12-15 15:45:58 +0900

    feat: 마케팅 페이지 그림자 수정

--- src/components/templates/landings/Recommendation/RecommendationSectionOne.tsx
+++ src/components/templates/landings/Recommendation/RecommendationSectionOne.tsx
@@ -41,13 +41,13 @@ export default function SectionOne() {
           </p>
           <div tw="flex items-center gap-5">
             <Link
-              tw="border border-nego-800 block mx-auto md:mr-auto md:ml-0 mb-5 bg-white text-nego-800 rounded-full w-[148px] md:w-[230px] py-3 md:py-[16px] font-bold md:text-[20px] text-sm leading-[22px] md:leading-[28px] hover:opacity-70 transition-opacity"
+              tw="border border-nego-800 block mx-auto md:mr-auto md:ml-0 mb-5 bg-white text-nego-800 rounded-full w-[148px] md:w-[230px] py-3 md:py-[16px] font-bold md:text-[20px] text-sm leading-[22px] md:leading-[28px] hover:opacity-70 transition-opacity [box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.1)]"
               href="/"
             >
               네고시오 바로가기
             </Link>
             <Link
-              tw="block mx-auto md:mr-auto md:ml-0 mb-5 bg-nego-800 text-white rounded-full w-[148px] md:w-[230px] py-3 md:py-[16px] font-bold md:text-[20px] text-sm leading-[22px] md:leading-[28px] hover:opacity-70 transition-opacity"
+              tw="block mx-auto md:mr-auto md:ml-0 mb-5 bg-nego-800 text-white rounded-full w-[148px] md:w-[230px] py-3 md:py-[16px] font-bold md:text-[20px] text-sm leading-[22px] md:leading-[28px] hover:opacity-70 transition-opacity [box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.1)]"
               href="/recommendGuide"
             >
               네고 매물 추천받기
