2385c639cb7bf90aeb1127b95570eb16c983d417
roy-ju <roy.ju@negocio.co.kr>
2023-12-21 12:40:20 +0900

    feat: 작업중인 페이지 commentout

--- src/pages/m/realty/danji/[danjiID].tsx
+++ src/pages/m/realty/danji/[danjiID].tsx
@@ -1,55 +1,58 @@
 import { NextPageWithLayout } from '@/pages/_app';
 
-import { fetcher } from '@/lib/swr';
+// import { fetcher } from '@/lib/swr';
 
-import { checkPlatform } from '@/utils/checkPlatform';
+// import { checkPlatform } from '@/utils/checkPlatform';
 
-import { GetServerSideProps } from 'next';
+// import { GetServerSideProps } from 'next';
 
-import PlatformProvider from '@/providers/PlatformProvider';
+// import PlatformProvider from '@/providers/PlatformProvider';
 
-import { Container } from '@/components/container';
+// import { Container } from '@/components/container';
 
-import DanjiDetail from '@/components/pages/Danji/detail';
+// import DanjiDetail from '@/components/pages/Danji/detail';
 
-import { DanjiDetailResponse } from '@/services/danji/types';
+// import { DanjiDetailResponse } from '@/services/danji/types';
 
-const Page: NextPageWithLayout<{ prefetchedData?: { [key: string]: any } | null; platform: string }> = ({
-  prefetchedData,
-  platform,
-}) => (
-  <PlatformProvider platform={platform}>
-    <Container>
-      <DanjiDetail prefetchedData={prefetchedData} />
-    </Container>
-  </PlatformProvider>
-);
+// const Page: NextPageWithLayout<{ prefetchedData?: { [key: string]: any } | null; platform: string }> = ({
+//   prefetchedData,
+//   platform,
+// }) => (
+// <PlatformProvider platform={platform}>
+//   <Container>
+//     <DanjiDetail prefetchedData={prefetchedData} />
+//   </Container>
+// </PlatformProvider>
+
+// );
+
+const Page: NextPageWithLayout = () => <div />;
 
 Page.getLayout = function getLayout(page) {
   return <>{page}</>;
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const userAgent = context.req.headers['user-agent'];
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const userAgent = context.req.headers['user-agent'];
 
-  const platform = checkPlatform(userAgent);
+//   const platform = checkPlatform(userAgent);
 
-  let danjiDetail: Nullable<DanjiDetailResponse> = null;
+//   let danjiDetail: Nullable<DanjiDetailResponse> = null;
 
-  if (context.query.danjiID) {
-    const response: DanjiDetailResponse = await fetcher(['/danji/detail', { danji_id: Number(context.query.danjiID) }]);
+//   if (context.query.danjiID) {
+//     const response: DanjiDetailResponse = await fetcher(['/danji/detail', { danji_id: Number(context.query.danjiID) }]);
 
-    if (response.danji_id) {
-      danjiDetail = response;
-    }
-  }
+//     if (response.danji_id) {
+//       danjiDetail = response;
+//     }
+//   }
 
-  return {
-    props: {
-      ...(danjiDetail ? { prefetchedData: danjiDetail } : {}),
-      platform,
-    },
-  };
-};
+//   return {
+//     props: {
+//       ...(danjiDetail ? { prefetchedData: danjiDetail } : {}),
+//       platform,
+//     },
+//   };
+// };
 
 export default Page;
