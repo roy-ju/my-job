d3d67de3af7d0464d205c90010b58f6309f5ead7
roy-ju <jujihong2@gmail.com>
2024-01-11 19:58:21 +0900

    feat: 업데이트

--- src/components/domains/suggest/form/Stepper.tsx
+++ src/components/domains/suggest/form/Stepper.tsx
@@ -12,6 +12,8 @@ import StepFour from '@/assets/icons/stepper_4.svg';
 
 import StepFive from '@/assets/icons/stepper_5.svg';
 
+import StepChecked from '@/assets/icons/stepper_check.svg';
+
 import useGetStepperInfo from './hooks/useGetStepperInfo';
 
 import getColor from '../utils/getColor';
@@ -35,27 +37,72 @@ export default function Stepper() {
     return false;
   }, [currentIndex]);
 
+  console.log(currentIndex);
+
   return (
     <div tw="p-5 pb-8 [box-shadow: 0px 16px 24px 0px #00000014]">
       <div tw="flex items-center justify-center mb-6" css={[hidden && tw`hidden`]}>
-        <AnimateStepperNumber active={getStep(1, currentIndex) === 'current'}>
-          <StepOne color={getColor(1, currentIndex)} />
+        <AnimateStepperNumber active={getStep(0, currentIndex) === 'current'}>
+          {getStep(0, currentIndex) === 'prev' ? (
+            <StepChecked />
+          ) : (
+            <StepOne
+              color={getColor(1, currentIndex)}
+              css={[getStep(0, currentIndex) === 'current' && tw`[width: 26px] [height: 26px]`]}
+            />
+          )}
         </AnimateStepperNumber>
+
         <StepperSeperator step={getStep(2, currentIndex)} />
-        <AnimateStepperNumber active={getStep(2, currentIndex) === 'current'}>
-          <StepTwo color={getColor(2, currentIndex)} />
+
+        <AnimateStepperNumber active={getStep(1, currentIndex) === 'current'}>
+          {getStep(1, currentIndex) === 'prev' ? (
+            <StepChecked />
+          ) : (
+            <StepTwo
+              color={getColor(2, currentIndex)}
+              css={[getStep(1, currentIndex) === 'current' && tw`[width: 26px] [height: 26px]`]}
+            />
+          )}
         </AnimateStepperNumber>
+
         <StepperSeperator step={getStep(3, currentIndex)} />
-        <AnimateStepperNumber active={getStep(3, currentIndex) === 'current'}>
-          <StepThree color={getColor(3, currentIndex)} />
+
+        <AnimateStepperNumber active={getStep(2, currentIndex) === 'current'}>
+          {getStep(2, currentIndex) === 'prev' ? (
+            <StepChecked />
+          ) : (
+            <StepThree
+              color={getColor(3, currentIndex)}
+              css={[getStep(2, currentIndex) === 'current' && tw`[width: 26px] [height: 26px]`]}
+            />
+          )}
         </AnimateStepperNumber>
+
         <StepperSeperator step={getStep(4, currentIndex)} />
-        <AnimateStepperNumber active={getStep(4, currentIndex) === 'current'}>
-          <StepFour color={getColor(4, currentIndex)} />
+
+        <AnimateStepperNumber active={getStep(3, currentIndex) === 'current'}>
+          {getStep(3, currentIndex) === 'prev' ? (
+            <StepChecked />
+          ) : (
+            <StepFour
+              color={getColor(4, currentIndex)}
+              css={[getStep(3, currentIndex) === 'current' && tw`[width: 26px] [height: 26px]`]}
+            />
+          )}
         </AnimateStepperNumber>
+
         <StepperSeperator step={getStep(5, currentIndex)} />
-        <AnimateStepperNumber active={getStep(5, currentIndex) === 'current'}>
-          <StepFive color={getColor(5, currentIndex)} />
+
+        <AnimateStepperNumber active={getStep(4, currentIndex) === 'current'}>
+          {getStep(4, currentIndex) === 'prev' ? (
+            <StepChecked />
+          ) : (
+            <StepFive
+              color={getColor(5, currentIndex)}
+              css={[getStep(4, currentIndex) === 'current' && tw`[width: 26px] [height: 26px]`]}
+            />
+          )}
         </AnimateStepperNumber>
       </div>
       <StepperTitle title={title} subTitle={subTitle} isIcon={isIcon} />
