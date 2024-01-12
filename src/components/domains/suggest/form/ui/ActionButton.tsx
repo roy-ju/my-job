8722c95fbf34920751ecd94765071167e35cad78
roy-ju <jujihong2@gmail.com>
2024-01-12 19:03:04 +0900

    feat: updateLogic

--- src/components/domains/suggest/form/ui/ActionButton.tsx
+++ src/components/domains/suggest/form/ui/ActionButton.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 
 import ButtonV2 from '@/components/atoms/ButtonV2';
 
-import PersistentBottomBar from '@/components/atoms/PersistentBottomBar';
+import PersistentBottomBarV2 from '@/components/atoms/PersistentBottomBarV2';
 
 const ScrollUp = dynamic(() => import('./ScrollUp'));
 
@@ -28,13 +28,12 @@ function ActionButton({
   if (isRenderUpdateButton) {
     return (
       <div tw="w-full">
-        <PersistentBottomBar>
+        <PersistentBottomBarV2>
           <ScrollUp isRender />
           <ButtonV2 tw="w-full" size="bigger" onClick={handleClick} disabled={disabled}>
             수정완료
           </ButtonV2>
-          <div tw="[min-height: 18px]" />
-        </PersistentBottomBar>
+        </PersistentBottomBarV2>
       </div>
     );
   }
@@ -42,7 +41,7 @@ function ActionButton({
   if (isRenderSummitButton) {
     return (
       <div tw="w-full">
-        <PersistentBottomBar>
+        <PersistentBottomBarV2>
           <div tw="flex gap-3">
             {handleClickBack && (
               <ButtonV2 variant="gray" tw="w-full text-gray-1000" size="bigger" onClick={handleClickBack}>
@@ -53,21 +52,19 @@ function ActionButton({
               제출하기
             </ButtonV2>
           </div>
-          <div tw="[min-height: 18px]" />
-        </PersistentBottomBar>
+        </PersistentBottomBarV2>
       </div>
     );
   }
 
   return (
     <div tw="w-full">
-      <PersistentBottomBar>
+      <PersistentBottomBarV2>
         <ScrollUp isRender={isRenderRevisionText} />
         <ButtonV2 tw="w-full" size="bigger" onClick={handleClick} disabled={disabled}>
           다음
         </ButtonV2>
-        <div tw="[min-height: 18px]" />
-      </PersistentBottomBar>
+      </PersistentBottomBarV2>
     </div>
   );
 }
