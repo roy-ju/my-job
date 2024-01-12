4810781b7df23a12556829b00629f0ee68de62c3
roy-ju <jujihong2@gmail.com>
2024-01-12 16:22:45 +0900

    global navigation 수정

--- src/components/organisms/MobGlobalNavigation/index.tsx
+++ src/components/organisms/MobGlobalNavigation/index.tsx
@@ -24,69 +24,60 @@ export default function MobGlobalNavigation({
   };
 
   const buttonStyles = {
-    selected: tw`text-gray-1000`,
-    default: tw`text-gray-700`,
+    selected: tw`text-nego-800`,
+    default: tw`text-gray-800`,
+    defaultIcon: tw`text-gray-500`,
   };
 
   return (
     <div
-      tw="w-full border-t border-t-gray-300 flex flex-col gap-5 pt-2.5 pb-7 px-[0.6025rem] bg-white [z-index: 300]"
+      tw="w-full border-t border-t-gray-300 flex flex-col gap-5 py-[5px] px-[5px] bg-white [z-index: 300]"
       id="negocio-mob-global-navigation"
     >
       <div tw="flex items-center">
         <Button
           variant="ghost"
-          tw="flex-col px-0 h-auto flex-1 gap-[5px]"
+          tw="flex-col px-0 h-auto flex-1 gap-0.5"
           onClick={() => router.replace(`/${Routes.EntryMobile}`)}
         >
-          <HomeIcon css={index === 0 ? buttonStyles.selected : buttonStyles.default} />
-          <span css={[tw`text-gray-700 text-mobCaption`, index === 0 ? buttonStyles.selected : buttonStyles.default]}>
-            홈
-          </span>
+          <HomeIcon css={index === 0 ? buttonStyles.selected : buttonStyles.defaultIcon} />
+          <span css={[tw`text-body_01`, index === 0 ? buttonStyles.selected : buttonStyles.default]}>홈</span>
         </Button>
         <Button
           variant="ghost"
-          tw="flex-col px-0 h-auto flex-1 gap-[5px]"
+          tw="flex-col px-0 h-auto flex-1 gap-0.5"
           onClick={() => onClickButton(Routes.MyFavoriteList)}
         >
-          <HeartIcon css={index === 1 ? buttonStyles.selected : buttonStyles.default} />
-          <span css={[tw`text-gray-700 text-mobCaption`, index === 1 ? buttonStyles.selected : buttonStyles.default]}>
-            관심목록
-          </span>
+          <HeartIcon css={index === 1 ? buttonStyles.selected : buttonStyles.defaultIcon} />
+          <span css={[tw`text-body_01`, index === 1 ? buttonStyles.selected : buttonStyles.default]}>관심목록</span>
         </Button>
         <Button
           variant="ghost"
-          tw="flex-col px-0 h-auto flex-1 gap-[5px]"
+          tw="flex-col px-0 h-auto flex-1 gap-0.5"
           onClick={() => {
             onClickButton(Routes.Map);
             map?.setZoom(15, true);
           }}
         >
-          <MapIcon css={index === 2 ? buttonStyles.selected : buttonStyles.default} />
-          <span css={[tw`text-gray-700 text-mobCaption`, index === 2 ? buttonStyles.selected : buttonStyles.default]}>
-            지도
-          </span>
+          <MapIcon css={index === 2 ? buttonStyles.selected : buttonStyles.defaultIcon} />
+          <span css={[tw`text-body_01`, index === 2 ? buttonStyles.selected : buttonStyles.default]}>지도</span>
         </Button>
         <Button
           variant="ghost"
-          tw="flex-col px-0 h-auto flex-1 gap-[5px]"
+          tw="flex-col px-0 h-auto flex-1 gap-0.5"
           onClick={() => onClickButton(Routes.ChatRoomList)}
         >
           <div tw="relative">
-            <ChatIcon css={index === 3 ? buttonStyles.selected : buttonStyles.default} />
+            <ChatIcon css={index === 3 ? buttonStyles.selected : buttonStyles.defaultIcon} />
             {unreadChatCount > 0 && (
               <NewCount value="N" tw="font-bold absolute top-0 right-0 translate-x-1/2 -translate-y-1/2" />
             )}
           </div>
-          <span css={[tw`text-gray-700 text-mobCaption`, index === 3 ? buttonStyles.selected : buttonStyles.default]}>
-            채팅
-          </span>
+          <span css={[tw`text-body_01`, index === 3 ? buttonStyles.selected : buttonStyles.default]}>채팅</span>
         </Button>
-        <Button variant="ghost" tw="flex-col px-0 h-auto flex-1 gap-[5px]" onClick={() => onClickButton(Routes.My)}>
-          <UserIcon css={index === 4 ? buttonStyles.selected : buttonStyles.default} />
-          <span css={[tw`text-gray-700 text-mobCaption`, index === 4 ? buttonStyles.selected : buttonStyles.default]}>
-            마이페이지
-          </span>
+        <Button variant="ghost" tw="flex-col px-0 h-auto flex-1 gap-0.5" onClick={() => onClickButton(Routes.My)}>
+          <UserIcon css={index === 4 ? buttonStyles.selected : buttonStyles.defaultIcon} />
+          <span css={[tw`text-body_01`, index === 4 ? buttonStyles.selected : buttonStyles.default]}>마이페이지</span>
         </Button>
       </div>
     </div>
