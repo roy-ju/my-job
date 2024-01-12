828752652cd6f664e4129106024d08040e7c9d90
roy-ju <roy.ju@negocio.co.kr>
2024-01-12 19:03:04 +0900

    feat: updateLogic

---
+++ src/components/atoms/PersistentBottomBarV2/index.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react';
+
+export interface PersistentBottomBarV2Props {
+  children?: ReactNode;
+}
+
+export default function PersistentBottomBarV2({ children }: PersistentBottomBarV2Props) {
+  return <div tw="bg-white w-full py-3 px-5 shadow-[0px_0px_24px_rgba(0,0,0,0.08)]">{children}</div>;
+}
