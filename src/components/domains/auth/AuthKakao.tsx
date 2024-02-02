dad06ede432c1169c491409772f53e050d54603c
roy-ju <jujihong2@gmail.com>
2024-02-02 11:50:48 +0900

    feat: update Logic

--- src/components/domains/auth/AuthKakao.tsx
+++ src/components/domains/auth/AuthKakao.tsx
@@ -1,21 +1,20 @@
-import { useEffect } from 'react';
+import { useCallback } from 'react';
 
 import { useRouter } from 'next/router';
 
 import { loginWithKakao } from '@/lib/kakao';
 
+import { useTimeout } from '@/hooks/useTimeout';
+
 export default function AuthKakao() {
   const router = useRouter();
 
-  useEffect(() => {
-    const type = router.query.type;
+  const handleLoginWithKakao = useCallback(() => {
+    const type = router?.query?.type;
+    loginWithKakao(type === 'update' ? type : '');
+  }, [router]);
 
-    const timeout = setTimeout(() => loginWithKakao(type === 'update' ? type : ''), 100);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [router.query.type]);
+  useTimeout(handleLoginWithKakao, 1000);
 
   return <div />;
 }
