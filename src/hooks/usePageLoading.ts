8387c963b4e89bec33dfb40b745489096dd262f2
roy-ju <roy.ju@negocio.co.kr>
2024-02-02 10:32:50 +0900

    feat : update

--- src/hooks/usePageLoading.ts
+++ src/hooks/usePageLoading.ts
@@ -9,16 +9,22 @@ export default function usePageLoading() {
     const routeEventStart = () => {
       NProgress.start();
     };
+
     const routeEventEnd = () => {
       NProgress.done();
     };
 
     Router.events.on('routeChangeStart', routeEventStart);
+
     Router.events.on('routeChangeComplete', routeEventEnd);
+
     Router.events.on('routeChangeError', routeEventEnd);
+
     return () => {
       Router.events.off('routeChangeStart', routeEventStart);
+
       Router.events.off('routeChangeComplete', routeEventEnd);
+
       Router.events.off('routeChangeError', routeEventEnd);
     };
   }, []);
