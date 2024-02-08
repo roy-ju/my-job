ad6fc1111e87a68bfb92826d11ac885b83653723
roy-ju <jujihong2@gmail.com>
2024-02-08 10:29:52 +0900

    feat: temp commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -52,12 +52,9 @@ const ToastContainer = dynamic(() => import('@/lib/react-toastify'), { ssr: fals
 
 const AppVersionChecker = dynamic(() => import('@/providers/AppVersionChecker'), { ssr: false });
 
-const GlobalAuthPopup = dynamic(() => import('@/components/organisms/GlobalAuthPopup/GlobalAuthPopup'), { ssr: false });
+const AuthPopup = dynamic(() => import('@/components/domains/auth/global-login'), { ssr: false });
 
-const GlobalAuthVerifyCiPopup = dynamic(
-  () => import('@/components/organisms/GlobalAuthVerifyCiPopup/GlobalAuthVerifyCiPopup'),
-  { ssr: false },
-);
+const VerifyCiPopup = dynamic(() => import('@/components/domains/auth/global-verify-ci'), { ssr: false });
 
 export type NextPageWithLayout<P = { children?: ReactNode }, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactNode, pageProps: any, prevPage?: ReactNode) => ReactNode;
@@ -154,8 +151,8 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
             <div id="rootOverlay" tw="pointer-events-none [z-index: 1500]" />
             <ErrorBoundary>
               <NegocioProvider>{getLayout(getComponent(pageProps), pageProps)}</NegocioProvider>
-              <GlobalAuthPopup />
-              <GlobalAuthVerifyCiPopup />
+              <AuthPopup />
+              <VerifyCiPopup />
             </ErrorBoundary>
             <ToastContainer platform={platform} />
             <TooltipProvider />
