5fa674cc093237108850978f03b7fb7a66cf2f45
roy-ju <roy.ju@negocio.co.kr>
2024-02-20 15:44:47 +0900

    feat: update

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -5,15 +5,11 @@ import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 export default function useInterval(callback: () => void, delay: number | null) {
   const savedCallback = useRef(callback);
 
-  // Remember the latest callback if it changes.
   useIsomorphicLayoutEffect(() => {
     savedCallback.current = callback;
   }, [callback]);
 
-  // Set up the interval.
   useEffect(() => {
-    // Don't schedule if no delay is specified.
-    // Note: 0 is a valid value for delay.
     if (delay === null) {
       return;
     }
