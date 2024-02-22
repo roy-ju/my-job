a986239d6d010ccb4d3351f360fc23dc37969872
roy-ju <roy.ju@negocio.co.kr>
2024-02-22 19:22:56 +0900

    feat: LineUserAgent 추가

--- src/components/domains/auth/global-app-install/index.tsx
+++ src/components/domains/auth/global-app-install/index.tsx
@@ -11,8 +11,8 @@ export default function GlobalAppInstall({
     <OverlayPresenter>
       <Popup>
         <Popup.ContentGroup>
-          <Popup.SmallTitle>앱 다운로드하러가기</Popup.SmallTitle>
-          <Popup.Body>앱에서 더많은 서비스를 이용하실 수 있습니다.</Popup.Body>
+          <Popup.Title>앱을 다운로드 하시겠어요?</Popup.Title>
+          <Popup.Body>해당 서비스는 앱을 다운로드하여 이용해주세요.</Popup.Body>
         </Popup.ContentGroup>
         <Popup.ButtonGroup>
           <Popup.CancelButton onClick={handleClickCancel}>취소</Popup.CancelButton>
