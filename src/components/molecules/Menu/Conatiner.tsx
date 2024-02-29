1d990b89001506e047071f73e899f51aee12eb5f
roy-ju <jujihong2@gmail.com>
2024-02-29 15:01:35 +0900

    feat: update Menu Components

---
+++ src/components/molecules/Menu/Conatiner.tsx
@@ -0,0 +1,7 @@
+import tw, { styled } from 'twin.macro';
+
+const Container = styled.div`
+  ${tw`z-[110]`}
+`;
+
+export default Container;
---
+++ src/components/molecules/Menu/Content.tsx
@@ -0,0 +1,7 @@
+import tw, { styled } from 'twin.macro';
+
+const Content = styled.button`
+  ${tw`[padding-top: 13px] [padding-bottom: 13px] px-4 text-body_02 text-gray-1000 hover:bg-nego-100 active:bg-nego-200 disabled:text-gray-500 [min-width: 112px]`}
+`;
+
+export default Content;
