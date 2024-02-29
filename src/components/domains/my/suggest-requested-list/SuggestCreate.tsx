85c154006dc83e228af856d5b07683247370913e
roy-ju <jujihong2@gmail.com>
2024-02-29 12:47:43 +0900

    feat: update Logic

--- src/components/domains/my/suggest-requested-list/SuggestCreate.tsx
+++ src/components/domains/my/suggest-requested-list/SuggestCreate.tsx
@@ -5,7 +5,7 @@ import Plus24 from '@/assets/icons/plus_24.svg';
 import FabButton from '@/components/atoms/FabButton/FabButton';
 
 const StickyContainer = styled.div`
-  ${tw`sticky bottom-4 right-4`}
+  ${tw`fixed bottom-4 right-4`}
 `;
 
 export default function SuggestCreate({
@@ -31,8 +31,27 @@ export default function SuggestCreate({
 
   return (
     <StickyContainer>
-      <FabButton onClick={handleClick} type={isScrollingButton ? 'none' : 'extended'}>
-        <Plus24 /> {!isScrollingButton && '집 구해요 등록'}
+      {/* <motion.button
+        initial={false}
+        animate={{
+          height: isScrollingButton ? 200 : 100,
+          width: isScrollingButton ? 200 : 100,
+        }}
+      > */}
+      <FabButton
+        onClick={handleClick}
+        animate={{
+          minWidth: isScrollingButton ? 48 : 'auto',
+          width: isScrollingButton ? 48 : 'auto',
+          height: 48,
+          paddingLeft: isScrollingButton ? '0px' : '16px',
+          paddingRight: isScrollingButton ? '0px' : '16px',
+          transition: { type: 'tween', duration: 0.5 },
+          scale: 1,
+        }}
+        // css={[isScrollingButton ? tw`px-0` : tw`flex items-center gap-1 px-4`]}
+      >
+        <Plus24 style={{ width: '24px', height: '24px' }} /> {!isScrollingButton && '집 구해요 등록'}
       </FabButton>
     </StickyContainer>
   );
