33368e908921079921819b364caa8bfc9e7fba03
roy-ju <roy.ju@negocio.co.kr>
2024-03-04 17:07:27 +0900

    feat: update Logic

--- src/components/domains/my/suggest-requested-list/Negotiable.tsx
+++ src/components/domains/my/suggest-requested-list/Negotiable.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 export default function Negotiable({ negotiable }: { negotiable: boolean }) {
   if (!negotiable) return null;
 
-  return <span>&nbsp;(협의 가능)</span>;
+  return <span>&nbsp;(협의가능)</span>;
 }
