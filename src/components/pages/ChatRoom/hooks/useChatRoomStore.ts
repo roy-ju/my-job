e05fd6d58ebf96e389c607daf63081f582e212a0
roy-ju <roy.ju@negocio.co.kr>
2024-03-05 18:15:23 +0900

    feat: update

--- src/components/pages/ChatRoom/hooks/useChatRoomStore.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import { ChatRoomContext } from '../provider/ChatRoomProvider';
-
-export default function useChatRoomStore() {
-  return useContext(ChatRoomContext);
-}
--- src/components/pages/ChatRoom/provider/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as ChatRoomProvider } from './ChatRoomProvider';
