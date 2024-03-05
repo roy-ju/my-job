41f522a59e21185aa89ab33aeb04172c1dd2440a
roy-ju <roy.ju@negocio.co.kr>
2024-03-05 10:05:14 +0900

    feat: update test

--- src/hooks/useIosWebkitNoneApplySafeArea.ts
+++ src/hooks/useIosWebkitNoneApplySafeArea.ts
@@ -3,7 +3,11 @@ import { useEffect } from 'react';
 export default function useIosWebkitNoneApplySafeArea() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      window.webkit?.messageHandlers?.noneApplySafeArea?.postMessage?.('');
+      window.webkit?.messageHandlers?.noneApplySafeArea?.postMessage?.('mount');
+
+      return () => {
+        window.webkit?.messageHandlers?.noneApplySafeArea?.postMessage?.('unmount');
+      };
     }
   }, []);
 }
