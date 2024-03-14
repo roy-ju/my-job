b792707550b1f14d8cdf20ca10704cbbc12f8a0f
roy-ju <roy.ju@negocio.co.kr>
2024-03-14 18:11:38 +0900

    feat: update realestate-dict

--- src/components/domains/realestate-helper/realestate-document-verify-result/widget/RealestateDocumentVerifyResultWidget.ts
+++ src/components/domains/realestate-helper/realestate-document-verify-result/widget/RealestateDocumentVerifyResultWidget.ts
@@ -3,3 +3,7 @@ import tw, { styled } from 'twin.macro';
 export const PreviouslyEnteredAddressTitle = styled.p`
   ${tw`mb-2 text-gray-600 text-body_02`}
 `;
+
+export const ListWrraper = styled.div`
+  ${tw`flex flex-col gap-4 px-5`}
+`;
