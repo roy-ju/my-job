e01f0193f28c10e9dca8c174779b17fcf8a61d48
roy-ju <roy.ju@negocio.co.kr>
2024-03-20 21:29:51 +0900

    feat: update

---
+++ src/components/atoms/FlexContents/index.tsx
@@ -0,0 +1,7 @@
+import tw, { styled } from 'twin.macro';
+
+const FlexContents = styled.div`
+  ${tw`flex flex-col flex-1 h-full overflow-x-hidden overflow-y-auto`}
+`;
+
+export default FlexContents;
