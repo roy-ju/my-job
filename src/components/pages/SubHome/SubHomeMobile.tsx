c5b9c345bda03a4dfcc391997851a1461d6bc944
roy-ju <jujihong2@gmail.com>
2024-03-22 18:02:37 +0900

    feat: update Logic

--- src/components/pages/SubHome/SubHomeMobile.tsx
+++ src/components/pages/SubHome/SubHomeMobile.tsx
@@ -2,9 +2,15 @@ import SubHome from '@/components/domains/realestate-helper/SubHome';
 
 import MobileContainer from '@/components/atoms/MobileContainer';
 
+import useSyncronizer from '@/states/hooks/useSyncronizer';
+
+import BottomGlobalNavigation from '@/components/organisms/global/BottomGlobalNavigation';
+
 export default function SubHomeMobile() {
+  const { unreadChatCount } = useSyncronizer();
+
   return (
-    <MobileContainer>
+    <MobileContainer bottomNav={<BottomGlobalNavigation index={1} unreadChatCount={unreadChatCount} />}>
       <SubHome />
     </MobileContainer>
   );
