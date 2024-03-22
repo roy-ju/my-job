68255c3bb1b8145c39effc53be837c8560744197
roy-ju <roy.ju@negocio.co.kr>
2024-03-22 18:31:04 +0900

    feat: update realestate-dict

--- src/components/domains/realestate-helper/sub-home/Preview.tsx
+++ src/components/domains/realestate-helper/sub-home/Preview.tsx
@@ -8,31 +8,78 @@ import HomeImage from '@/../public/static/images/subhome_home.png';
 
 import StampImage from '@/../public/static/images/subhome_stamp.png';
 
+import CheckImage from '@/../public/static/images/checked_3d.png';
+
+import FolderImage from '@/../public/static/images/folder_3d.png';
+
 import { Box, Flex, PreviewContainer, PreviewIconWrraper } from './widget/PreviewWidget';
 
-export default function Preview() {
+export default function Preview({
+  handleNavigateTradeProcess,
+  handleNavigateDeungibu,
+  handleNavigateCheckList,
+  handleNavigateSpecialTerms,
+  handleNavigateCommonSense,
+  handleNavigateDict,
+}: {
+  handleNavigateTradeProcess: () => void;
+  handleNavigateDeungibu: () => void;
+  handleNavigateCheckList: () => void;
+  handleNavigateSpecialTerms: () => void;
+  handleNavigateCommonSense: () => void;
+  handleNavigateDict: () => void;
+}) {
   return (
-    <PreviewContainer>
-      <Box tw="px-4">
-        <Flex>
-          <PreviewIconWrraper>
-            <Image src={HomeImage.src} width={51.2} height={51.2} alt="homeImage" />
-            거래 절차
-          </PreviewIconWrraper>
-          <PreviewIconWrraper>
-            <Image src={BulbImage.src} width={51.2} height={51.2} alt="bulbImage" />
-            부동산 상식
-          </PreviewIconWrraper>
-          <PreviewIconWrraper>
-            <Image src={StampImage.src} width={51.2} height={51.2} alt="stampImage" />
-            계약서
-          </PreviewIconWrraper>
-          <PreviewIconWrraper>
-            <Image src={DictImage.src} width={51.2} height={51.2} alt="dictImage" />
-            용어사전
-          </PreviewIconWrraper>
-        </Flex>
-      </Box>
-    </PreviewContainer>
+    <>
+      <PreviewContainer>
+        <Box tw="px-5">
+          <div tw="flex gap-3 justify-center">
+            <button
+              type="button"
+              tw="flex-1 text-subhead_02 flex gap-2 items-center text-left text-yellow-1000 pl-3 py-4 [padding-right: 18px] border border-yellow-300 [background: rgba(255, 245, 217, 0.6)] [border-radius: 12px]"
+              onClick={handleNavigateDeungibu}
+            >
+              <Image src={FolderImage.src} width={42} height={42} alt="folder" />
+
+              <div tw="flex-col">
+                <p tw="whitespace-nowrap">무료</p>
+                <p tw="whitespace-nowrap">등기부 조회</p>
+              </div>
+            </button>
+            <button
+              type="button"
+              tw="flex-1 text-subhead_02 flex gap-2 items-center text-left text-green-800 pl-3 py-4 [padding-right: 18px]  border border-green-300 [background: rgba(227, 252, 237, 0.6)] [border-radius: 12px]"
+              onClick={handleNavigateCheckList}
+            >
+              <Image src={CheckImage.src} width={42} height={42} alt="check" />
+
+              <div tw="flex-col">
+                <p tw="whitespace-nowrap">매물 방문</p>
+                <p tw="whitespace-nowrap">체크리스트</p>
+              </div>
+            </button>
+          </div>
+          <div tw="[min-height: 1px] w-full bg-gray-200 my-5" />
+          <Flex tw="px-1 gap-5">
+            <PreviewIconWrraper onClick={handleNavigateTradeProcess}>
+              <Image src={HomeImage.src} width={56} height={56} alt="homeImage" />
+              거래 절차
+            </PreviewIconWrraper>
+            <PreviewIconWrraper onClick={handleNavigateCommonSense}>
+              <Image src={BulbImage.src} width={56} height={56} alt="bulbImage" />
+              상식
+            </PreviewIconWrraper>
+            <PreviewIconWrraper onClick={handleNavigateSpecialTerms}>
+              <Image src={StampImage.src} width={56} height={56} alt="stampImage" />
+              계약서
+            </PreviewIconWrraper>
+            <PreviewIconWrraper onClick={handleNavigateDict}>
+              <Image src={DictImage.src} width={56} height={56} alt="dictImage" />
+              용어사전
+            </PreviewIconWrraper>
+          </Flex>
+        </Box>
+      </PreviewContainer>
+    </>
   );
 }
--- src/components/domains/realestate-helper/sub-home/widget/PreviewWidget.ts
+++ src/components/domains/realestate-helper/sub-home/widget/PreviewWidget.ts
@@ -5,25 +5,16 @@ export const PreviewContainer = styled.div`
 `;
 
 export const Box = styled.div`
-  ${tw`[margin-block: 22px] [border-radius: 12px] [width: calc(100% - 40px)] mx-auto`}
-
-  padding-left: 18px !important;
-
-  padding-right: 16px;
-
-  padding-top: 22px;
-
-  padding-bottom: 26px;
-
+  ${tw`my-5 [border-radius: 12px] [width: calc(100% - 40px)] mx-auto px-5 py-5 bg-white`}
   box-shadow: 0px 2px 20px 0px rgba(0, 0, 0, 0.06), 0px 2px 10px 0px rgba(0, 0, 0, 0.03);
 `;
 
 export const Flex = styled.div`
-  ${tw`flex items-center w-full [padding-left: 10px] [padding-right: 12px] justify-between`}
+  ${tw`flex items-center w-full [padding-left: 10px] [padding-right: 12px] gap-5 justify-center`}
 `;
 
 export const PreviewIconWrraper = styled.div`
-  ${tw`flex flex-col items-center justify-center flex-1 text-gray-900`}
+  ${tw`flex flex-col items-center justify-center flex-1 text-gray-900 bg-white text-body_01`}
 
   gap: 4.8px
 `;
