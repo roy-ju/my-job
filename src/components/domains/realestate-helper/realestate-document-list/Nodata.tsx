7e3ebc02eb793b35aeb3b4ce0c56323f5b946314
roy-ju <jujihong2@gmail.com>
2024-03-22 17:56:40 +0900

    feat: realesate-dict

--- src/components/domains/realestate-helper/realestate-document-list/Nodata.tsx
+++ src/components/domains/realestate-helper/realestate-document-list/Nodata.tsx
@@ -15,7 +15,15 @@ export default function Nodata() {
     <Container>
       <ExclamationMark tw="mx-auto" />
       <MarginTopTwenty />
-      <NodataParagraph>조회한 등기부가 없습니다.</NodataParagraph>
+      <NodataParagraph tw="text-center">조회한 등기부가 없습니다.</NodataParagraph>
+      <MarginTopTwenty />
+      <NodataParagraph tw="[width: 295px] rounded-2xl text-body_02 bg-gray-100 text-center [padding-inline: 30px] [padding-block: 16px]">
+        주 3회 등기부 조회가 무료입니다.
+        <br />
+        신규 조회를 눌러서
+        <br />
+        원하는 건물의 등기부를 확인해보세요!
+      </NodataParagraph>
     </Container>
   );
 }
