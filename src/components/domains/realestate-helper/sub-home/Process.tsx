c35cdf5e73ce3225b27b1e95b7027d3e1fbec951
roy-ju <roy.ju@negocio.co.kr>
2024-03-26 19:33:42 +0900

    feat: update Logic

--- src/components/domains/realestate-helper/sub-home/Process.tsx
+++ src/components/domains/realestate-helper/sub-home/Process.tsx
@@ -6,6 +6,7 @@ import TextButton from '@/components/atoms/TextButton';
 
 import TradeProcessImage from '@/../public/static/images/image_step.png';
 
+import useCheckPlatform from '@/hooks/useCheckPlatform';
 import {
   ProcessImageContainer,
   ProcessTitleWrraper,
@@ -37,16 +38,18 @@ function ProcessMoreButton({ handleClick }: { handleClick: () => void }) {
 }
 
 export default function Process({ handleClick }: { handleClick: () => void }) {
+  const { platform } = useCheckPlatform();
+
   return (
     <ProcessContainer>
       <ProcessTitle />
       <MarginTopFour />
       <ProcessImageContainer onClick={handleClick}>
         <Image
-          id="negocio-trade-process-image"
+          id={platform === 'pc' ? 'negocio-trade-process-image-pc' : 'negocio-trade-process-image-mobile'}
           src={TradeProcessImage.src}
           alt="tradeProcessImage"
-          tw="w-full h-full object-cover object-left [min-height: 378px]"
+          tw="w-full h-full object-left [min-height: 378px]"
           width={375}
           height={378}
           quality={100} // 품질을 조절할 수 있는 속성
