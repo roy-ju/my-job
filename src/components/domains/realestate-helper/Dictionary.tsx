24bf82892ef0cdf0602881b73c12a6db4ec16404
roy-ju <roy.ju@negocio.co.kr>
2024-03-26 21:45:33 +0900

    feat: update Logic

--- src/components/domains/realestate-helper/Dictionary.tsx
+++ src/components/domains/realestate-helper/Dictionary.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-
 import tw, { styled } from 'twin.macro';
 
 import Container from '@/components/atoms/Container';
@@ -12,68 +10,32 @@ import { NavigationHeader } from '@/components/molecules';
 
 import useFetchSubHomeGuideList from '@/services/sub-home/useFetchSubHomeGuideList';
 
-import useRecentlyClickedElementId from '@/states/hooks/useRecentlyClickedElementId';
-
 import useIosWebkitNoneApplySafeArea from '@/hooks/useIosWebkitNoneApplySafeArea';
 
 import useHandleClickBack from './dictionary/hooks/useHandleClickBack';
+
+import useRecentlyGoClickedElement from './dictionary/hooks/useRecentlyGoClickedElement';
 
 import CategoryTabs from './dictionary/CategoryTabs';
 
 import DictContents from './dictionary/DictContents';
 
-import useCategoryTabs from './dictionary/hooks/useCategoryTabs';
+import BottomRefElement from './dictionary/BottomRefElement';
 
-import { DictElementListItem } from './dictionary/types';
-
-import {
-  DictionaryBottomElementId,
-  DictionaryContainerElementId,
-  PrefixListElementItemId,
-} from './dictionary/constants/element_id';
+import { DictionaryContainerElementId } from './dictionary/constants/element_id';
 
 const FlexContents = styled.div`
   ${tw`relative flex flex-col flex-1 min-h-0 gap-5 px-5 overflow-x-hidden overflow-y-auto`}
 `;
 
 export default function Dictionary() {
-  const [elementsList, setElementsList] = useState<DictElementListItem[]>([]);
-
   const { handleClickBack } = useHandleClickBack();
-
-  const { recenltyClickedElementID, handleResetRecentlyClickedElementId } = useRecentlyClickedElementId();
-
-  const { tab, tabIndex, handleChangeTab } = useCategoryTabs({ elementsList });
 
   const { isLoading, middleCategoryList, list } = useFetchSubHomeGuideList({ code: 'DICT' });
 
-  useEffect(() => {
-    list?.forEach((i, idx) => {
-      const item = document.getElementById(`${PrefixListElementItemId}-${i.name}`);
-
-      if (item) {
-        setElementsList((prev) => [...prev, { name: i.name, element: item, priority: idx + 1 }]);
-      }
-    });
-  }, [list]);
-
-  useEffect(() => {
-    if (recenltyClickedElementID) {
-      const element = document.getElementById(recenltyClickedElementID);
-
-      setTimeout(() => {
-        element?.scrollIntoView();
-
-        setTimeout(() => {
-          handleResetRecentlyClickedElementId();
-        }, 200);
-      });
-    }
-  }, [handleResetRecentlyClickedElementId, recenltyClickedElementID]);
+  useRecentlyGoClickedElement();
 
   useIosWebkitNoneApplySafeArea();
-
-  console.log('render');
 
   if (isLoading) {
     return (
@@ -90,13 +52,13 @@ export default function Dictionary() {
         <NavigationHeader.Title>부동산 용어 사전</NavigationHeader.Title>
       </NavigationHeader>
 
-      <CategoryTabs tab={tab} tabIndex={tabIndex} handleChangeTab={handleChangeTab} list={middleCategoryList} />
+      <CategoryTabs list={list} middleCategoryList={middleCategoryList} />
 
       <FlexContents id={DictionaryContainerElementId}>
         {list.map((item) => (
           <DictContents key={item.name} item={item} />
         ))}
-        <div id={DictionaryBottomElementId} tw="[min-height: 10px] [min-width: 100%]" />
+        <BottomRefElement />
       </FlexContents>
     </Container>
   );
