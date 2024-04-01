2e941bcb1d61e90f3c7cf81ee26e9e22234fdb04
roy-ju <roy.ju@negocio.co.kr>
2024-04-01 14:44:06 +0900

    feat: update Logic

--- src/states/hooks/useInAppBrowserCheck.ts
+++ src/states/hooks/useInAppBrowserCheck.ts
@@ -24,14 +24,12 @@ export default function useInAppBrowserCheck() {
 
     const userAgent = window.navigator.userAgent.toLowerCase();
 
-    if (userAgent.match(/Line|instagram|FB_IAB|FB4A|FBAN|FBIOS|FBSS\/[^1]/i)) {
-      if (userAgent.includes('iphone') || userAgent.includes('ipad') || userAgent.includes('ipod')) {
-        // Ios devices
-        setInAppInfo((prev) => ({ ...prev, isInAppBrowser: true }));
-      } else {
-        // Android devices
+    if (userAgent.includes('iphone') || userAgent.includes('ipad') || userAgent.includes('ipod')) {
+      if (userAgent.match(/Line|kakaotalk|instagram|FB_IAB|FB4A|FBAN|FBIOS|FBSS\/[^1]/i)) {
         setInAppInfo((prev) => ({ ...prev, isInAppBrowser: true }));
       }
+    } else if (userAgent.match(/Line||instagram|FB_IAB|FB4A|FBAN|FBIOS|FBSS\/[^1]/i)) {
+      setInAppInfo((prev) => ({ ...prev, isInAppBrowser: true }));
     }
   }, [isNativeApp, platform, setInAppInfo]);
 }
