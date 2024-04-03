70b027ec045b55bc8183defce718483c8c9d3412
roy-ju <roy.ju@negocio.co.kr>
2024-04-03 10:42:21 +0900

    feat: update Logic

--- src/components/atoms/MobileContainer/index.tsx
+++ src/components/atoms/MobileContainer/index.tsx
@@ -1,15 +1,29 @@
 import { ReactNode } from 'react';
 
+import tw, { styled } from 'twin.macro';
+
 export interface MobileContainerProps {
   children?: ReactNode;
   bottomNav?: JSX.Element | null;
 }
 
+const Container = styled.div`
+  ${tw`flex flex-col w-full h-full bg-white`}
+`;
+
+const ChildrenWrraper = styled.div`
+  ${tw`relative w-full flex-1 min-h-0 my-0 mx-auto [z-index: 1] bg-white overflow-hidden`}
+`;
+
+const BottomNavWrraper = styled.div`
+  ${tw`w-full mx-auto bg-white`}
+`;
+
 export default function MobileContainer({ children, bottomNav }: MobileContainerProps) {
   return (
-    <div tw="flex flex-col w-full h-full bg-nego-1300">
-      <div tw="relative w-full flex-1 min-h-0 my-0 mx-auto [z-index: 1] bg-white overflow-hidden">{children}</div>
-      {bottomNav && <div tw="w-full mx-auto bg-white">{bottomNav}</div>}
-    </div>
+    <Container>
+      <ChildrenWrraper>{children}</ChildrenWrraper>
+      {bottomNav && <BottomNavWrraper>{bottomNav}</BottomNavWrraper>}
+    </Container>
   );
 }
