698878b8ba81fc8a762f8320e9d6ed8d1d00b7a7
roy-ju <roy.ju@negocio.co.kr>
2024-04-04 18:19:27 +0900

    feat: 단지상세 모바일 구조변경

--- src/components/domains/danji/detail/around-info-mobile/index.tsx
+++ src/components/domains/danji/detail/around-info-mobile/index.tsx
@@ -1,300 +1,121 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-
-import { useRef, useState, MouseEvent, useMemo, useEffect } from 'react';
-
 import dynamic from 'next/dynamic';
-
-import tw, { styled } from 'twin.macro';
 
 import { v4 as uuid4 } from 'uuid';
 
-import cloneDeep from 'lodash/cloneDeep';
-
 import Button from '@/components/atoms/Button';
 
-import ConvertArrayToSubwayComponent from '@/components/organisms/MobDanjiDetail/SubwayFormatComponent';
-
-import useMobileDanjiInteraction from '@/states/hooks/useMobileDanjiInteraction';
-
-import { convertedArr, convertedArrForMarker, getAverageDistance } from '@/utils/danjiAroundInfo';
-
-import { KakaoMapCategoryCode } from '@/lib/kakao/kakao_map_category';
-
-import { searchCategoryGroup, SearchCategoryResponse } from '@/lib/kakao/search_category';
+import { getAverageDistance } from '@/utils/danjiAroundInfo';
 
 import Nodata from './Nodata';
 
+import SubwayFormatUI from '../danji-around-map-card/SubwayFormatUI';
+
 import { CommonDanjiDetailProps } from '../types';
+
+import {
+  Container,
+  ListContainer,
+  DetailCategoryButton,
+  DistanceText,
+  ListItem,
+  LoadingWrraper,
+  MapWrraper,
+  NameText,
+  NodataWrraper,
+  ScrollContainer,
+  Title,
+  TitleWrraper,
+} from './widget/DanjiAroundInfoMobileWidget';
+
+import useAroundInfoMobileHandler from './hooks/useAroundInfoMobileHandler';
+
+import { categoryButtonList } from './types';
 
 const DanjiAroundMapCard = dynamic(() => import('../danji-around-map-card'), { ssr: false });
 
-type BtnState = {
-  SW8?: boolean;
-  HP8?: boolean;
-  MT1?: boolean;
-  CS2?: boolean;
-  BK9?: boolean;
-  PO3?: boolean;
-};
-
-const buttonList: { id: keyof BtnState; korTitle: string }[] = [
-  { id: 'SW8', korTitle: '지하철' },
-  { id: 'HP8', korTitle: '병원' },
-  { id: 'MT1', korTitle: '마트' },
-  { id: 'CS2', korTitle: '편의점' },
-  { id: 'BK9', korTitle: '은행' },
-  { id: 'PO3', korTitle: '관공서' },
-];
-
-const Div = styled.div``;
-
 export default function AroundInfo({ danji }: CommonDanjiDetailProps) {
-  const scrollRef = useRef<HTMLDivElement | null>(null);
-  const refs = useRef<any>([]);
-  const [catergoryList, setCategoryList] = useState<SearchCategoryResponse['documents']>([]);
-  const [markers, setMarkers] = useState<SearchCategoryResponse['documents']>([]);
-  const [isMoreClick, setIsMoreClick] = useState(false);
-  const [sliceNum, setSliceNum] = useState(3);
-  const [update, setUpdate] = useState(false);
-  const [nodata, setNodata] = useState<boolean>();
-  const [activeCategory, setActiveCategory] = useState<BtnState>({
-    HP8: true,
-  });
   const {
-    makeTrueAround,
-    makeBindDanji,
-    makeDanjiAroundDetailDefault,
-    makeDanjiAroundAddress,
-    makeDanjiAroundLatLng,
-    makeDanjiAroundPlace,
-  } = useMobileDanjiInteraction();
-  const [activeIndex, setActiveIndex] = useState<number>(0);
-  const [isDrag, setIsDrag] = useState<boolean>(false);
-  const [startX, setStartX] = useState<number>();
+    scrollRef,
+    refs,
 
-  const onDragStart = (e: MouseEvent<HTMLDivElement>) => {
-    if (!scrollRef.current) return;
+    onDragStart,
+    onDragMove,
+    onDragEnd,
 
-    e.preventDefault();
-    setIsDrag(true);
-    setStartX(e.pageX + scrollRef.current.scrollLeft);
-  };
+    activeCategory,
+    catergoryList,
+    onClickCategory,
 
-  const onDragEnd = () => {
-    setIsDrag(false);
-  };
+    handleClickBtn,
 
-  const onDragMove = (e: MouseEvent<HTMLDivElement>) => {
-    if (!isDrag) return;
+    nodata,
 
-    if (isDrag && scrollRef.current && startX) {
-      const { scrollWidth, clientWidth, scrollLeft } = scrollRef.current;
+    convertedMarker,
+    convertedCategory,
 
-      scrollRef.current.scrollLeft = startX - e.pageX;
+    isMoreClick,
+    setIsMoreClick,
 
-      if (scrollLeft === 0) {
-        setStartX(e.pageX);
-      } else if (scrollWidth <= clientWidth + scrollLeft) {
-        setStartX(e.pageX + scrollLeft);
-      }
-    }
-  };
-
-  const convertedCategory = useMemo(() => {
-    if (activeCategory.SW8)
-      return convertedArr([...catergoryList].sort((a, b) => Number(a.distance) - Number(b.distance)));
-
-    return [...catergoryList].sort((a, b) => Number(a.distance) - Number(b.distance));
-  }, [activeCategory, update]);
-
-  const convertedMarker = useMemo(() => {
-    if (activeCategory.SW8) {
-      return markers;
-    }
-    return convertedArrForMarker([...markers]);
-  }, [update, activeCategory]);
-
-  const onClickCategory = async (id: keyof BtnState, index: number) => {
-    setActiveIndex(index);
-    setSliceNum(3);
-    setIsMoreClick(false);
-
-    if (id === Object.keys(activeCategory)[0]) return;
-
-    setActiveCategory(() => ({ [id]: true }));
-    setCategoryList([]);
-    setMarkers([]);
-  };
-
-  const handleClickBtn = async (address?: string, placeName?: string, lat?: string, lng?: string) => {
-    await makeTrueAround();
-    makeBindDanji(danji);
-    makeDanjiAroundDetailDefault(Object.keys(activeCategory)[0] as keyof BtnState);
-
-    if (address) {
-      makeDanjiAroundAddress(address);
-    }
-
-    if (placeName) {
-      makeDanjiAroundPlace(placeName);
-    }
-
-    if (lat && lng) {
-      makeDanjiAroundLatLng(lat, lng);
-    }
-  };
-
-  useEffect(() => {
-    const selectedElement = refs.current[activeIndex];
-
-    if (scrollRef.current && selectedElement) {
-      const { offsetLeft } = scrollRef.current;
-      const { offsetLeft: childOffsetLeft, offsetWidth } = selectedElement;
-      scrollRef.current.scrollLeft = childOffsetLeft - offsetLeft - scrollRef.current.offsetWidth / 2 + offsetWidth / 2;
-    }
-  }, [activeIndex]);
-
-  useEffect(() => {
-    let page = 1;
-
-    async function getPlaceData({ x, y, p }: { x?: string; y?: string; p: number }) {
-      const response = await searchCategoryGroup({
-        category_group_code: Object.keys(activeCategory)[0],
-        x,
-        y,
-        radius: 1000,
-        size: 15,
-        page: p,
-      });
-
-      if (!danji?.lat || !danji?.long) return;
-
-      if (response && response.documents.length === 0) {
-        setCategoryList([]);
-        setMarkers([]);
-        setNodata(true);
-        return;
-      }
-
-      if (response && response?.documents) {
-        setNodata(false);
-        const copiedResData = cloneDeep(response.documents);
-        const copiedResMarkerData = cloneDeep(response.documents);
-
-        const convertedResData = copiedResData.map((item) => {
-          if (item.category_group_code === KakaoMapCategoryCode.SUBWAY) {
-            return {
-              ...item,
-              category_name: item.category_name
-                ? item.category_name
-                    .replaceAll(/\s/gi, '')
-                    .slice(item.category_name.replaceAll(/\s/gi, '').lastIndexOf('>') + 1)
-                : item.category_name,
-              place_name: item.place_name.indexOf(' ')
-                ? item.place_name.slice(0, item.place_name.indexOf(' '))
-                : item.place_name,
-            };
-          }
-          return item;
-        });
-
-        setCategoryList((prev) => [...prev, ...convertedResData]);
-        setMarkers((prev) => [...prev, ...copiedResMarkerData]);
-      }
-
-      if (response && !response?.meta.is_end) {
-        getPlaceData({
-          x: danji.long?.toString(),
-          y: danji.lat?.toString(),
-          p: (page += 1),
-        });
-      }
-
-      if (response && response?.meta.is_end) {
-        getPlaceData({
-          x: danji.long?.toString(),
-          y: danji.lat?.toString(),
-          p: 0,
-        });
-
-        setUpdate(true);
-      }
-    }
-
-    getPlaceData({
-      x: danji?.long?.toString(),
-      y: danji?.lat?.toString(),
-      p: 1,
-    });
-
-    return () => {
-      setUpdate(false);
-    };
-  }, [activeCategory]);
+    sliceNum,
+    setSliceNum,
+  } = useAroundInfoMobileHandler({
+    danji,
+  });
 
   if (!danji) {
     return null;
   }
 
   return (
-    <div tw="w-full pt-10 pb-10 px-5 [min-height: 590px]">
-      <div tw="flex w-full justify-between items-center">
-        <span tw="font-bold text-b1 [line-height: 1]">교통 및 주변정보</span>
+    <Container>
+      <TitleWrraper>
+        <Title>교통 및 주변정보</Title>
         <Button size="small" variant="outlined" onClick={() => handleClickBtn()}>
           지도에서 보기
         </Button>
-      </div>
+      </TitleWrraper>
 
-      <div
+      <ScrollContainer
         role="presentation"
         ref={scrollRef}
         onMouseDown={onDragStart}
         onMouseMove={onDragMove}
         onMouseUp={onDragEnd}
         onMouseLeave={onDragEnd}
-        tw="flex items-center [column-gap: 4px] pt-4 pb-4 overflow-x-scroll overflow-y-hidden [-webkit-overflow-scrolling: touch] [padding-right: 1px]"
       >
-        {buttonList.map((item, index) => (
-          <Button
-            variant="outlined"
-            size="medium"
+        {categoryButtonList.map((item, index) => (
+          <DetailCategoryButton
             key={item.id}
             id={item.id}
             ref={(element) => {
               refs.current[index] = element;
             }}
-            tw="[border-radius: 22px] [scroll-snap-align: center] whitespace-nowrap"
             onClick={() => onClickCategory(item.id, index)}
             selected={activeCategory[item.id]}
           >
             {item.korTitle}
-          </Button>
+          </DetailCategoryButton>
         ))}
-      </div>
+      </ScrollContainer>
 
-      <div tw="relative [margin-top: 16px] [margin-bottom: 16px]">
+      <MapWrraper>
         <DanjiAroundMapCard aroundList={convertedMarker} danji={danji} addressName="" />
-      </div>
+      </MapWrraper>
 
       {catergoryList.length === 0 &&
         (nodata ? (
-          <div tw="mt-11" style={{ minHeight: '9.5125rem' }}>
+          <NodataWrraper style={{ minHeight: '9.5125rem' }}>
             <Nodata message="다른 주변 정보를 확인해 보세요." />
-          </div>
+          </NodataWrraper>
         ) : (
-          <div style={{ minWidth: '100%', minHeight: '9.5125rem' }} />
+          <LoadingWrraper />
         ))}
 
       {catergoryList && catergoryList.length > 0 && (
-        <div tw="mt-4">
+        <ListContainer>
           {convertedCategory.slice(0, sliceNum).map((item, index) => (
-            <Div
-              tw="flex items-center"
-              css={[
-                index === 0
-                  ? tw`[border-top: 1px solid #F4F6FA] [border-bottom: 1px solid #F4F6FA] px-4 py-[8px]`
-                  : tw`[border-bottom: 1px solid #F4F6FA] px-4 py-[8.5px]`,
-              ]}
+            <ListItem
+              isFirst={index === 0}
               id={item.id}
               key={`${item.id}-${uuid4()}`}
               onClick={() => {
@@ -306,14 +127,11 @@ export default function AroundInfo({ danji }: CommonDanjiDetailProps) {
               }}
             >
               {activeCategory.SW8 && (
-                <ConvertArrayToSubwayComponent
-                  categoryGroupName={item.category_name}
-                  categoryGroupCode={item.category_group_code}
-                />
+                <SubwayFormatUI categoryGroupName={item.category_name} categoryGroupCode={item.category_group_code} />
               )}
-              <span tw="ml-2 text-b2">{item.place_name}</span>
-              <span tw="text-b2 ml-auto text-gray-1000">{getAverageDistance(item.distance)}m</span>
-            </Div>
+              <NameText>{item.place_name}</NameText>
+              <DistanceText>{getAverageDistance(item.distance)}m</DistanceText>
+            </ListItem>
           ))}
           {convertedCategory.length > 3 &&
             (!isMoreClick ? (
@@ -341,8 +159,8 @@ export default function AroundInfo({ danji }: CommonDanjiDetailProps) {
                 접기
               </Button>
             ))}
-        </div>
+        </ListContainer>
       )}
-    </div>
+    </Container>
   );
 }
--- src/components/domains/danji/detail/around-info-mobile/types.ts
+++ src/components/domains/danji/detail/around-info-mobile/types.ts
@@ -27,3 +27,21 @@ export type Item = {
   road_address_name: string;
   place_url: string;
 };
+
+export type BtnState = {
+  SW8?: boolean;
+  HP8?: boolean;
+  MT1?: boolean;
+  CS2?: boolean;
+  BK9?: boolean;
+  PO3?: boolean;
+};
+
+export const categoryButtonList: { id: keyof BtnState; korTitle: string }[] = [
+  { id: 'SW8', korTitle: '지하철' },
+  { id: 'HP8', korTitle: '병원' },
+  { id: 'MT1', korTitle: '마트' },
+  { id: 'CS2', korTitle: '편의점' },
+  { id: 'BK9', korTitle: '은행' },
+  { id: 'PO3', korTitle: '관공서' },
+];
