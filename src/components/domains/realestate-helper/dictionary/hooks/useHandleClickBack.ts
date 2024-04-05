d358bc4740c3eaa0b14c239576395aad14979920
roy-ju <jujihong2@gmail.com>
2024-04-05 16:35:55 +0900

    feat: update Logic

--- src/components/domains/realestate-helper/dictionary/hooks/useHandleClickBack.ts
+++ src/components/domains/realestate-helper/dictionary/hooks/useHandleClickBack.ts
@@ -2,43 +2,16 @@ import { useCallback } from 'react';
 
 import { useRouter } from 'next/router';
 
-import useAuth from '@/hooks/services/useAuth';
-
-import useAuthPopup from '@/states/hooks/useAuhPopup';
-
-import useReturnUrl from '@/states/hooks/useReturnUrl';
-
-import useInAppBroswerHandler from '@/hooks/useInAppBroswerHandler';
-
 import useCheckPlatform from '@/hooks/useCheckPlatform';
 
 import Routes from '@/router/routes';
 
 export default function useHandleClickBack() {
-  const { user } = useAuth();
-
-  const { openAuthPopup } = useAuthPopup();
-
-  const { handleUpdateReturnUrl } = useReturnUrl();
-
-  const { inAppInfo, handleOpenAppInstallPopup } = useInAppBroswerHandler();
-
   const { platform } = useCheckPlatform();
 
   const router = useRouter();
 
   const handleClickBack = useCallback(() => {
-    if (!user && inAppInfo.isInAppBrowser) {
-      handleOpenAppInstallPopup();
-      return;
-    }
-
-    if (!user && !inAppInfo.isInAppBrowser) {
-      handleUpdateReturnUrl();
-      openAuthPopup('onlyLogin');
-      return;
-    }
-
     if (typeof window !== 'undefined') {
       if (platform === 'pc') {
         router.replace(router?.query?.entry === 'home' ? `/` : `/${Routes.SubHome}`);
@@ -50,15 +23,7 @@ export default function useHandleClickBack() {
         );
       }
     }
-  }, [
-    handleOpenAppInstallPopup,
-    handleUpdateReturnUrl,
-    inAppInfo.isInAppBrowser,
-    openAuthPopup,
-    platform,
-    router,
-    user,
-  ]);
+  }, [platform, router]);
 
   return { handleClickBack };
 }
