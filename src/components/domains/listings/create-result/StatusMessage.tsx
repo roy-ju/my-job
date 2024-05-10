5ec49a0e80a2b3fe6dbfd4fdc3f14aeee6c46798
roy-ju <roy.ju@negocio.co.kr>
2024-05-10 15:42:51 +0900

    feat: qa티켓 업데이트

--- src/components/domains/listings/create-result/StatusMessage.tsx
+++ src/components/domains/listings/create-result/StatusMessage.tsx
@@ -2,7 +2,7 @@ export default function StatusMessage() {
   return (
     <div tw="px-5 pt-7 pb-10 flex flex-col gap-3">
       <div tw="text-heading_02">매물등록 준비 중입니다.</div>
-      <div tw="text-info text-gray-700">담당 중개사님을 배정 중입니다.</div>
+      <div tw="text-info text-gray-700">매물 등록이 완료되면 알려드릴게요! 조금만 기다려주세요.</div>
     </div>
   );
 }
