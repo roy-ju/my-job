b2fcbb86a8cf13aa7827b3f38f1bee817af3c29b
roy-ju <roy.ju@negocio.co.kr>
2024-06-05 17:24:39 +0900

    feat: 안쓰는 파일 삭제

--- src/components/templates/MobDanjiDetail/Components/suggestsOrListings/hooks/useCheckSuggestsOrListings.ts
+++
@@ -1,7 +0,0 @@
-import getSuggestOrListings from '../../../utils/getSuggestOrListings';
-
-export default function useCheckSuggestsOrListings({ tab }: { tab: number }) {
-  const type = getSuggestOrListings(tab);
-
-  return { type };
-}
